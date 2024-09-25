88d3ed349751db28cd60ae9a32057a2949535658
Joao Miguel Morais Sellers <joao.morais@unealum.local>
2024-09-25 13:30:32 +0200

    java primer commit

---
+++ entregas/moraisMiguel/java/helloworldtypeshit.java
@@ -0,0 +1,11 @@
+class HolaMundo{
+
+    public static void main(String[]args){
+        System.out.println("==========================");
+        System.out.println("|Hola Mundo              |");
+        System.out.println("|Hoy comere macarrones :D|");
+        System.out.println("|Adios Mundo!            |");
+        System.out.println("==========================");
+    }
+
+}
