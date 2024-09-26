37dadc74046c6d4c5c12a5c307c1136dc3c93eb5
madero.carla <carla.madero@alumnos.uneatlantico.es>
2024-09-26 10:37:08 +0200

    cambios

---
+++ entregas/maderoCarla/src/Hola mundo.java
@@ -0,0 +1,36 @@
+class HolaMundo {
+    public static void main(string [] args ){
+
+        System.out.println("===============");
+        System.out.println("Hola mundo");
+        System.out.println("Hace mucho calor");
+        System.out.println("Adios mundo cruel");
+        System.out.println("===============");
+        
+
+    }
+}
+
+
+
+
+//clase 25/9
+
+class HolaMundo {
+
+    public static void main(string [] args ){
+
+        System.out.println("===============");
+        int edad;
+        double dinero;
+
+        string nombre;
+        edad = 12;
+        dinero = 100;
+        nombre ="Firibicundio";
+
+        system.out.println("edad");
+        system.out.println("dinero");
+        system.out.println("nombre");
+    }
+}
