6d56195716b62f14d3981d3caed86b27adc8e347
31Diego <diegomartinezt31@gmail.com>
2024-10-01 12:45:52 +0200

    char

--- entregas/martinezDiego/src/HolaMundo.java
+++ entregas/martinezDiego/src/HolaMundo.java
@@ -2,20 +2,30 @@ class HolaMundo {
     public static void main(String[] args) {
 
         System.out.println("inicio");
-        
 
         int edad;
         double dinero;
         String nombre;
+        char unChar;
 
         edad = 12;
         dinero = 100.0;
         nombre = "Juan";
+        unChar = 'x';
 
         System.out.println(edad);
         System.out.println(dinero);
         System.out.println(nombre);
-        
+
+        System.out.println(" me llamo " + nombre + " tengo " + edad + " años y " + dinero + " euros");
+
+        dinero = dinero = 120;
+
+        System.out.println("ahora tengo " + dinero + " euros");
+
+        dinero = dinero = 50;
+
+        System.out.println("ahora tengo " + dinero + " euros");
 
     }
 }
