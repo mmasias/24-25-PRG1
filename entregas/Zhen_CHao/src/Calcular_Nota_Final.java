df28ad495d955928ca7b117f0e9e7a76b18c8cd0
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-10-01 14:05:15 +0200

    Uso del Scanner para Nota Final

--- entregas/Zhen_CHao/src/Calcular_Nota_Final.java
+++ entregas/Zhen_CHao/src/Calcular_Nota_Final.java
@@ -1,19 +1,31 @@
-public class Calcular_Nota_Final {
+    import java.util.Scanner;
+    public class Calcular_Nota_Final {
 
     public static void main(String[] args) {
-
-      double  examenFinal;
+    @SuppressWarnings("resource")           
+    Scanner scanner = new Scanner(System.in); 
+                 
       double evaluacionContinua;
+      double  examenFinal;
       double examenParcial ; 
       double evaluacionProfe;
 
-    examenFinal = 9*50;
-    evaluacionContinua = 8*20;
-    examenParcial = 7*25;
-    evaluacionProfe = 3*5;
+        System.out.println("Digite su nota");
+        examenFinal =  scanner.nextDouble();
+        System.out.println("Digite su nota");
+        evaluacionContinua =scanner.nextDouble() ;
+        System.out.println("Digite su nota");
+        examenParcial = scanner.nextDouble();
+        System.out.println("Digite su nota");
+        evaluacionProfe = scanner.nextDouble();
 
-    double notaFinal = (examenFinal +evaluacionContinua + examenParcial +evaluacionProfe)/100;
-    System.out.println("Su nota final es "+ notaFinal);
+        final double EXAMEN_FINAL_PONDERADO = 0.5;    
+        final double EXAMEN_PARCIAL_PONDERADO = 0.25;  
+        final double EVALUACION_CONTINUA_PONDERADO = 0.2;  
+        final double NOTA_PROFESOR_PONDERADO = 0.05;  
+
+        double notaFinal = (examenFinal*EXAMEN_FINAL_PONDERADO) +(evaluacionContinua*EVALUACION_CONTINUA_PONDERADO)  + (examenParcial*EXAMEN_PARCIAL_PONDERADO) +(evaluacionProfe*NOTA_PROFESOR_PONDERADO);
+        System.out.println("Su nota final es "+ notaFinal*10 + "%");
 
 }
 }
