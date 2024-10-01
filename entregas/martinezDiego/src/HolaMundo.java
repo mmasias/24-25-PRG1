ca669b9d65ed147ba8d33c96f296c88b597f61ab
31Diego <diegomartinezt31@gmail.com>
2024-10-01 12:30:56 +0200

    holamundo

---
+++ entregas/martinezDiego/src/HolaMundo.java
@@ -0,0 +1,21 @@
+class HolaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("inicio");
+        
+
+        int edad;
+        double dinero;
+        String nombre;
+
+        edad = 12;
+        dinero = 100.0;
+        nombre = "Juan";
+
+        System.out.println(edad);
+        System.out.println(dinero);
+        System.out.println(nombre);
+        
+
+    }
+}
