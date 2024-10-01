e7324ace8baee46c4335404e49ec0dac79c1b4ce
Jose Daniel Filoth <jose.filoth@alumnos.uneatlantico.es>
2024-10-01 12:47:26 +0200

    Practica en Clase

---
+++ entregas/filothJose/src/HolaMundo.java
@@ -0,0 +1,30 @@
+class HolaMundo (
+    public static void main(String[] args) {
+
+        System.out.println("Inicio");
+
+        int edad;
+        double dinero;
+        String nombre;
+        char unChar;
+
+        edad =12;
+        dinero = 100;
+        nombre = "Firibricundio";
+        unChar = 'x';
+
+        System.out.println("Me llamo" + nombre + ", tengo" + edad + " años y " + dinero + " euros ");
+        
+        dinero = dinero + 100;
+
+        System.out.println("Ahora tengo " + dinero + "euros");
+
+        dinero = dinero - 30;
+
+        System.out.println(" Y ahora tengo" + dinero + "euros")
+
+
+    }
+
+    )
+)
