9384c3a06993a6bde082e9089b056d41cc6d8305
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-10-01 13:59:57 +0200

    Actualizo NotaFinal

--- entregas/Java/NotaFinal.java
+++ entregas/Java/NotaFinal.java
@@ -1,14 +1,64 @@
+import java.util.Scanner;
 
 class NotaFinal {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         double evFinal, evContinua, evParcial, evProfesor;
 
-        evFinal = 8;
-        evContinua = 2;
-        evParcial = 3;
-        evProfesor = 0;
+        System.out.println("¿Cuál es tu nota en el examen final?");
+        evFinal = scanner.nextDouble();
+
+        if (evFinal > 10) {
+            System.out.println("Su nota del examen final no puede ser mayor a 10.");
+            System.exit(0);
+        }
+
+        if (evFinal < 0) {
+            System.out.println("Su nota del examen final no puede ser menor a 0.");
+            System.exit(0);
+        }
+
+        System.out.println("¿Cuál es tu nota en la evaluación continua?");
+        evContinua = scanner.nextDouble();
+
+        if (evContinua > 10) {
+            System.out.println("Su nota de la evaluación continua no puede ser mayor a 10.");
+            System.exit(0);
+        }
+
+        if (evContinua < 0) {
+            System.out.println("Su nota de la evaluación continua no puede ser menor a 0.");
+            System.exit(0);
+        }
+
+        System.out.println("¿Cuál es tu nota en el examen parcial?");
+        evParcial = scanner.nextDouble();
+
+        if (evParcial > 10) {
+            System.out.println("Su nota del examen parcial no puede ser mayor a 10.");
+            System.exit(0);
+        }
+
+        if (evParcial < 0) {
+            System.out.println("Su nota del examen parcial no puede ser menor a 0.");
+            System.exit(0);
+        }
+
+        System.out.println("¿Cuál es la nota de su profesor sobre usted?");
+        evProfesor = scanner.nextDouble();
+
+        if (evProfesor > 10) {
+            System.out.println("La nota de su profesor no puede ser mayor a 10.");
+            System.exit(0);
+        }
+
+        if (evProfesor < 0) {
+            System.out.println("La nota de su profesor no puede ser menor a 0.");
+            System.exit(0);
+        }
 
         final double EV_FINAL_PONDERACION = 0.50;
         final double EV_CONTINUA_PONDERACION = 0.20;
@@ -23,7 +73,6 @@ class NotaFinal {
                 + evParcial * EV_PARCIAL_PONEDERACION
                 + evProfesor * EV_PROFESOR_PONDERACION;
 
-        System.out.println("nota final: " + notaFinal);
-
+        System.out.println("Por la información dada, su nota resultante sería la consiguiente: " + notaFinal);
     }
 }
