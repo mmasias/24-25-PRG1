7f419e20299d1c1230a47541a6f6b01fba776067
Bea <beatrizorozcodelgado@gmail.com>
2024-10-01 13:39:40 +0200

    Creo las constantes de las ponderaciones de la evaluaciones

--- entregas/orozcoBeatriz/01-10/src/CalculoNotaFinal.java
+++ entregas/orozcoBeatriz/01-10/src/CalculoNotaFinal.java
@@ -1,6 +1,11 @@
 public class CalculoNotaFinal {
     public static void main(String[] args) {
         double examenFinal, examenParcial, evaluacionContinua, notaProfesor;
+
+        final double EXAMEN_FINAL_PONDERADO = 0.5;
+        final double EXAMEN_PARCIAL_PONDERADO = 0.25;
+        final double EVALUACION_CONTINUA_PONDERADO = 0.2;
+        final double NOTA_PROFESOR_PONDERADO = 0.05;
     }
     
 }
