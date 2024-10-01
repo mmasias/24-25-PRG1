955d085f6332e1ca220a5408399c361f3a5e4a31
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-10-01 13:24:48 +0200

    Nota Final

--- entregas/Zhen_CHao/src/Hola_Mundo.java
+++ entregas/Zhen_CHao/src/Hola_Mundo.java
@@ -4,15 +4,21 @@ public static void main(String[] args) {
     int num ;
     String nombre ;
     char unChar;
+    double dinero;
 
     num = 25;
     nombre = "Angelo";
     unChar =  'X';
+    dinero = 30.5;
 
     System.out.println(nombre +" tiene "+ num );
     System.out.println("ok");
     System.out.println("que tal");
      
     System.out.println(unChar);
+
+    double lol = num-dinero; 
+    System.out.println(lol);
+    System.out.println("Ahora tengo " +  lol + " euros en la cuenta");
 }
 }
