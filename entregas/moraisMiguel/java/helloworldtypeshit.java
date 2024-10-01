8ebff237e5724a265fb324030a899f7fa2e9576c
Joao Miguel Morais Sellers <joao.morais@unealum.local>
2024-10-01 13:01:43 +0200

    calculo de nota hecho

--- entregas/moraisMiguel/java/helloworldtypeshit.java
+++ entregas/moraisMiguel/java/helloworldtypeshit.java
@@ -39,6 +39,32 @@ class HolaMundo{
         /** Intente hacer exponente pero dado que es un double y no un int me dio error */
 
         System.out.println("Milagrosamente, ya que inverti en bitcoin hace 15 años, ahora tengo la gran suma de " + dineroTrasBitcoin + " euros.");
+
+        /** valor de notas:
+         * examen parcial  25%
+         * examen final 50%
+         * evaluacion del profesor 5%
+         * evaluacion continua 20%
+         */
+
+        double examenParcial;
+        double examenFinal;
+        double evaluacionProfesor;
+        double evaluacionContinua;
+        /**notas sobre 10 */
+        examenParcial = 8;
+        examenFinal = 7;
+        evaluacionContinua = 9;
+        evaluacionProfesor = 6;
+
+        System.out.println("Si he sacado en mis parciales una media de " + examenParcial + " puntos, en mi final saque " + examenFinal + " puntos, " + evaluacionContinua + " fue lo que obtuve en mi evaluacion continua, y la nota que me puso mi profesor en su evaluacion personal fue de " + evaluacionProfesor + ", tengo una nota media de:");
+
+        double mediaFinal;
+
+        mediaFinal = examenFinal * 0.5 + examenParcial * 0.25 + evaluacionContinua * 0.2 + evaluacionProfesor * 0.05;
+
+        System.out.println(mediaFinal + " puntos sobre 10"); 
+
     }
 
 }
