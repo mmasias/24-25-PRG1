9943b4473a8c2372add6bd15af4e20471d901de3
Jaime Lomo Benito <jaime.lomo@alumnos.uneatlantico.es>
2024-10-01 14:00:04 +0200

    Subiendo los progrmas

---
+++ entregas/lomoJaime/src/Holamundo.java
@@ -0,0 +1,19 @@
+/**
+ * Holamundo
+ */
+class Holamundo {
+    public static void main(String[] args) 
+    {
+      System.out.println(" *Inicio*");
+      int edad = 20 ;
+      double dinero= 100;
+      String nombre = "Jaime Lomo";
+      char unChar = 'x';
+        System.out.println("Me llamo " + nombre +", tengo " + edad + " años tengo " + dinero + " Euros");
+        dinero = dinero + 50;
+        System.out.println(" Ahora tengo "+ dinero + " Euros");
+        System.out.println("=============================================");
+
+    }
+    
+}
