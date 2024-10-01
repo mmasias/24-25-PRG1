11a625db163f0137a944ad4ce7c650fa33001fb6
danieldiezridner <danidiezridner@gmail.com>
2024-10-01 13:50:55 +0200

    SCANNER

--- entregas/díezDaniel/visual/CalculaNotaFinal.java
+++ entregas/díezDaniel/visual/CalculaNotaFinal.java
@@ -1,14 +1,32 @@
+
+import java.util.Scanner;
+
 public class CalculaNotaFinal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         double examenFinal, examenParcial, evalucaionContinua, notaProfesor;
 
         double examenFinalPonderado = 0.5;
         double examenParcialPonderado = 0.25;
-        double evalucaionContinuaPonderado = 0.25;
+        double evalucaionContinuaPonderado = 0.2;
         double notaProfesorPonderado = 0.05;
 
-        examenParcial = 5;
+        System.out.println("Dime tu nota del examen final");
+        examenFinal = scanner.nextDouble();
+        System.out.println("Dime tu nota del examen parcial");
+        examenParcial = scanner.nextDouble();
+        System.out.println("Dime tu nota de evaluacion continua");
+        evalucaionContinua = scanner.nextDouble();
+        System.out.println("Dime tu nota del profesor");
+        notaProfesor = scanner.nextDouble();
+
+        final double EF_PORCENTAJE = 0.5;
+        final double EP_PORCENTAJE = 0.25;
+        final double EC_PORCENTAJE = 0.2;
+        final double EPR_PORCENTAJE = 0.05;
+
+        examenParcial = 6;
         examenFinal = 6;
         evalucaionContinua = 3;
         notaProfesor = 5;
