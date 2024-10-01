13f61c5bb6c8df71e51d6e6f0ae293476e1916d2
OscarPineda9 <oscar.pineda@alumnos.uneatlantico.es>
2024-10-02 01:57:12 +0200

    Creacion de varaibles notas y el Scanner

---
+++ entregas/PinedaOscar/Entrega-01/CalcularNota.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class CalcularNota{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double notaFinal;
+        double notaParcial;
+        double notaContinua;
+        double notaProfe;
+
+        double porcentajeFinal = 0.5;
+        double porcentajeParcial = 0.25;
+        double porcentajeContinua = 0.20;
+        double porcentajeProfe = 0.05;
+    
+    }
+}
