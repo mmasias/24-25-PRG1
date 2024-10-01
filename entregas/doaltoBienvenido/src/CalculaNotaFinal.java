1bf02eda1029ad6772a6e7e9d609dd40f789e474
Bienvenido Doalto Cavada <bienvenido.doalto@alumnos.uneatlantico.es>
2024-10-01 14:00:55 +0200

    Practicas java

---
+++ entregas/doaltoBienvenido/src/CalculaNotaFinal.java
@@ -0,0 +1,39 @@
+import javax.swing.plaf.basic.BasicSplitPaneDivider;
+import java.util.Scanner;
+
+
+class CalculaNotaFinal
+{
+    public static void main(String[] args) 
+    {
+        Scanner objeto = new Scanner(System.in);
+
+        System.out.println("*INICIO*");
+        System.out.println("=============================");
+
+        final double PONDERACION_EXAMEN_FINAL = 0.5;
+        final double PONDERACION_EXAMEN_PARCIAL = 0.2;
+        final double PONDERACION_EVALUACION_CONTINUA = 0.25;
+        final double PONDERACION_EVALUACION_PROFESOR = 0.05;
+
+        System.out.println("Que nota en Examen Final: ");
+        double notaExamenFinal = objeto.nextDouble();
+       
+        System.out.println("Que nota en Examen Parcial: ");
+        double notaExamenParcial = objeto.nextDouble();
+
+        System.out.println("Que nota en Evaluacion Continua: ");
+        double notaEvaluacionContinua = objeto.nextDouble();
+
+        System.out.println("Que nota en Evaluacion del Profesor: ");
+        double notaEvalucacionProfesor = objeto.nextDouble();
+
+        double notaFinal =  notaExamenFinal * PONDERACION_EXAMEN_FINAL + 
+                            notaExamenParcial * PONDERACION_EXAMEN_PARCIAL + 
+                            notaEvaluacionContinua * PONDERACION_EVALUACION_CONTINUA + 
+                            notaEvalucacionProfesor * PONDERACION_EVALUACION_PROFESOR;
+
+        System.out.println();
+        System.out.println("Mi nota final de la evaluación es: " + notaFinal);
+    }
+}
--- entregas/doaltoBienvenido/src/HolaMundo.java
+++ entregas/doaltoBienvenido/src/HolaMundo.java
@@ -1,7 +1,26 @@
+import javax.swing.plaf.basic.BasicSplitPaneDivider;
+
+
 class HolaMundo
 {
     public static void main(String[] args) 
     {
         System.out.println("Hola Mundo");
+        System.out.println("=============================");
+
+        System.out.println("INICIO");
+        System.out.println("=============================");
+
+        int edad = 18;
+        String nombre = "Bienve";
+        double dinero = 100;
+
+        System.out.println("Me llamo " + nombre + ", tengo " + edad + " años y tengo " + dinero + " euros.");
+
+        dinero = dinero + 50;
+        
+        System.out.println("Ahora tengo " + dinero + " euros");
+        System.out.println("=============================");
+
     }
 }
