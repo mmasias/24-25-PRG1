5bb99ef85dfa7f9d093fa979da5cf9a86472be5d
Bernie Zablah <bernardo.zablah@alumnos.uneatlantico.es>
2024-10-02 15:23:06 +0200

    segundo commit funcion math

--- entregas/zablahBernardo/src/02-10/FuncionMath.java
+++ entregas/zablahBernardo/src/02-10/FuncionMath.java
@@ -4,7 +4,17 @@ class FuncionMath {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     
+        int minimo;
+        int maximo;
+        int aleatorio;
         
+        System.out.println("Numero minimo deseado");
+        minimo = scanner.nextInt();
+        System.out.println("Numero Maximo deseado");
+        maximo = scanner.nextInt();
+        aleatorio = (int)(Math.random()*(maximo-minimo)+minimo);
+
+        System.out.println(aleatorio);
 
         scanner.close();
     }
