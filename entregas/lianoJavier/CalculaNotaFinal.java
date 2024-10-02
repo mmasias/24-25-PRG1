cec1414348bc3e1c70011bda01b29272125b38fb
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-10-02 13:52:51 +0200

    Corrección al ejercicio del otro día.

--- entregas/lianoJavier/CalculaNotaFinal.java
+++ entregas/lianoJavier/CalculaNotaFinal.java
@@ -8,73 +8,81 @@ class CalculaNotaFinal {
 
         Scanner scanner = new Scanner(System.in);
 
-        double evFinal, evContinua, evParcial, evProfesor;
+        double evaluacionFinal;
 
         System.out.println("¿Cuál es tu nota en el examen final?");
-        evFinal = scanner.nextDouble();
+        evaluacionFinal = scanner.nextDouble();
 
-        if (evFinal > 10) {
-            System.out.println("Su nota del examen final no puede ser mayor a 10.");
-            System.exit(0);
-        }
+        // if (evFinal > 10) {
+        //     System.out.println("Su nota del examen final no puede ser mayor a 10.");
+        //     System.exit(0);
+        // }
 
-        if (evFinal < 0) {
-            System.out.println("Su nota del examen final no puede ser menor a 0.");
-            System.exit(0);
-        }
+        // if (evFinal < 0) {
+        //     System.out.println("Su nota del examen final no puede ser menor a 0.");
+        //     System.exit(0);
+        // }
+
+        double evaluacionContinua;
 
         System.out.println("¿Cuál es tu nota en la evaluación continua?");
-        evContinua = scanner.nextDouble();
+        evaluacionContinua = scanner.nextDouble();
 
-        if (evContinua > 10) {
-            System.out.println("Su nota de la evaluación continua no puede ser mayor a 10.");
-            System.exit(0);
-        }
+        // if (evContinua > 10) {
+        //     System.out.println("Su nota de la evaluación continua no puede ser mayor a 10.");
+        //     System.exit(0);
+        // }
 
-        if (evContinua < 0) {
-            System.out.println("Su nota de la evaluación continua no puede ser menor a 0.");
-            System.exit(0);
-        }
+        // if (evContinua < 0) {
+        //     System.out.println("Su nota de la evaluación continua no puede ser menor a 0.");
+        //     System.exit(0);
+        // }
+
+        double evaluacionParcial;
 
         System.out.println("¿Cuál es tu nota en el examen parcial?");
-        evParcial = scanner.nextDouble();
+        evaluacionParcial = scanner.nextDouble();
 
-        if (evParcial > 10) {
-            System.out.println("Su nota del examen parcial no puede ser mayor a 10.");
-            System.exit(0);
-        }
+        // if (evParcial > 10) {
+        //     System.out.println("Su nota del examen parcial no puede ser mayor a 10.");
+        //     System.exit(0);
+        // }
 
-        if (evParcial < 0) {
-            System.out.println("Su nota del examen parcial no puede ser menor a 0.");
-            System.exit(0);
-        }
+        // if (evParcial < 0) {
+        //     System.out.println("Su nota del examen parcial no puede ser menor a 0.");
+        //     System.exit(0);
+        // }
+
+        double evaluacionProfesor;
 
         System.out.println("¿Cuál es la nota de su profesor sobre usted?");
-        evProfesor = scanner.nextDouble();
+        evaluacionProfesor = scanner.nextDouble();
 
-        if (evProfesor > 10) {
-            System.out.println("La nota de su profesor no puede ser mayor a 10.");
-            System.exit(0);
-        }
+        // if (evProfesor > 10) {
+        //     System.out.println("La nota de su profesor no puede ser mayor a 10.");
+        //     System.exit(0);
+        // }
 
-        if (evProfesor < 0) {
-            System.out.println("La nota de su profesor no puede ser menor a 0.");
-            System.exit(0);
-        }
+        // if (evProfesor < 0) {
+        //     System.out.println("La nota de su profesor no puede ser menor a 0.");
+        //     System.exit(0);
+        // }
 
-        final double EV_FINAL_PONDERACION = 0.50;
-        final double EV_CONTINUA_PONDERACION = 0.20;
-        final double EV_PARCIAL_PONEDERACION = 0.25;
-        final double EV_PROFESOR_PONDERACION = 0.05;
+        final double PONDERACION_EVALUACION_FINAL = 0.50;
+        final double PONDERACION_EVALUACION_CONTINUA = 0.20;
+        final double PONDERACION_EVALUACION_PARCIAL = 0.25;
+        final double PONDERACION_EVALUACION_PROFESOR = 0.05;
 
         double notaFinal;
 
         notaFinal
-                = evFinal * EV_FINAL_PONDERACION
-                + evContinua * EV_CONTINUA_PONDERACION
-                + evParcial * EV_PARCIAL_PONEDERACION
-                + evProfesor * EV_PROFESOR_PONDERACION;
+                = evaluacionFinal * PONDERACION_EVALUACION_FINAL
+                + evaluacionContinua * PONDERACION_EVALUACION_CONTINUA
+                + evaluacionParcial * PONDERACION_EVALUACION_PARCIAL
+                + evaluacionProfesor * PONDERACION_EVALUACION_PROFESOR;
 
         System.out.println("Por la información dada, su nota resultante sería la consiguiente: " + notaFinal);
+
+        scanner.close();
     }
 }
