998df6395ac302d0d1b6fd354a7a6b0fd9703a95
Bernie Zablah <bernardo.zablah@alumnos.uneatlantico.es>
2024-10-02 13:58:32 +0200

    primer commit

---
+++ entregas/zablahBernardo/src/02-10/FuncionMath.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class FuncionMath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        
+
+        scanner.close();
+    }
+}
