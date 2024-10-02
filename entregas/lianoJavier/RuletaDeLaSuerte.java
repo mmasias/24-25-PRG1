67b05489cc980e693df5d1bfb1ac4ea5586a28ce
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-10-02 13:56:42 +0200

    Actualizo el archivo RuletaDeLaSuerte y le cambio el nombre

--- entregas/lianoJavier/RuletaDeLaSuerte.java
+++ entregas/lianoJavier/RuletaDeLaSuerte.java
@@ -1,27 +1,28 @@
 
 import java.util.Scanner;
 
-class NumeroAleatorio {
+class RuletaDeLaSuerte {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Jugemos a la ruleta :3c");
         System.out.println("Elige un número de la ruleta");
-        System.out.println("Desde el -1 al 36");
-
+        System.out.println("Desde el -1 al 36 (el -1 equivale al 00 en una ruleta real)");
+        
         int minimo, maximo;
-
-        minimo = 0;
-        maximo = 0;
+        
+        minimo = -1;
+        maximo = 36;
         int random = (int) (Math.random() * (maximo - minimo) + minimo);
-
+        
         String veredicto;
-
+        
         int eleccion = scanner.nextInt();
-
+        
         veredicto = random == eleccion ? "Ganó!" : "Perdió :3c";
-
+        
+        System.out.println("El resultado fué: " + random);
         System.out.println(veredicto);
 
         scanner.close();
