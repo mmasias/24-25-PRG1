72aa3877f5366c02e84af22c7efb3f03861ca2b9
31Diego <diegomartinezt31@gmail.com>
2024-10-02 14:20:33 +0200

    NumeroAleatorio

--- entregas/martinezDiego/src/CalcularNota.java
+++ entregas/martinezDiego/src/CalcularNota.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 
-
 class CalcularNota {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
-       
+
         double examenFinal, examenParcial, evaluacionContinua, evaluacionProfesor;
 
         System.out.println("nota del final");
-        examenFinal = scanner.nextDouble() ;
+        examenFinal = scanner.nextDouble();
         System.out.println("nota del parcial");
         examenParcial = scanner.nextDouble();
         System.out.println("nota de la evaluacion continua");
         evaluacionContinua = scanner.nextDouble();
         System.out.println("nota de la evaluacion del profesor");
         evaluacionProfesor = scanner.nextDouble();
-       
-       
-       
-       
+
         double EXAMEN_FINAL_PONDERADO = 0.5;
         double EXAMEN_PARCIAL_PONDERADO = 0.25;
-        double  EVALUACION_CONTINUA_PONDERADO= 0.2;
-        double EVALUACION_PROFESOR_PONDERADO= 0.05;
+        double EVALUACION_CONTINUA_PONDERADO = 0.2;
+        double EVALUACION_PROFESOR_PONDERADO = 0.05;
 
         double notaFinal;
 
-        notaFinal = examenFinal * EXAMEN_FINAL_PONDERADO + 
-        examenParcial * EXAMEN_PARCIAL_PONDERADO + 
-        evaluacionContinua * EVALUACION_CONTINUA_PONDERADO + 
-        evaluacionProfesor * EVALUACION_PROFESOR_PONDERADO;
-    
+        notaFinal = examenFinal * EXAMEN_FINAL_PONDERADO +
+                examenParcial * EXAMEN_PARCIAL_PONDERADO +
+                evaluacionContinua * EVALUACION_CONTINUA_PONDERADO +
+                evaluacionProfesor * EVALUACION_PROFESOR_PONDERADO;
 
-        System.out.println("Nota final ="  + notaFinal);
-}
-}
+        System.out.println("Nota final =" + notaFinal);
+        
+    }
+} 
+
+
+
+
