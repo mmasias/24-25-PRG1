be0be85da43c5ccf6587a276536a2c53b821e809
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-10-02 13:58:44 +0200

    Ejemplo clase 02-10

--- entregas/Zhen_CHao/src/Calcular_Nota_Final.java
+++ entregas/Zhen_CHao/src/Calcular_Nota_Final.java
@@ -19,12 +19,12 @@
         System.out.println("Digite su nota");
         evaluacionProfe = scanner.nextDouble();
 
-        final double EXAMEN_FINAL_PONDERADO = 0.5;    
-        final double EXAMEN_PARCIAL_PONDERADO = 0.25;  
-        final double EVALUACION_CONTINUA_PONDERADO = 0.2;  
-        final double NOTA_PROFESOR_PONDERADO = 0.05;  
+      final double EXAMEN_FINAL_PONDERADO = 0.5;    
+      final double EXAMEN_PARCIAL_PONDERADO = 0.25;  
+      final double EVALUACION_CONTINUA_PONDERADO = 0.2;  
+      final double NOTA_PROFESOR_PONDERADO = 0.05;  
 
-        double notaFinal = (examenFinal*EXAMEN_FINAL_PONDERADO) +(evaluacionContinua*EVALUACION_CONTINUA_PONDERADO)  + (examenParcial*EXAMEN_PARCIAL_PONDERADO) +(evaluacionProfe*NOTA_PROFESOR_PONDERADO);
+        double notaFinal = (examenFinal*EXAMEN_FINAL_PONDERADO) + (evaluacionContinua*EVALUACION_CONTINUA_PONDERADO)  + (examenParcial*EXAMEN_PARCIAL_PONDERADO) +(evaluacionProfe*NOTA_PROFESOR_PONDERADO);
         System.out.println("Su nota final es "+ notaFinal*10 + "%");
 
 }
