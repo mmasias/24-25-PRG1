d236476bfe39046cf435693f5a1474c3708c463d
Joao Miguel Morais Sellers <joao.morais@unealum.local>
2024-10-02 13:55:39 +0200

    mates added

---
+++ entregas/moraisMiguel/java/matematicas.java
@@ -0,0 +1,22 @@
+class matematicas{
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        final int MAXIMO = 10;
+        final int MINIMO = 1;
+
+
+        int aleatorio = (int) (Math.random()*(MAXIMO-MINIMO)+MINIMO);
+
+
+        System.out.println(aleatorio);
+
+        scanner.close();                
+
+
+    }
+
+
+}
