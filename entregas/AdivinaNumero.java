82a4fd78142fd9a9723dd89cd77c41d5372f75fc
sergiopuente67 <sergio.puente@alumnos.uneatlantico.es>
2024-10-02 18:07:43 +0200

    Inicio de programa pidiendo mínimo y máximo

---
+++ entregas/AdivinaNumero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AdivinaNumero {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int minimo, maximo ;
+        System.out.println("Nivel de dificultad");
+        System.out.println("Numero minimo");
+        minimo = scanner.nextInt();
+        System.out.println("Numero maximo");
+        maximo = scanner.nextInt();
+
+        
+    }
+}
