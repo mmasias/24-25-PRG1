25a113fc796ac2ec365426d0439cbc5c0318f299
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-10-02 13:58:16 +0200

    Ejemplo clase 02/10/24

---
+++ entregas/Zhen_CHao/src/ejemploClase.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ejemploClase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       int minimo,maximo;
+
+        System.out.println("Nivel de dififcultad");
+        System.out.println("Dime el numero minimo");
+        minimo = scanner.nextInt();
+        System.out.println("Dime el numero maximo");
+        maximo = scanner.nextInt();
+
+        int aleatorio = (int)(Math.random()*(maximo-minimo)+minimo);
+
+        int numeroDelUsuario;
+        System.out.println("Dime un numeor entre "+minimo+ " y "+maximo);
+        numeroDelUsuario =scanner.nextInt();
+
+        String veredicto;
+        veredicto = numeroDelUsuario!= aleatorio? "adivino!" :"no adivina!";
+        System.out.println(veredicto);
+        scanner.close();
+
+        
+    }
+}
+
