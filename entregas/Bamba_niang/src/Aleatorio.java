dd5c14053fa463d7ad4bd484e15f4a0de0496434
elhadjiniang646 <niangbamba811@gmail.com>
2024-10-02 16:35:29 +0200

    segunda PRG

---
+++ entregas/Bamba_niang/src/Aleatorio.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Aleatorio{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int minimo, maximo;
+        System.out.println("Nivel de dificultad");
+        System.out.println("Numero minimo");
+        minimo = scanner.nextInt();
+        System.out.println("Numero maximo");
+        maximo = scanner.nextInt();
+
+        int aleatorio = (int) (Math.random()*(maximo-minimo)+minimo);
+
+        int numeroDelusuario;
+
+        System.out.println("Dime un numero entre "+minimo+" y "+maximo+" (inclusive)" );
+        numeroDelusuario = scanner.nextInt();
+
+        String verdicto;
+        
+         verdicto = numeroDelusuario==aleatorio ? "adivino" : "no adivino";
+
+         System.out.println("verdicto");
+
+
+         scanner.close();
+
+
+    }
+}
+
