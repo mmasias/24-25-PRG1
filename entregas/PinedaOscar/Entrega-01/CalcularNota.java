c8e04d31d49c3a94e605196472285286f53da41e
OscarPineda9 <oscar.pineda@alumnos.uneatlantico.es>
2024-10-02 02:07:58 +0200

    Declaracion de variables y la ecuacion para sacar la nota final del curso.

--- entregas/PinedaOscar/Entrega-01/CalcularNota.java
+++ entregas/PinedaOscar/Entrega-01/CalcularNota.java
@@ -4,15 +4,33 @@ class CalcularNota{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double notaFinal;
+        double notaExFinal;
         double notaParcial;
         double notaContinua;
         double notaProfe;
+        double notaFinal;
 
         double porcentajeFinal = 0.5;
         double porcentajeParcial = 0.25;
         double porcentajeContinua = 0.20;
         double porcentajeProfe = 0.05;
-    
+
+        System.out.println(" Cuanto has sacado en tu examen final");
+        notaExFinal = scanner.nextDouble();
+
+        System.out.println(" Cuanto has sacado en el parcial");
+        notaParcial = scanner.nextDouble();
+
+        System.out.println(" Cuanto has sacado en Evaluacion Continua");
+        notaContinua = scanner.nextDouble();
+
+        System.out.println(" Cuanto te a dado el profesor en participacion de clase e interes ");
+        notaProfe = scanner.nextDouble();
+
+        notaFinal = porcentajeFinal*notaExFinal + porcentajeParcial*notaParcial + porcentajeContinua*notaContinua + porcentajeProfe*notaProfe;
+
+        System.out.println("Tu nota final del curso es de: "+notaFinal+"");
+
+
     }
 }
