0032ec7bf0c675a267eaa7f45c500fe722c5b5cc
sergiopuente67 <sergio.puente@alumnos.uneatlantico.es>
2024-10-07 18:04:21 +0200

    Definidas el porcentaje de la nota

---
+++ entregas/AdivinaNota.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AdivinaNota {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double examenFinal, examenParcial, evaluacionContinua, notaProfesor;
+
+        final double EXAMEN_FINAL_PONDERADO = 0.5;
+        final double EXAMEN_PARCIAL_PONDERADO = 0.25;
+        final double EVALUACION_CONTINUA_PONDERADO = 0.2;
+        final double NOTA_PROFESOR_PONDERADO = 0.05;
+    }   
+}
