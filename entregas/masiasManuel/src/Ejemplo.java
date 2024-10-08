94d1a22e7809c621c463a3bee308efd7d19e9fdc
manuel@AIO.universidad <manuel@masiasweb.com>
2024-10-08 11:13:07 +0200

    Dos ejemplos...

---
+++ entregas/masiasManuel/src/Ejemplo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Ejemplo {
+    public static void main(String[] args) {
+
+        final int MAXIMO = 10;
+        final int MINIMO = 1;
+
+        int numeroAleatorio = (int) (Math.random() * (MAXIMO - MINIMO)) + MINIMO;
+
+        System.out.println(numeroAleatorio);
+
+        int entrada = new Scanner(System.in).nextInt();
+
+        String respuesta = numeroAleatorio == entrada ? "Adivinó" : "No adivinó";
+
+        System.out.println(respuesta);
+        entrada.close();
+    }
+}
