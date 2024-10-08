56dafff726803d57f216808467b5616200fadc22
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2024-10-09 00:10:39 +0200

    Piedra Papel Y Tijeras, corección array y variables

--- entregas/navasNicolas/src/PiedraPapelTijera.java
+++ entregas/navasNicolas/src/PiedraPapelTijera.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
-public class PiedraPapelTijera {
+class PiedraPapelTijera {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in); 
         
-        String[] cpu_options = { "piedra", "papel", "tijera" };
-        int user_selection;
+        int eleccionUsuario;
 
         System.out.print("Indique su selección [1=Piedra, 2=Papel, 3=Tijera]: ");
-        user_selection = scanner.nextInt(); 
+        eleccionUsuario = scanner.nextInt(); 
 
-        int random_pick = (int) (Math.random() * 3); 
-        String cpu_selection = cpu_options[random_pick];
+        int eleccionAleatoria = (int) (Math.random() * 3); 
 
-        String resultado = (user_selection == 1 && cpu_selection.equals("tijera")) ? "Ganaste! La piedra aplasta la tijera." :
-                        (user_selection == 2 && cpu_selection.equals("piedra")) ? "Ganaste! El papel cubre la piedra." :
-                        (user_selection == 3 && cpu_selection.equals("papel")) ? "Ganaste! La tijera corta el papel." :
-                        (user_selection == 1 && cpu_selection.equals("papel")) ? "Perdiste! El papel cubre la piedra." :
-                        (user_selection == 2 && cpu_selection.equals("tijera")) ? "Perdiste! La tijera corta el papel." :
-                        (user_selection == 3 && cpu_selection.equals("piedra")) ? "Perdiste! La piedra aplasta la tijera." :
+        String eleccionOrdenador = (eleccionAleatoria == 0) ? "piedra" : 
+                               (eleccionAleatoria == 1) ? "papel" : 
+                               "tijera";
+
+        String resultado = (eleccionUsuario == 1 && eleccionOrdenador.equals("tijera")) ? "Ganaste!" :
+                        (eleccionUsuario == 2 && eleccionOrdenador.equals("piedra")) ? "Ganaste!" :
+                        (eleccionUsuario == 3 && eleccionOrdenador.equals("papel")) ? "Ganaste!" :
+                        (eleccionUsuario == 1 && eleccionOrdenador.equals("papel")) ? "Perdiste!" :
+                        (eleccionUsuario == 2 && eleccionOrdenador.equals("tijera")) ? "Perdiste!" :
+                        (eleccionUsuario == 3 && eleccionOrdenador.equals("piedra")) ? "Perdiste!" :
                         "Empate!";
 
-        System.out.println("La computadora eligió: " + cpu_selection);
+        System.out.println("La computadora eligió: " + eleccionOrdenador);
         System.out.println(resultado);
     }
 }
