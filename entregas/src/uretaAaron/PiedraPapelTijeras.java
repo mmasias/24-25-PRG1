c04259089f3cc483404df5930d24155166e93d3e
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2024-10-08 16:20:27 -0500

    Update PiedraPapelTijeras.java

--- entregas/src/uretaAaron/PiedraPapelTijeras.java
+++ entregas/src/uretaAaron/PiedraPapelTijeras.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
-public class PiedraPapelTijeras {
+class PiedraPapelTijeras {
     public static void main(String[] args) {
-        String[] opciones = {"Piedra", "Papel", "Tijeras"};
-        Scanner sc = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
+        final int MAXIMO = 3;
+        final int MINIMO = 0; 
 
-        System.out.print("Tú eliges: 0 = Piedra, 1 = Papel, 2 = Tijeras: ");
-        int usuario = sc.nextInt();
+        String[] opciones = {"Piedra", "Papel", "Tijeras"}; 
 
-        int computadora = (usuario + 1) % 3; 
+        System.out.println("Elige: 0= Piedra, 1= Papel, 2= Tijeras");
+        int eleccionDeUsuario = entrada.nextInt();
+        
+        int aleatorio = (int) (Math.random() * MAXIMO); 
+    
+        String resultado = (eleccionDeUsuario == aleatorio) ? "Empate" :
+            (eleccionDeUsuario == 0 && aleatorio == 2) || 
+            (eleccionDeUsuario == 1 && aleatorio == 0) ||
+            (eleccionDeUsuario == 2 && aleatorio == 1) ? "Ganaste" : "Perdiste";
 
-        System.out.println("Tú: " + opciones[usuario] + " | Computadora: " + opciones[computadora]);
-
-        String resultado = (usuario == computadora) ? "Empate" :
-        (usuario == 0 && computadora == 2) || (usuario == 1 && computadora == 0) || (usuario == 2 && computadora == 1) ? "Ganaste" : "Perdiste";
+        System.out.println("Tu elección: " + opciones[eleccionDeUsuario]);
+        System.out.println("Elección del sistema: " + opciones[aleatorio]);
         System.out.println(resultado);
-        sc.close();
+
+        entrada.close();
     }
 }
