12d6abc16d101bd321bcb3d8b54b76604ae4650b
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2024-10-08 22:24:44 +0200

    JuegoTerminado

--- entregas/figueroaMarco/src/PiedraPapelTijera.java
+++ entregas/figueroaMarco/src/PiedraPapelTijera.java
@@ -4,18 +4,27 @@ public class PiedraPapelTijera {
         
         System.out.println("Juega piedra papel o tijera");
         System.out.println("Como jugar? Ingresa T para Tijera, P para Piedra y Pa para Papel");
-        Scanner InputUsuario = new Scanner(System.in);
+        Scanner ScannerInput = new Scanner(System.in);
         
         final String Tijera = "T";
         final String Piedra = "P";
         final String Papel = "Pa";
         int numeroOrdernador;
         String comparador;
-
+        String inputUsuario;
+        String resultado;
+        
         numeroOrdernador = (int) (Math.random() * (3) + 1);
-        System.out.println(numeroOrdernador);
-        comparador = numeroOrdernador == 1 ? Tijera : numeroOrdernador == 2 ?  Piedra : numeroOrdernador == 3 ? Papel : "Numero no establecido";
-        System.out.println(comparador);
+        comparador = numeroOrdernador == 1 ? Tijera : numeroOrdernador == 2 ?  Piedra : numeroOrdernador == 3 
+        ? Papel : "Numero no establecido";
+        inputUsuario = ScannerInput.nextString();
+        resultado = comparador == inputUsuario ? "Empataron" : (inputUsuario == "T" && comparador == "Pa") ? 
+        "Ganaste" : (inputUsuario == "T" && comparador == "P") ? "Perdiste" : 
+        (inputUsuario == "P" && comparador == "T") ? "Ganaste" : (inputUsuario == "P" && comparador == "Pa")
+         ? "Perdiste" : (inputUsuario == "Pa" && comparador == "T") ? "Perdiste" : (inputUsuario == "Pa" && comparador == "P")
+         ? "Ganaste" : "Secuencia no establecida";
+         
+         System.out.println(resultado);
     }
     
 }
