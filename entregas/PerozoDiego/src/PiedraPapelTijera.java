c4d23854dd6ff5e4819cdb96c0634cf2ab16308f
DPerozo22 <diego.perozo@alumnos.uneatlantico.es>
2024-10-08 23:31:25 +0200

    Reto-002

--- entregas/PerozoDiego/src/PiedraPapelTijera.java
+++ entregas/PerozoDiego/src/PiedraPapelTijera.java
@@ -7,27 +7,27 @@ public class PiedraPapelTijera {
 
         int EleccionJugador;
         int aleatorio;
-        int Comparacion;
-        int Final;
+        String resultado;
 
-        aleatorio = (int) (Math.random() * (3 - 1));
+        aleatorio = (int) (Math.random() * 3) + 1;
 
         System.out.println("Bienvenido a Piedra, Papel o Tijera");
         System.out.println("Para jugar tienes que jugar de la siguiente manera: 1 = Piedra, 2 = Papel y 3 = Tijera");
-        System.out.print("Por favor introduce tu opcion favorita (1, 2 o 3): ");
+        System.out.println("Por favor introduce tu opción favorita (1, 2 o 3): ");
         EleccionJugador = input.nextInt();
 
         String EleccionJugadorFinal = (EleccionJugador == 1) ? "Piedra" : (EleccionJugador == 2) ? "Papel" : "Tijera";
-
         String EleccionComputadora = (aleatorio == 1) ? "Piedra" : (aleatorio == 2) ? "Papel" : "Tijera";
 
         System.out.println("Tu elección: " + EleccionJugadorFinal);
         System.out.println("Elección de la computadora: " + EleccionComputadora);
 
-        Comparacion = (EleccionJugador == aleatorio) ? 1 : 2;
+        resultado = (EleccionJugador == aleatorio) ? "¡Empate!" : 
+                    (EleccionJugador == 1 && aleatorio == 3) || (EleccionJugador == 2 && aleatorio == 1) || (EleccionJugador == 3 && aleatorio == 2) 
+                    ? "¡GANASTE!" 
+                    : "¡PERDISTE!";
 
-        Final = (Comparacion == 1) ? "GANASTE PAPU" : (Comparacion == 2) ? "PERDISTE CAUSA" : "Empate manito";
-
+        System.out.println(resultado);
         input.close();
     }
 }
