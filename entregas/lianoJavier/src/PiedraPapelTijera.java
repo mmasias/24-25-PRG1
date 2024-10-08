08e9b4ab1b56cb039242e3da8535ca342d9b04ea
Midas@Tower <symidas@proton.me>
2024-10-08 18:23:47 +0200

    Juego v1.0

--- entregas/lianoJavier/src/PiedraPapelTijera.java
+++ entregas/lianoJavier/src/PiedraPapelTijera.java
@@ -3,85 +3,97 @@ import java.util.Scanner;
 
 class PiedraPapelTijera {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+                Scanner input = new Scanner(System.in);
 
-        int jugadaDelRobotRaw;
-        jugadaDelRobotRaw = (int)(Math.random() * 3) + 1;
+                int jugadaDelRobotRaw;
+                // jugadaDelRobotRaw = 1; // prueba1
+                jugadaDelRobotRaw = (int) (Math.random() * 3) + 1;
 
-        boolean robotSeleccionoPiedra;
-        boolean robotSeleccionoPapel;
-        boolean robotSeleccionoTijeras;
+                boolean robotSeleccionoPiedra;
+                boolean robotSeleccionoPapel;
+                boolean robotSeleccionoTijeras;
 
-        robotSeleccionoPiedra = jugadaDelRobotRaw == 1;
-        robotSeleccionoPapel = jugadaDelRobotRaw == 2;
-        robotSeleccionoTijeras = jugadaDelRobotRaw == 3;
+                robotSeleccionoPiedra = (jugadaDelRobotRaw == 1);
+                robotSeleccionoPapel = (jugadaDelRobotRaw == 2);
+                robotSeleccionoTijeras = (jugadaDelRobotRaw == 3);
 
-        System.out.println("=========================================");
-        System.out.println("Bienvenido al juego PIEDRA PAPEL O TIJERA");
-        System.out.println("=========================================");
-        System.out.println("Para Jugar debe seleccionar una de las siguientes opciones");
-        System.out.println("");
-        System.out.println("      /| ________________");
-        System.out.println("O|===|* >________________>");
-        System.out.println("      \\|");
-        System.out.println("¿Con qué arma desea blandir esta pelea?");
-        System.out.println("1: piedra");
-        System.out.println("2: papel");
-        System.out.println("3: tijeras");
-        System.out.println("");
-
-        int jugadaDelUsuarioRaw;
-
-        jugadaDelUsuarioRaw = input.nextInt();
-
-        boolean jugadorSeleccionoPiedra;
-        boolean jugadorSeleccionoPapel;
-        boolean jugadorSeleccionoTijeras;
-
-        jugadorSeleccionoPiedra = jugadaDelUsuarioRaw == 1;
-        jugadorSeleccionoPapel = jugadaDelUsuarioRaw == 2;
-        jugadorSeleccionoTijeras = jugadaDelUsuarioRaw == 3;
-
-        String resultado;
-
-        resultado
-                = jugadaDelUsuarioRaw == jugadaDelRobotRaw ? "¡EMPATE!" 
-                : (jugadorSeleccionoPiedra && robotSeleccionoTijeras)
-                    || (jugadorSeleccionoPapel && robotSeleccionoPiedra)
-                    || (jugadorSeleccionoTijeras &&  robotSeleccionoPapel) ? "¡Has ganado!" 
-                :"¡Has perdido!";
+                System.out.println("=========================================");
+                System.out.println("Bienvenido al juego PIEDRA PAPEL O TIJERA");
+                System.out.println("=========================================");
 
 
-        String jugadaDelRobot;
+                System.out.println("Para Jugar debe seleccionar una de las siguientes opciones");
+                System.out.println("");
+                System.out.println("      /| ________________");
+                System.out.println("O|===|* >________________>");
+                System.out.println("      \\|");
 
-        jugadaDelRobot 
-            = robotSeleccionoPapel ? "Robotin ha sacado papel."
-            : robotSeleccionoTijeras ? "Robotin ha sacado tijeras."
-            : robotSeleccionoPiedra ? "Robotin ha sacado piedra." 
-            : "Error";
+                
+                System.out.println("¿Con qué arma desea blandir esta pelea?");
+                System.out.println("1: piedra");
+                System.out.println("2: papel");
+                System.out.println("3: tijeras");
+                System.out.println("");
 
-        boolean usuarioSeleccionoPapel;
-        boolean usuarioSeleccionoTijeras;
-        boolean usuarioSeleccionoPiedra;
+                int jugadaDelUsuarioRaw;
 
-        usuarioSeleccionoPapel = jugadaDelUsuarioRaw == 1;
-        usuarioSeleccionoTijeras = jugadaDelUsuarioRaw == 2;
-        usuarioSeleccionoPiedra = jugadaDelUsuarioRaw == 3;
+                System.out.println("Por favor elija: ");
+                jugadaDelUsuarioRaw = input.nextInt();
 
-        String jugadaDelUsuario;
+                boolean usuarioSeleccionoPapel;
+                boolean usuarioSeleccionoTijeras;
+                boolean usuarioSeleccionoPiedra;
 
-        jugadaDelUsuario 
-            = usuarioSeleccionoPapel ? "El usuario ha sacado papel."
-            : usuarioSeleccionoTijeras ? "El usuario ha sacado tijeras."
-            : usuarioSeleccionoPiedra ? "El usuario ha sacado piedra." 
-            : "Error";
+                usuarioSeleccionoPiedra = jugadaDelUsuarioRaw == 1;
+                usuarioSeleccionoPapel = jugadaDelUsuarioRaw == 2;
+                usuarioSeleccionoTijeras = jugadaDelUsuarioRaw == 3;
 
-        System.out.println(jugadaDelRobot);
-        System.out.println(jugadaDelUsuario);
-        System.out.println(resultado);
+                String jugadaDelUsuario;
 
-        input.close();
-    }
+                jugadaDelUsuario = usuarioSeleccionoPapel ? "Has escogido papel."
+                                : usuarioSeleccionoTijeras ? "Has escogido tijeras."
+                                                : usuarioSeleccionoPiedra ? "Has escogido piedra."
+                                                                : "Error: Elige un número entre 1 y 3.";
+
+                boolean jugadorSeleccionoPiedra;
+                boolean jugadorSeleccionoPapel;
+                boolean jugadorSeleccionoTijeras;
+
+                jugadorSeleccionoPiedra = jugadaDelUsuarioRaw == 1;
+                jugadorSeleccionoPapel = jugadaDelUsuarioRaw == 2;
+                jugadorSeleccionoTijeras = jugadaDelUsuarioRaw == 3;
+
+                String resultado;
+
+                resultado = (jugadaDelUsuarioRaw == jugadaDelRobotRaw) ? "¡EMPATE!"
+                                : (jugadorSeleccionoPiedra && robotSeleccionoTijeras)
+                                                || (jugadorSeleccionoPapel && robotSeleccionoPiedra)
+                                                || (jugadorSeleccionoTijeras && robotSeleccionoPapel)
+                                                                ? "\u001B[32m ¡Has ganado!"
+                                                                : (jugadorSeleccionoTijeras && robotSeleccionoPiedra)
+                                                                                || (jugadorSeleccionoPiedra
+                                                                                                && robotSeleccionoPapel)
+                                                                                || (jugadorSeleccionoPapel
+                                                                                                && robotSeleccionoTijeras)
+                                                                                                                ? "\u001B[31m ¡Has perdido!"
+                                                                                                                : "Error: No has escojido una jugada valida";
+
+                String jugadaDelRobot;
+
+                jugadaDelRobot = robotSeleccionoPapel ? "Robotin ha sacado papel."
+                                : robotSeleccionoTijeras ? "Robotin ha sacado tijeras."
+                                                : "Robotin ha sacado piedra.";
+
+                System.out.println("");
+                System.out.println("");
+                System.out.println(jugadaDelRobot);
+                System.out.println("");
+                System.out.println(jugadaDelUsuario);
+                System.out.println("");
+                System.out.println(resultado);
+
+                input.close();
+        }
 }
