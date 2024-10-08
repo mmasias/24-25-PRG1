a91cc06f358f817467bc610dc49d409c9a333747
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2024-10-08 23:49:25 +0200

    Update PiedraPapelTijeras.java

--- entregas/uretaAaron/src/PiedraPapelTijeras.java
+++ entregas/uretaAaron/src/PiedraPapelTijeras.java
@@ -3,23 +3,26 @@ import java.util.Scanner;
 class PiedraPapelTijeras {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        final int MAXIMO = 3;
-        final int MINIMO = 0; 
 
-        String[] opciones = {"Piedra", "Papel", "Tijeras"}; 
-
-        System.out.println("Elige: 0= Piedra, 1= Papel, 2= Tijeras");
-        int eleccionDeUsuario = entrada.nextInt();
+        System.out.println("Opciones:");
+        System.out.println("0: Piedra");
+        System.out.println("1: Papel");
+        System.out.println("2: Tijeras");
         
-        int aleatorio = (int) (Math.random() * MAXIMO); 
-    
-        String resultado = (eleccionDeUsuario == aleatorio) ? "Empate" :
-            (eleccionDeUsuario == 0 && aleatorio == 2) || 
-            (eleccionDeUsuario == 1 && aleatorio == 0) ||
-            (eleccionDeUsuario == 2 && aleatorio == 1) ? "Ganaste" : "Perdiste";
+        int eleccionDeUsuario = entrada.nextInt();
+        int aleatorio = (int) (Math.random() * 3); 
 
-        System.out.println("Tu elección: " + opciones[eleccionDeUsuario]);
-        System.out.println("Elección del sistema: " + opciones[aleatorio]);
+        String eleccionUsuario = (eleccionDeUsuario == 0) ? "Piedra" : (eleccionDeUsuario == 1) ? "Papel" : "Tijeras";
+        String eleccionComputadora = (aleatorio == 0) ? "Piedra" : (aleatorio == 1) ? "Papel" : "Tijeras";
+
+        System.out.println("Tu elección: " + eleccionUsuario);
+        System.out.println("Elección del sistema: " + eleccionComputadora);
+        
+        String resultado = (eleccionDeUsuario == aleatorio) ? "¡Es un empate!" :
+                           (eleccionDeUsuario == 0 && aleatorio == 2) || 
+                           (eleccionDeUsuario == 1 && aleatorio == 0) || 
+                           (eleccionDeUsuario == 2 && aleatorio == 1) ? "¡Ganaste!" : "¡Perdiste!";
+
         System.out.println(resultado);
 
         entrada.close();
