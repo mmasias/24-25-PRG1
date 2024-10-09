f6e40d83ed0d1e8d3d5f968ace661106ffcf1614
sergiopuente67 <sergio.puente@alumnos.uneatlantico.es>
2024-10-09 18:03:09 +0200

    Captura de la jugada del usuario

--- entregas/Clase9_10.java
+++ entregas/Clase9_10.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Clase9_10 {
     public static void main(String[] args) {
         final int PIEDRA=0;
         final int PAPEL=1;
         final int Tijera=2;
+        int jugadaPersona=0;
 
-        System.out.println("Introduzca piedra papel o tijera");
+        Scanner scanner= new Scanner(System.in);
+        do { 
+             System.out.println("Introduzca 0-piedra 1-papel o 2-tijera");
+            jugadaPersona=scanner.nextInt();
+            if(jugadaPersona<0 || jugadaPersona>2){
+                System.out.println("No es un ajugada válida");
+            }
+        } while (jugadaPersona<0 || jugadaPersona>2);
+       
+      
+
+
 
 
 
