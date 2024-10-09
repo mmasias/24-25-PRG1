bc381e1a646c648e947b3356c0d96ea839ee31cd
JoseLuis.delaAsuncion <jose.delaasuncion@alumnos.uneatlantico.es>
2024-10-09 16:39:11 +0200

    Entrega 09-10

--- entregas/delaasuncionJose/src/RockPaperScissors.java
+++ entregas/delaasuncionJose/src/RockPaperScissors.java
@@ -13,59 +13,81 @@ class RockPaperScissors {
         System.out.println("|       1.  Rock       *     - Crush those scissors!      *          |");
         System.out.println("|       2.  Paper            - Wrap up that rock!                    |");
         System.out.println("|   *   3.  Scissors         - Snip away that paper!            *    |");
-        System.out.println("|                                                                    |");
+        System.out.println("|                                      *                             |");
         System.out.println("+--------------------------------------------------------------------+");
-        System.out.print("|  > ");
 
         
         Scanner scanner = new Scanner(System.in);
         
         boolean error;
-        int userGuess = scanner.nextInt();
+        int userGuess;
         int machineGuess;
         int result;
+        int userWins=0;
+        int userLost=0;
 
         String userWord;
         String machineWord;
         String veredict;
+        String score;
 
 
         final String ERROR_VEREDICT = "|  > Please input a number within the range of 1 to 3                |";
+        final String SPACER = "|                                                                    |";
+        final String USER_WIN_VEREDICT = "|  > The user has won                                                |";
+        final String MACHINE_WIN_VEREDICT = "|  > The machine has won                                             |";
+        final String DRAW_VEREDICT = "|  > The game is tied                                                |";
 
         final int ROCK = 1;
         final int PAPER = 2;
         final int MINIMUM = 1;
         final int MAXIMUM = 3;
 
+        for (int turn = 1; turn <= 3; turn++) {
+            System.out.println(SPACER);
+            System.out.println("|  > Game " + turn + " has begun                                                |");
+            
+            do {
+                System.out.print("|  > ");
+                userGuess = scanner.nextInt();
+                error = userGuess > MAXIMUM || userGuess < MINIMUM;
+                System.out.println( error ? ERROR_VEREDICT : SPACER);
+            } while (error == true);
 
-        error = userGuess > MAXIMUM || userGuess < MINIMUM;
-        machineGuess = (int)(Math.random()*(MAXIMUM - MINIMUM + 1) + MINIMUM);
+            machineGuess = (int)(Math.random()*(MAXIMUM - MINIMUM + 1) + MINIMUM);
 
-        userWord = error ? ERROR_VEREDICT
-        : userGuess == ROCK ? "|  > The user has chosen ROCK                                        |"
-        : userGuess == PAPER ? "|  > The user has chosen PAPER                                       |"
-        : "|  > The user has chosen SCISSORS                                    |";
+            userWord = userGuess == ROCK ? "|  > The user has chosen ROCK                                        |"
+            : userGuess == PAPER ? "|  > The user has chosen PAPER                                       |"
+            : "|  > The user has chosen SCISSORS                                    |";
 
-        machineWord = machineGuess == ROCK ? "|  > Machine has chosen ROCK                                         |"
-        : machineGuess == PAPER ? "|  > Machine has chosen PAPER                                        |"
-        : "|  > Machine has chosen SCISSORS                                     |";
+            machineWord = machineGuess == ROCK ? "|  > Machine has chosen ROCK                                         |"
+            : machineGuess == PAPER ? "|  > Machine has chosen PAPER                                        |"
+            : "|  > Machine has chosen SCISSORS                                     |";
 
-        result = (userGuess-machineGuess+3)%3;
+            result = (userGuess-machineGuess+3)%3;
 
-        veredict = error ? "|  > The game has concluded with no winner due to user failure       |"
-        : result == 0 ? "|  > El juego queda en empate                                        |"
-        :result == 1 ? "|  > The user has won                                                |" 
-        : "|  > The machine has won                                             |";
+            veredict = DRAW_VEREDICT;
 
+            if (result == 2) {
+                veredict = MACHINE_WIN_VEREDICT;
+                userLost++;
+            } else if (result == 1) {
+                veredict = USER_WIN_VEREDICT;
+                userWins++;
+            }
 
-        System.out.println("|                                                                    |");
-        System.out.println(userWord);
-        System.out.println(machineWord);
-        System.out.println("|                                                                    |");
-        System.out.println(veredict);
-        System.out.println("|                                                                    |");
-        System.out.println("+--------------------------------------------------------------------+");
+            score = "|  > The score is:     Wins - " + userWins + "       Loses - " + userLost + "       Draws - " + (turn - userLost - userWins) + "      |";
 
+            System.out.println(SPACER);
+            System.out.println(userWord);
+            System.out.println(machineWord);
+            System.out.println(SPACER);
+            System.out.println(veredict);
+            System.out.println(SPACER);
+            System.out.println(score);
+            System.out.println(SPACER);
+            System.out.println("+--------------------------------------------------------------------+");
+        }
 
         scanner.close();
     }
