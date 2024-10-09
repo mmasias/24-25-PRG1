73aae409db440899ca392f0b0f09d837db19dc81
Bienvenido Doalto Cavada <bienvenido.doalto@unealum.local>
2024-10-09 13:53:44 +0200

    PiedraPapelTijera Mejorado

--- entregas/doaltoBienvenido/src/PiedraPapelTijera.java
+++ entregas/doaltoBienvenido/src/PiedraPapelTijera.java
@@ -9,6 +9,14 @@ class PiedraPapelTijera{
         final String MENSAJE_GANAR = "¡Ganaste!";
         final String MENSAJE_PERDER = "¡Perdiste!";
 
+        final int PIEDRA = 1;
+        final int PAPEL = 2;
+        final int TIJERA = 3;
+
+        final int MAXIMO = 3;
+        final int MINIMO = 1;
+
+
         Scanner scanner = new Scanner(System.in);
         int eleccionUsuario;
         int eleccionOrdenador;
@@ -16,19 +24,30 @@ class PiedraPapelTijera{
         System.out.println("PIEDRA, PAPEL O TIJERA");
         System.out.println("======================");
 
-        System.out.println("Elige: 1 para piedra, 2 para papel, 3 para tijera:");
-        eleccionUsuario = scanner.nextInt();
-        eleccionOrdenador = (int)(Math.random()*(3-1)+1);
+        for(int turno=0 ; turno<3 ; turno++){
+            do{
+                System.out.println("Elige: 1 para piedra, 2 para papel, 3 para tijera:");
 
-        String resultado = eleccionUsuario == eleccionOrdenador ? MENSAJE_EMPATE
-                        : eleccionUsuario == 1 && eleccionOrdenador == 2 ? MENSAJE_PERDER
-                        : eleccionUsuario == 2 && eleccionOrdenador == 3 ? MENSAJE_PERDER
-                        : eleccionUsuario == 3 && eleccionOrdenador == 1 ? MENSAJE_PERDER
-                        : MENSAJE_GANAR;
+                eleccionUsuario = scanner.nextInt();
+                eleccionOrdenador = (int)(Math.random()*(MAXIMO-MINIMO+1)+MINIMO);
+
+                if(eleccionUsuario < MINIMO || eleccionUsuario > MAXIMO)
+                {
+                    System.out.println("¡ERROR!");
+                }
+
+            }while(!(eleccionUsuario >= MINIMO && eleccionUsuario <=MAXIMO));
+
+            
+            String resultado = eleccionUsuario == eleccionOrdenador ? MENSAJE_EMPATE
+                        : eleccionUsuario == PIEDRA && eleccionOrdenador == PAPEL 
+                        || eleccionUsuario == PAPEL && eleccionOrdenador == TIJERA 
+                        || eleccionUsuario == TIJERA && eleccionOrdenador == PIEDRA 
+                        ? MENSAJE_PERDER : MENSAJE_GANAR;
                         
-        System.out.println("Tu eleccion es: " + eleccionUsuario);
-        System.out.println("La eleccion del ordenador es: " + eleccionOrdenador);
-        System.out.println(resultado);
-
+            System.out.println("Tu eleccion es: " + eleccionUsuario);
+            System.out.println("La eleccion del ordenador es: " + eleccionOrdenador);
+            System.out.println(resultado);
+        }
     }
 }
