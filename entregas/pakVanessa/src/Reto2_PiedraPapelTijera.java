340adb745ad17985e98365bf9316ff11eaf55544
vanepakk <vanessa.pakcastillo@alumnos.uneatlantico.es>
2024-10-09 13:55:04 +0200

    piedra papel tijera modificado

--- entregas/pakVanessa/src/Reto2_PiedraPapelTijera.java
+++ entregas/pakVanessa/src/Reto2_PiedraPapelTijera.java
@@ -5,6 +5,12 @@ class PiedraPapelTijera {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+     final int PIEDRA = 1;
+     final int PAPEL = 2;
+    final int TIJERA = 3;
+
+
+
         final int MINIMO = 1;
         final int MAXIMO = 2;
 
@@ -13,12 +19,16 @@ class PiedraPapelTijera {
 
         int aleatorio = (int) (Math.random() * (MAXIMO - MINIMO) + MINIMO);
 
-        String resultado = (eleccionDelUsuario == aleatorio) ? "Empate!" :
-                           (eleccionDelUsuario == 1 && aleatorio == 3) ? "Ganaste!" :
-                           (eleccionDelUsuario == 2 && aleatorio == 1) ? "Ganaste!" :
-                           (eleccionDelUsuario == 3 && aleatorio == 2) ? "Ganaste!" :
-                           "Perdiste!";
-        
+        String resultado="PIERDES";
+
+        if (eleccionDelUsuario==aleatorio) {
+            resultado = "EMPATE";
+        }else if ((eleccionDelUsuario == PIEDRA && eleccionDelUsuario == TIJERA || 
+                    eleccionDelUsuario == PAPEL && aleatorio == PIEDRA ||
+                    eleccionDelUsuario == TIJERA && aleatorio == PAPEL)){
+                resultado = "GANAS";
+        }
+
         System.out.println("Tu eleccion: " + eleccionDelUsuario);
         System.out.println("Eleccion de la maquina: " + aleatorio);
         System.out.println(resultado);
