83fe2489ae961f134ed157f0619e966c69d3e64f
DPerozo22 <diego.perozo@alumnos.uneatlantico.es>
2024-10-09 13:58:35 +0200

    Clase commit

--- entregas/PerozoDiego/src/PiedraPapelTijera.java
+++ entregas/PerozoDiego/src/PiedraPapelTijera.java
@@ -2,32 +2,51 @@ import java.util.Scanner;
 
 public class PiedraPapelTijera {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
+        int continuar;
 
-        int EleccionJugador;
-        int aleatorio;
-        String resultado;
+        do {
+            int EleccionJugador;
+            int aleatorio;
+            String resultado;
 
-        aleatorio = (int) (Math.random() * 3) + 1;
+            aleatorio = (int) (Math.random() * 3) + 1;
 
-        System.out.println("Bienvenido a Piedra, Papel o Tijera");
-        System.out.println("Para jugar tienes que jugar de la siguiente manera: 1 = Piedra, 2 = Papel y 3 = Tijera");
-        System.out.println("Por favor introduce tu opción favorita (1, 2 o 3): ");
-        EleccionJugador = input.nextInt();
+            System.out.println("Bienvenido a Piedra, Papel o Tijera");
+            System.out.println("Para jugar tienes que jugar de la siguiente manera: 1 = Piedra, 2 = Papel y 3 = Tijera");
 
-        String EleccionJugadorFinal = (EleccionJugador == 1) ? "Piedra" : (EleccionJugador == 2) ? "Papel" : "Tijera";
-        String EleccionComputadora = (aleatorio == 1) ? "Piedra" : (aleatorio == 2) ? "Papel" : "Tijera";
+            while (true) {
+                if (input.hasNextInt()) {
+                    EleccionJugador = input.nextInt();
 
-        System.out.println("Tu elección: " + EleccionJugadorFinal);
-        System.out.println("Elección de la computadora: " + EleccionComputadora);
+                    if (EleccionJugador < 1 || EleccionJugador > 3) {
+                        System.out.println("Error: Debes introducir un número entre 1 y 3.");
+                    } else {
+                        break;
+                    }
+                 } else {
+                    System.out.println("Error: Debes introducir un número entero.");
+                    input.next();
+                }
+            }
 
-        resultado = (EleccionJugador == aleatorio) ? "¡Empate!" : 
-                    (EleccionJugador == 1 && aleatorio == 3) || (EleccionJugador == 2 && aleatorio == 1) || (EleccionJugador == 3 && aleatorio == 2) 
-                    ? "¡GANASTE!" 
-                    : "¡PERDISTE!";
+            String EleccionJugadorFinal = (EleccionJugador == 1) ? "Piedra" : (EleccionJugador == 2) ? "Papel" : "Tijera";
+            String EleccionComputadora = (aleatorio == 1) ? "Piedra" : (aleatorio == 2) ? "Papel" : "Tijera";
 
-        System.out.println(resultado);
+            System.out.println("Tu elección: " + EleccionJugadorFinal);
+            System.out.println("Elección de la computadora: " + EleccionComputadora);
+
+            resultado = (EleccionJugador == aleatorio) ? "¡Empate!" : 
+                        (EleccionJugador == 1 && aleatorio == 3) || (EleccionJugador == 2 && aleatorio == 1) || (EleccionJugador == 3 && aleatorio == 2) 
+                        ? "¡GANASTE!" 
+                        : "¡PERDISTE!";
+
+            System.out.println(resultado);
+
+            System.out.print("Quieres jugar otra vez? (1 = Si / 2 = No): ");
+            continuar = input.nextInt();
+        } while (continuar == 1);
+        System.out.println("¡Gracias por jugar!");
         input.close();
     }
 }
