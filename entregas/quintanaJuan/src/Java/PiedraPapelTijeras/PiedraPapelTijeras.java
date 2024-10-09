c931443f0c623a36fe052a9f7753a9f95e1e2e39
juanquintanaing <juan.quintana@alumnos.uneatlantico.es>
2024-10-09 14:04:20 +0200

    asistencia

--- entregas/quintanaJuan/src/Java/PiedraPapelTijeras/PiedraPapelTijeras.java
+++ entregas/quintanaJuan/src/Java/PiedraPapelTijeras/PiedraPapelTijeras.java
@@ -1,28 +1,12 @@
-import java.util.Scanner;
-
 class PiedraPapelTijeras {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
 
         final int MAXIMO = 3;
         final int MINIMO = 1;
-
-        System.out.println("Elige: 1= Piedra, 2= Papel, 3= Tijeras");
-        int eleccionDeUsuario = entrada.nextInt();
-
-        int aleatorio = (int)(Math.random() * (MAXIMO - MINIMO) + MINIMO);
-
-        String resultado = (eleccionDeUsuario == aleatorio) ? "Empate" :
-                           (eleccionDeUsuario == 1 && aleatorio == 3) ||
-                           (eleccionDeUsuario == 2 && aleatorio == 1) ||
-                           (eleccionDeUsuario == 3 && aleatorio == 2) ? "Ganaste" :
-                           "Perdiste";
-
-        System.out.println("Tu eleccion: " + eleccionDeUsuario);
-        System.out.println("Elección de la maquina: " + aleatorio);
-        System.out.println(resultado);
-
-        entrada.close();
+	
+	for (int i=0;i<10;i++){
+		System.out.println("XXX");
+	}
 
     }
 }
