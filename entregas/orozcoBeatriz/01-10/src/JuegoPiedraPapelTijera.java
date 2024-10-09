9fbf039e698ce4b29cc8e1e54ba9d0e1ba255654
Bea <beatrizorozcodelgado@gmail.com>
2024-10-09 13:13:48 +0200

    Creo el bucle do while, para que si insertan un valor no válido, se vuelva a repetir

--- entregas/orozcoBeatriz/01-10/src/JuegoPiedraPapelTijera.java
+++ entregas/orozcoBeatriz/01-10/src/JuegoPiedraPapelTijera.java
@@ -8,6 +8,8 @@ public class JuegoPiedraPapelTijera {
         final int TIJERA = 3;
         final int LIMITE_INFERIOR = 0;
         final int LIMITE_SUPERIOR = 4;
+        final int MAX = 3;
+        final int MIN = 1;
 
         final String PIEDRA_DESCRIPTOR = "Piedra";
         final String  PAPEL_DESCRIPTOR = "Papel";
@@ -18,25 +20,28 @@ public class JuegoPiedraPapelTijera {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("¡Juguemos a piedra, papel o tijera!");
-        System.out.println("Elige: 1 para Piedra, 2 para Papel y 3 para Tijera");
-        eleccion = scanner.nextInt();
+        
+        do {
+          System.out.println("Elige: " + PIEDRA + " para Piedra, " + PAPEL + " para Papel y " +  TIJERA + " para Tijera");
+          eleccion  = scanner.nextInt();
+          if (eleccion <= LIMITE_INFERIOR || eleccion >= LIMITE_SUPERIOR) {
+            System.out.println(ERROR);
+          }
+          } while (eleccion <= LIMITE_INFERIOR || eleccion >= LIMITE_SUPERIOR);
+
         String descriptor;
-        descriptor = eleccion <= LIMITE_INFERIOR ? ERROR
-        : eleccion == PIEDRA ? PIEDRA_DESCRIPTOR
+        descriptor = eleccion == PIEDRA ? PIEDRA_DESCRIPTOR
         : eleccion == PAPEL ? PAPEL_DESCRIPTOR
-        : eleccion == TIJERA ?  TIJERA_DESCRIPTOR
-        : eleccion >= LIMITE_SUPERIOR ? ERROR : ERROR;
+        : eleccion == TIJERA ?  TIJERA_DESCRIPTOR : ERROR;
 
         System.out.println("Tu eleccion: " + descriptor);
 
-        int randomNumber = (int)(Math.random() * 3) + 1;
+        int randomNumber = (int)(Math.random() * (MAX - MIN) + 1) + MIN;
         String eleccionRandom;
 
-        eleccionRandom = randomNumber <= LIMITE_INFERIOR ? ERROR
-        : randomNumber == PIEDRA ? PIEDRA_DESCRIPTOR
+        eleccionRandom = randomNumber == PIEDRA ? PIEDRA_DESCRIPTOR
         : randomNumber == PAPEL ? PAPEL_DESCRIPTOR
-        : randomNumber == TIJERA ?  TIJERA_DESCRIPTOR
-        : randomNumber >= LIMITE_SUPERIOR ? ERROR : ERROR;
+        : randomNumber == TIJERA ?  TIJERA_DESCRIPTOR : ERROR;
 
         System.out.println("Elección del ordenador: " + eleccionRandom);
 
@@ -45,8 +50,8 @@ public class JuegoPiedraPapelTijera {
         randomNumber <= LIMITE_INFERIOR || randomNumber >= LIMITE_SUPERIOR) ? "No hay resultado"
         : eleccion == randomNumber ? "Empate"
         : (eleccion == PIEDRA && randomNumber == PAPEL) || (eleccion == PAPEL && randomNumber == TIJERA) || 
-          (eleccion == TIJERA && randomNumber == PIEDRA) ? "Pierdes"
-        : "Ganas";
+          (eleccion == TIJERA && randomNumber == PIEDRA) ? "Pierdes :("
+        : "Ganas!! :D";
 
         System.out.println(resultado);
         scanner.close();
