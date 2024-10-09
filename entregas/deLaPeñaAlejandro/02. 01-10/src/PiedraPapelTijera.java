c229e33113ba31f9926a8a3d979fac11f854d901
ApKy <alejandro.delapena@alumnos.uneatlantico.es>
2024-10-09 10:26:55 +0200

    Corrección Reto002

--- entregas/deLaPeñaAlejandro/02. 01-10/src/PiedraPapelTijera.java
+++ entregas/deLaPeñaAlejandro/02. 01-10/src/PiedraPapelTijera.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class PiedraPapelTijera {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         try(Scanner scanner = new Scanner(System.in)) {
         
         final int PIEDRA = 1;
