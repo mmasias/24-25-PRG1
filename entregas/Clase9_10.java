864ced9e2e8140658b8d11e5aa6edddc4e4be0c9
sergiopuente67 <sergio.puente@alumnos.uneatlantico.es>
2024-10-09 17:45:22 +0200

    Definición de las constantes de piedra papel y tijera

---
+++ entregas/Clase9_10.java
@@ -0,0 +1,12 @@
+public class Clase9_10 {
+    public static void main(String[] args) {
+        final int PIEDRA=0;
+        final int PAPEL=1;
+        final int Tijera=2;
+
+
+        
+    
+    }
+    
+}
