5ed83babea86e69e1849c78429f501a6944bf24e
ikercelaya <iker.celaya@alumnos.uneatlantico.es>
2024-10-09 13:49:30 +0200

    Añado bucles y condicionales vistos en clase

--- entregas/celayaIker/Reto002/PiedraPapelTijera.java
+++ entregas/celayaIker/Reto002/PiedraPapelTijera.java
@@ -9,35 +9,48 @@ class PiedraPapelTijera {
         final int ELEGIR_PIEDRA = 1;
         final int ELEGIR_PAPEL = 2;
         final int ELEGIR_TIJERA = 3;
+        final int MINIMO = 1;
+        final int MAXIMO = 3;
 
-        int numeroOrdenador = (int) (Math.random()*(3-1+1) + 1);
-        int numeroUsuario;
+        for(int turno = 0; turno < 3; turno = turno + 1){
+            int numeroOrdenador = (int) (Math.random()*(MAXIMO-MINIMO + 1) + MINIMO);
+            int numeroUsuario;
 
-        System.out.println("Juguemos a Piedra, Papel o Tijera!");
-        System.out.println("Elige: 1 para Piedra, 2 para Papel, 3 para Tijera");
-        numeroUsuario = scanner.nextInt();
+            System.out.println("Juguemos a Piedra, Papel o Tijera!");
+            do {
+                System.out.println("Elige: 1 para Piedra, 2 para Papel, 3 para Tijera");
+                numeroUsuario = scanner.nextInt();
+            }while (!(numeroUsuario >= MINIMO && numeroUsuario <= MAXIMO));
+            
 
-        String eleccionUsuario;
-        String eleccionOrdenador;
+            String eleccionUsuario;
+            String eleccionOrdenador;
 
-        eleccionUsuario = numeroUsuario == 1 ? "Piedra"
-            : numeroUsuario == 2 ? "Papel"
-            : "Tijera";
+            eleccionUsuario = numeroUsuario == 1 ? "Piedra"
+                : numeroUsuario == 2 ? "Papel"
+                : "Tijera";
 
-        eleccionOrdenador = numeroOrdenador == 1 ? "Piedra"
-            : numeroOrdenador == 2 ? "Papel"
-            : "Tijera";
+            eleccionOrdenador = numeroOrdenador == 1 ? "Piedra"
+                : numeroOrdenador == 2 ? "Papel"
+                : "Tijera";
 
-        System.out.println("Tu elección: " + eleccionUsuario);
-        System.out.println("Elección del ordenador: " + eleccionOrdenador);
+            System.out.println("Tu elección: " + eleccionUsuario);
+            System.out.println("Elección del ordenador: " + eleccionOrdenador);
 
-        String resultado;
-        resultado = numeroOrdenador == numeroUsuario ? "Empate!"
-            : numeroUsuario == ELEGIR_PIEDRA && numeroOrdenador == ELEGIR_TIJERA ||
-                numeroUsuario == ELEGIR_PAPEL && numeroOrdenador == ELEGIR_PIEDRA ||
-                numeroUsuario == ELEGIR_TIJERA && numeroOrdenador == ELEGIR_PAPEL ? "¡Ganaste!" : "Perdiste.";
+            String resultado = "Perdiste!";
 
-        System.out.println(resultado);
+            if (numeroUsuario == numeroOrdenador){
+                resultado = "Empate!";
+            } else if((numeroUsuario == ELEGIR_PIEDRA && numeroOrdenador == ELEGIR_TIJERA ||
+                        numeroUsuario == ELEGIR_PAPEL && numeroOrdenador == ELEGIR_PIEDRA ||
+                        numeroUsuario == ELEGIR_TIJERA && numeroOrdenador == ELEGIR_PAPEL)) {
+                resultado = "Ganaste!";
+            }
+
+
+            System.out.println(resultado);
+        }
+        
         scanner.close();
     }
 }
