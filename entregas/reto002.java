b64ac3cb170288e1fac497c986c0a7a0020a1208
sergiopuente67 <sergio.puente@alumnos.uneatlantico.es>
2024-10-14 17:48:58 +0200

    Creadas jugadas

--- entregas/reto002.java
+++ entregas/reto002.java
@@ -1,3 +1,34 @@
-public class reto002 {
+import java.util.Scanner;
 
+class Reto002 {
+
+    public static void main(String[] args) {
+        final int PIEDRA = 0;
+        final int PAPEL = 1;
+        final int TIJERA = 2;
+
+        int jugadaPersona = 0;
+        int jugadaOrdenador = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Introduzca 0-piedra 1-papel o 2-tijera");
+            jugadaPersona = scanner.nextInt();
+            if (jugadaPersona < 0 || jugadaPersona > 2) {
+                System.out.println("No es un ajugada válida");
+            }
+        } while (jugadaPersona < 0 || jugadaPersona > 2);
+
+        /* 
+        for(int i=0;i<50;i++){
+            jugadaOrdenador=(int)(Math.random()*100)%3;
+            System.out.println(jugadaOrdenador);
+        }
+         */
+        jugadaOrdenador = (int) (Math.random() * 100) % 3;
+        String jugadaPersonaTexto = "";
+        String jugadaOrdenadorTexto = "";
+       
+    }
 }
+
