011400612f38020893568d7c41351588a5257123
Ricardo Rubio Cao <ricardo.rubio@alumnos.uneatlantico.es>
2024-10-15 14:10:38 +0200

    avances de juego caracol

--- entregas/rubioRicardo/src/JuegoCaracol.java
+++ entregas/rubioRicardo/src/JuegoCaracol.java
@@ -7,14 +7,19 @@ public class JuegoCaracol {
         final String POZO_TOPE = "[__]             [__]";
         final String POZO_PARED = "|___________________|";
         final String POZO_AGUA = "|~~~~~~~~~~~~~~~~~~~|";
-        final String POZO_CARACOL = "|______caracol______|";
-
-        int profundidadAgua = 5;
-        int profundidadCaracol = 7;
-
-        System.out.println(POZO_TOPE);
+        final String POZO_CARACOL = "|______¨{___________|";
         
-        for(int i=0 ; i<=PROFUNDIDAD ; i++){
+        final int PROFUNDIDAD_MAXIMA = 20;
+        final int PROFUNDIDAD_MINIMA = 10;
+ 
+        int profundidadAgua = 5;
+        int profundidadCaracol = (int)(Math.random()*PROFUNDIDAD_MAXIMA-PROFUNDIDAD_MINIMA+1)+PROFUNDIDAD_MINIMA;
+        int dia =0;
+        
+        do{
+          System.out.println("DIA" + dia);
+          System.out.println(POZO_TOPE);
+          for(int i=0 ; i<=PROFUNDIDAD ; i++){
             if (profundidadCaracol==i){
                 System.out.println(POZO_CARACOL + i);
             } else if (i > PROFUNDIDAD-profundidadAgua){
@@ -22,8 +27,11 @@ public class JuegoCaracol {
             } else{
                 System.out.println(POZO_PARED + i);
             }
-        }
-        System.out.println(POZO_BASE);
+          }
+          System.out.println(POZO_BASE);
+          profundidadCaracol = profundidadCaracol -2;
+        dia++;
+        }while(profundidadCaracol>0);
 
 
 
