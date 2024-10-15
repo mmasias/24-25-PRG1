f549aa323381a4a5c20dca920298074c019eee2a
ikercelaya <iker.celaya@alumnos.uneatlantico.es>
2024-10-15 13:51:49 +0200

    Caracol dibujo inicial

---
+++ entregas/celayaIker/src/Caracol.java
@@ -0,0 +1,34 @@
+package entregas.celayaIker.src;
+
+class Caracol {
+    
+    public static void main(String[] args) {
+        
+        final int PROFUNDIDAD = 20;
+
+        final String POZO_TOPE = "[__]              [__]";
+        final String POZO_PARED = "  []:. :. :. :. :.[] _ __";
+        final String POZO_BASE = "  [][][][][][][][][]";
+        final String POZO_AGUA = "  []~~~~~~~~~~~~~~[] _ __";
+        final String POZO_CARACOL = "  []    _@)_/’    [] _ __";
+
+        int profundidadAgua = 5;
+        int profundidadCaracol = 10;
+
+        System.out.println(POZO_TOPE);
+
+        for(int i = 0; i <= PROFUNDIDAD; i++) {
+
+            if(profundidadCaracol == i) {
+                System.out.println(POZO_CARACOL + i);
+            } else if(i > PROFUNDIDAD - profundidadAgua) {
+                System.out.println(POZO_AGUA + i);
+            } else {
+                System.out.println(POZO_PARED + i);
+            }
+        }
+        System.out.println(POZO_BASE);
+
+    }
+
+}
