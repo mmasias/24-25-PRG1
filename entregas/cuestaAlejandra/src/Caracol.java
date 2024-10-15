ddf768756ae0ba3c250b2f7f986cce3c1673dc12
Alex Rodríguez <alejandra.cuesta@alumnos.uneatlantico.es>
2024-10-15 14:08:22 +0200

    Caracol

--- entregas/cuestaAlejandra/src/Caracol.java
+++ entregas/cuestaAlejandra/src/Caracol.java
@@ -1,19 +1,26 @@
 class Caracol{
-public static void main(String[] args){
+ public static void main(String[] args){
    
    final int PROFUNDIDAD = 20;
    final String POZO_BASE = "  #################";
-   final String POZO_TOPE = "####             ####";
+   final String POZO_TOPE = "####              ####";
    final String POZO_PARED = "  ##:. :. :. :. :.## _ __";
    final String POZO_AGUA = "  ##--------------## _ __";
-   final String POZO_CARACOL = "  ##   _@)_/'    ## _ __";
+   final String POZO_CARACOL = "  ##   _@)_/'     ## _ __";
+
+   final int PROFUNDIDAD_MAXIMA = 20;
+   final int PROFUNDIDAD_MINIMA = 10;
 
 
-    int profundidadAgua = 19;
-    int profundidadCaracol = 14;
+    int profundidadAgua = 0;
+    int profundidadCaracol = (int)(Math.random()*PROFUNDIDAD_MAXIMA-PROFUNDIDAD_MINIMA+1)+10;
+    int dia = 0;
 
-    System.out.println(POZO_TOPE);
-    for(int i=0; i<=PROFUNDIDAD; i++){
+
+    do {
+
+      System.out.println(POZO_TOPE);
+      for(int i=0; i<=PROFUNDIDAD; i++){
 
         if (profundidadCaracol==i) {
             System.out.println(POZO_CARACOL + i);
@@ -23,11 +30,12 @@ public static void main(String[] args){
            System.out.println(POZO_PARED + i);
         }
 
-    }
-
-
-    System.out.println(POZO_BASE);
-
-}
+      }
+     System.out.println(POZO_BASE);
+     profundiadCaracol - 2;
+     dia++;
+    } while (profundidadCaracol>0);
+    
+ } 
 
 }
