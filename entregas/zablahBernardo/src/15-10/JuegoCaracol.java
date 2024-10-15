cd688c99943d14c5d0357da1ab2b9aea0446f6d4
Bernie Zablah <bernardo.zablah@alumnos.uneatlantico.es>
2024-10-15 14:08:58 +0200

    Avance clase 15 de octubre

--- entregas/zablahBernardo/src/15-10/JuegoCaracol.java
+++ entregas/zablahBernardo/src/15-10/JuegoCaracol.java
@@ -8,24 +8,31 @@ public class JuegoCaracol {
         final String POZO_PARED = "|___________________|";
         final String POZO_AGUA = "|~~~~~~~~~~~~~~~~~~~|";
         final String POZO_CARACOL = "|______caracol______|";
+        final int PROFUNDIDAD_MAXIMA = 20;
+        final int PROFUNDIDAD_MINIMA = 10;
 
         int profundidadAgua = 5;
-        int profundidadCaracol = 7;
+        int profundidadCaracol = (int)(Math.random()*PROFUNDIDAD_MAXIMA-PROFUNDIDAD_MINIMA+1)+PROFUNDIDAD_MINIMA;
+        int dia = 0;
 
-        System.out.println(POZO_TOPE);
+        do {
+            System.out.println("Dia " + dia);
+            System.out.println(POZO_TOPE);
 
-        for(int i=0 ; i<=PROFUNDIDAD ; i++){
-            if (profundidadCaracol==i){
-                System.out.println(POZO_CARACOL + i);
-            } else if (i > PROFUNDIDAD-profundidadAgua){
-                System.out.println(POZO_AGUA + i);
-            } else{
-                System.out.println(POZO_PARED + i);
+            for(int i=0 ; i<=PROFUNDIDAD ; i++){
+                if (profundidadCaracol==i){
+                    System.out.println(POZO_CARACOL + i);
+                } else if (i > PROFUNDIDAD-profundidadAgua){
+                    System.out.println(POZO_AGUA + i);
+                } else {
+                    System.out.println(POZO_PARED + i);
+                }
             }
-        }
-        System.out.println(POZO_BASE);
-
-
+            
+            profundidadCaracol --;
+            dia++;
+            System.out.println(POZO_BASE);
+        } while (profundidadCaracol>0) ;
 
     }
 
