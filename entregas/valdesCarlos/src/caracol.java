3d9be1069691bf5a73d73f59099ae933cdbdddd0
carlos.valdes <carlos.valdes@alumnos.uneatlantico.es>
2024-10-15 17:21:56 +0200

    Arreglo caracol

--- entregas/valdesCarlos/src/caracol.java
+++ entregas/valdesCarlos/src/caracol.java
@@ -3,29 +3,37 @@ public class caracol {
         
         final int PROFUNDIDAD = 20;
         final String POZO_BASE = "###################";
-        final String POZO_TOPE = "##                 ##";
-        final String POZO_PARED = "## :.:.:.:.:.:.";
-        final String POZO_AGUA = "##~~~~~~~~~~~~~##";
-        final String POZO_CARACOL = "   ##        _@_/`       ##";
+        final String POZO_TOPE = "##               ##";
+        final String POZO_PARED = "## :.:.:.:.:.:.:.##";
+        final String POZO_AGUA = "##~~~~~~~~~~~~~~~##";
+        final String POZO_CARACOL = "##      _@_/`    ##";
+        
 
         int profundidadAgua = 10;
         int profundidadCaracol = 16;
         System.out.println(POZO_TOPE);
+        int dia=0;
 
-        for(int i=0; i<=PROFUNDIDAD; i++) {
+        do {
+            System.out.println("DIA" + dia);
+            System.out.println(POZO_TOPE);
+            for(int i=0; i<=PROFUNDIDAD; ++i) {
 
-            if(i>PROFUNDIDAD-profundidadAgua==i) {
-                System.out.println(POZO_AGUA + i);
-            } else if (i>PROFUNDIDAD-profundidadAgua){
-                System.out.println(POZO_AGUA + i);
-            } else {
-                System.out.println(POZO_PARED + i);
+                if(profundidadCaracol==i) {
+                    System.out.println(POZO_CARACOL + i);
+                } else if (i>PROFUNDIDAD-profundidadAgua) {
+                    System.out.println(POZO_AGUA + i);
+                } else {
+                    System.out.println(POZO_PARED + i);
+                }
             }
+            System.out.println(POZO_BASE); 
+            profundidadCaracol = profundidadCaracol -2;
+            dia++;
+        } while (profundidadCaracol>0);    
 
-            }
-            System.out.println(POZO_PARED + i);
+    }
 
-        }
 
 
 }
