2bc74577a823db122e86755aece4f04f3cc9ea88
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2024-10-15 12:14:22 +0000

    15-10

---
+++ entregas/caicedoEsteban/Caracol.java
@@ -0,0 +1,36 @@
+class Caracol {
+    public static void main(String[] args) {
+      System.out.println ("El caracol");
+
+      final int PROFUNDIDAD = 20;
+      final String POZO_BASE= "[][][][][][][][][][][][][]";
+      final String POZO_TOPE= "[][][][]          [][][][]";
+      final String POZO_PARED= "  [][]: .: .: .: .: .[][] _ __";
+      final String POZO_AGUA = "   [][]~~~~~~~~~~~~~~~~~~~~~~~~~~ [][]";
+      final String POZO_CARACOL = "  [][]     _@_/'          [][]  _ __";
+
+      final int PROFUNDIDADMAXIMA= 20;
+      final int PROFUNDIDADMINIMA= 10;
+
+
+      int profundidadAgua = 0;
+      int profundidadCaracol = (int) (Math.random()* PROFUNDIDADMAXIMA-PROFUNDIDADMINIMA+1)+1PROFUNDIDADMINIMA;
+      int dia = 0;
+
+      {
+         System.out.println (POZO_TOPE);
+
+         for  (int i=0; i <=PROFUNDIDAD; i++){
+
+            if (profundidadCaracol){
+
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
