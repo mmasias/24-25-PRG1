ecfc81b91c146478e2e633aba0384853eb2e63c3
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-10-15 14:10:52 +0200

    alpha 0.2

--- entregas/lianoJavier/TrabajoCaracol/src/Caracol.java
+++ entregas/lianoJavier/TrabajoCaracol/src/Caracol.java
@@ -6,8 +6,8 @@ class Caracol {
         int dia = 0;
         int nivelAgua = 0;
 
-        int PROFUNDIDAD_MAXIMA_CARACOL = 20;
-        int PROFUNDIDAD_MINIMA_CARACOL = 10;
+        int PROFUNDIDAD_MAXIMA_CARACOL = 10;
+        int PROFUNDIDAD_MINIMA_CARACOL = 20;
         int posicionCaracol = (int) (Math.random() * PROFUNDIDAD_MAXIMA_CARACOL - PROFUNDIDAD_MINIMA_CARACOL + 1) + PROFUNDIDAD_MINIMA_CARACOL;
 
         final String SUPERFICIE = "[__]            [__]";
@@ -16,29 +16,27 @@ class Caracol {
         final String PAREZ_CON_CARACOL = "  []    _@)/'   []";
         final String BASE = "  [][][][][][][][]";
 
-        do {
-            System.out.println("DÍA: " + dia);
-            System.out.println(SUPERFICIE);
+        System.out.println("DÍA: " + dia);
+        System.out.println(SUPERFICIE);
 
-            final int PROFUNDIDAD = 20;
-            for (int i = 1; i <= PROFUNDIDAD; i++) {
+        final int PROFUNDIDAD = 20;
+        for (int i = 1; i <= PROFUNDIDAD; i++) {
 
-                String Dibujo;
+            String Dibujo;
 
-                Dibujo = i == posicionCaracol ? PAREZ_CON_CARACOL
-                        : i >= (PROFUNDIDAD - nivelAgua + 1) ? PAREZ_CON_AGUA
-                                : PAREZ_VACIA;
+            Dibujo = i == posicionCaracol ? PAREZ_CON_CARACOL
+                    : i >= (PROFUNDIDAD - nivelAgua + 1) ? PAREZ_CON_AGUA
+                            : PAREZ_VACIA;
 
-                String Contrador = "_ __" + i;
+            String Contrador = "_ __" + i;
 
-                System.out.println(Dibujo + Contrador);
-            }
+            System.out.println(Dibujo + Contrador);
+        }
 
-            System.out.println(BASE);
-            System.out.println(posicionCaracol);
+        System.out.println(BASE);
+        System.out.println(posicionCaracol);
 
-            dia++;
-        } while (posicionCaracol > 0);
+        dia++;
 
     }
 }
