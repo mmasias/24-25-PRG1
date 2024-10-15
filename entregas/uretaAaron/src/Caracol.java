cca2ae440e29c183347a1651526e35950f722f52
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2024-10-15 14:06:05 +0200

    Add files via upload

---
+++ entregas/uretaAaron/src/Caracol.java
@@ -0,0 +1,36 @@
+public class Caracol {
+
+    public static void main(String [] args) {
+    
+    final String POZO_BASE = "[][][][][][][][][][] _ __";
+    final String POZO_TOPE = "[  ]              [  ] _ __ ";
+    final String POZO_PARED = "[]:. :. :. :. :.[] _ __ ";
+    final String POZO_AGUA = "~~~~~~~~~~~~ _ __";
+    final String POZO_CARACOL = "[]    _@)_/'    [] _ __";
+
+    final int PROFUNDIDAD_MAXIMA = 20;
+    final int PROFUNDIDAD_MINIMA = 10;
+
+    int PROFUNDIDAD = 20;
+    int profundidadAgua = 0;
+    int profundidadCaracol = (int)(Math.random()*PROFUNDIDAD_MAXIMA-PROFUNDIDAD_MINIMA+1)+PROFUNDIDAD_MINIMA;
+    int dia=0;
+
+    do {
+    System.out.println(POZO_TOPE);
+    for(int i=0; i <= PROFUNDIDAD; i++ ){
+
+        if(profundidadCaracol == i){
+            System.out.println(POZO_CARACOL + i );
+        } else if (i>PROFUNDIDAD-profundidadAgua){
+            System.out.println(POZO_AGUA + i );
+        } else {
+            System.out.println(POZO_PARED + i);
+        }
+      }
+       System.out.println(POZO_BASE);
+       profundidadCaracol--;
+    } while (profundidadCaracol>0);
+
+}
+}
