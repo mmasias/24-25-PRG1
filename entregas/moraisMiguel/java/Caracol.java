6c9690e67e4fb670262bf5ea98d1efb5e60d9962
Miugel Justdance <joao.morais@alumnos.uneatlantico.es>
2024-10-16 17:25:55 +0200

    derrota

--- entregas/moraisMiguel/java/Caracol.java
+++ entregas/moraisMiguel/java/Caracol.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 class Caracol{
     public static void main(String[]args){
 
-        final int MAXIMO = 21;
+        Scanner scanner = new Scanner(System.in);
+
+        final int MAXIMO = 41;
         final int MINIMO = 1;
-        final int DEPTH = 20;
+        final int DEPTH = 40;
         final String BASE = "|_____________________|";
         final String TOP = "|_____           _____|";
         final String WALL = "|_:.:.:.:.:.:.:.:.:.:_|";
@@ -12,12 +14,14 @@ class Caracol{
         final String SNAIL = "|        _@)_/        |";
         final String CAR = "|_____  ô======ô  _____|";
 
+        int day = 0;
         int waterDepth = 5;
         int snailDepth = (int) (Math.random()*(MAXIMO-MINIMO)+MINIMO);
         boolean carChance = (Math.random()<=0.35);
 
 
         System.out.println("el caracol cae " + snailDepth + " metros al inicio");
+        System.out.println("profundidad " + snailDepth + " nivel del agua " + waterDepth + " dia " + day);
         System.out.println(carChance?CAR:TOP);
 
         for(int i=0; i<=DEPTH; i++){
@@ -35,5 +39,23 @@ class Caracol{
 
         }
         System.out.println(BASE);
+
+        do{
+            final int MAX_ASCENSION = 4;
+            final int MIN_ASCENSION = 1;
+            int ascension;
+            ascension = (int) ((Math.random()*(MAX_ASCENSION - MIN_ASCENSION + 1))+ MIN_ASCENSION);
+
+            int ascensionUpdate;
+             ascensionUpdate
+                    = day == 10 && ascension > 3 ? 3
+                            : day == 20 && ascension > 2 ? 2
+                                    : ascension;
+
+            final int MAX_DESCENT = 2;
+            final int MIN_DESCENT = 0;
+            int descent;
+            descent = (int) ((Math.random()*(MAX_DESCENT - MIN_DESCENT + 1)) + MIN_DESCENT);
+    }while(day<50);
     }
 }
