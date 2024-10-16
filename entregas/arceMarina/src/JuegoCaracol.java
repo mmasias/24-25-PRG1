55f314fdfc4990eeba8420a9ff9e51da1af31b92
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2024-10-16 13:59:02 +0200

    Avance Juego Caracol en clase

--- entregas/arceMarina/src/JuegoCaracol.java
+++ entregas/arceMarina/src/JuegoCaracol.java
@@ -8,23 +8,56 @@ public class JuegoCaracol {
         final String POZO_PARED = "|_______|";
         final String POZO_AGUA = "|~~~~~~~|";
         final String POZO_CARACOL = "|___caracol___|";
+        final String COCHE 
 
-        int profundidadAgua = 5;
-        int profundidadCaracol = 7;
+        final int PROFUNDIDAD_MAXIMA = 20;
+        final int PROFUNDIDAD_MINIMA = 10;
 
-        System.out.println(POZO_TOPE);
-        
-        for(int i=0 ; i<=PROFUNDIDAD ; i++){
-            if (profundidadCaracol==i){
-                System.out.println(POZO_CARACOL + i);
-            } else if (i > PROFUNDIDAD-profundidadAgua){
-                System.out.println(POZO_AGUA + i);
+        int profundidadAgua = 0;
+        int profundidadCaracol = (int)(Math.random()*PROFUNDIDAD_MAXIMA-PROFUNDIDAD_MINIMA+1)+PROFUNDIDAD_MINIMA;
+        System.out.println("Al inicio el caracol cae a ["+profundidadCaracol+"] metros");
+        int dia =0;
+        int caracolSube=0
+        int caracolBaja=0
+       
+        do{
+            dia++;
+            caracolSube = (int)(Math.random()*(4-1+1)+1);
+            caracolBaja = (int)(Math.random()*(2-0+1)+0);
+            System.out.println("DIA" + dia);
+            boolean aparcaCoche = Math.random()<=0.35;
+            double probabilidadDeLluvia = Math.random();
+            int aporteAgua = probabilidadDeLluvia <= 0.05 ? 5 :
+                             probabilidadDeLluvia <= 0.1 ? 2 : 0 ;
+            profundidadAgua = profundidadAgua + profundidadAgua ; 
+           
+            profundidadCaracol = profundidadCaracol
+                                    -caracolSube
+                                    +caracolBaja
+                                    +(aparcaCoche ? 2 : 0);
+            System.out.println("Dia["+dia+"] - El caracol subio["+caracolSube+"] - Bajó ["+caracolBaja+"] - Posición ^["+profundidadCaracol+"]");
+            if (aparcaCoche){
+                System.out.println(COCHE);
             } else{
-                System.out.println(POZO_PARED + i);
+                System.out.println(POZO_TOPE);  
             }
-        }
-        System.out.println(POZO_BASE);
+            System.out.println(POZO_TOPE);
+                for(int i=0 ; i<=PROFUNDIDAD ; i++){
+                    if (profundidadCaracol==i){
+                        System.out.println(POZO_CARACOL + i);
+                    } else if (i > PROFUNDIDAD-profundidadAgua){
+                        System.out.println(POZO_AGUA + i);
+                    } else{
+                        System.out.println(POZO_PARED + i);
+                    }
+                }
+                System.out.println(POZO_BASE);
+                profundidadCaracol = profundidadCaracol -2;
+            
 
+    }while(profundidadCaracol>0);
+    
+    
     }
     
 }
