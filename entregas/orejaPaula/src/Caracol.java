a8fee5c8ad81e90f70df43996de654c0ba568370
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2024-10-16 11:19:29 +0200

    juego caracol

--- entregas/orejaPaula/src/Caracol.java
+++ entregas/orejaPaula/src/Caracol.java
@@ -8,14 +8,18 @@ public class Caracol {
     final String POZO_AGUA = " ##~~~~~~~~~~~~~~## _ ___";
     final String POZO_CARACOL = " ##     _@)_/'   ## _ ___";
 
-    int profundidadAgua = 10;
-    int profundidaCaracol = 16;
+    final int PROFUNDIDAD_MAXIMA = 20;
+    final int PROFUNDIDAD_MINIMA = 10;
 
-    System.out.println(POZO_TOPE) ;
-    
-    for (int i=0 ; i<=PROFUNDIDAD;  i++) {
+    int profundidadAgua = 0;
+    int profundidaCaracol = (int) (Math.random()*PROFUNDIDAD_MAXIMA-PROFUNDIDAD_MINIMA+1 )+10;
+    int dia = 0;
 
-        if (profundidaCaracol==i)  {
+    do {
+        System.out.println("DIA" + dia) ;
+        System.out.println(POZO_TOPE) ;
+        for (int i=0 ; i<=PROFUNDIDAD;  i++) {
+             if (profundidaCaracol==i)  {
             System.out.println(POZO_CARACOL + i) ;
          } else if (i>PROFUNDIDAD-profundidadAgua) {
             System.out.println(POZO_AGUA +i) ;
@@ -26,5 +30,8 @@ public class Caracol {
 
 
         System.out.println(POZO_BASE) ;
-  }
+        profundidaCaracol = profundidaCaracol - 2;
+        dia++;
+  }  while (profundidaCaracol>0);      
+ }
 }
