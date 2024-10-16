bbe8abe69d7f62b37965a5a6033371d2700b591f
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2024-10-16 13:46:53 +0200

    CaracolPozo Mejorado

--- entregas/FernandezPablo/src/CaracolPozo.java
+++ entregas/FernandezPablo/src/CaracolPozo.java
@@ -8,17 +8,46 @@ class CaracolPozo{
         final String POZO_AGUA = "     ##~~~~~~~~~~~~~~## _ __";
         final String POZO_CARACOL= "     ##    _@)_/'    ## _ __";
 
+        final int MAXIMA = 20;
+        final int MINIMA = 11;
+
+        int caracolSube = 0;
+        int caracolBaja = 0;
+        int aporteAgua = 0;
+        int POZO_CON_COCHE = 0;
+        
         int profundidadAgua = 5;
-        int profundidadCaracol = (int) (Math.random()*20-10+1)+10;
+        int profundidadCaracol = (int) (Math.random()*MAXIMA-MINIMA+1)+MINIMA;
         int dia = 0;
+        System.out.println("Al inicio el caracol cae a ["+profundidadCaracol+"] metros");
 
         do {
-            System.out.println("DIA" + dia);
-            System.out.println(POZO_TOPE);
+            dia++;
 
+            double probabilidadLluvia = Math.random();
+            int aportaAgua = probabilidadLluvia <= 0.05 ? 5 :
+                                probabilidadLluvia <= 0.1 ? 2 : 0;
+            profundidadAgua = profundidadAgua + aporteAgua;
+
+
+            caracolSube = (int) (Math.random()*(4-1+1)+1);
+            caracolBaja = (int) (Math.random()*(2-0+1)+0);
+            
+            boolean aparcaCoche = Math.random()<=0.35;
+
+            profundidadCaracol = profundidadCaracol
+                                 -caracolSube
+                                 +caracolBaja
+                                 +(aparcaCoche ? 2 : 0);
+            
+
+            System.out.println("DIA["+dia+"] - Subio["+caracolSube+"] - Bajo["+caracolBaja+"] - Posicion["+profundidadCaracol+"]");
+            System.out.println(aparcaCoche?POZO_CON_COCHE:POZO_TOPE);
+            
             for(int i=0; i<=PROFUNDIDAD; i++){
     
                 if (profundidadCaracol==i) {
+                    
                     System.out.println(POZO_CARACOL + i);
                 } else if (i>PROFUNDIDAD-profundidadAgua) {
                     System.out.println(POZO_AGUA + i);
