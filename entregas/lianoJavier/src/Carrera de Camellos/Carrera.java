32066aa280e74b7030cd7314df0863a7be3cc95d
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-10-20 21:12:26 +0200

    Quitadas las funciones, porque no ayudan tanto a la legibilidad como esperaba.

--- entregas/lianoJavier/src/Carrera de Camellos/Carrera.java
+++ entregas/lianoJavier/src/Carrera de Camellos/Carrera.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 class Carrera {
-    public static final String COLOR_RESET = "\u001B[0m";
-    public static final String COLOR_BLUE = "\u001B[34m";
-    public static final String COLOR_GREEN = "\u001B[32m";
-    public static final String COLOR_RED = "\u001B[31m";
-    public static final String COLOR_ORANGE = "\u001B[38;5;214m";
-    public static final int RECORRIDO_TOTAL = 60;
 
     public static void main(String[] args) {
+        final String COLOR_RESET = "\u001B[0m";
+        final String COLOR_BLUE = "\u001B[34m";
+        final String COLOR_GREEN = "\u001B[32m";
+        final String COLOR_RED = "\u001B[31m";
+        final String COLOR_ORANGE = "\u001B[38;5;214m";
+        final int RECORRIDO_TOTAL = 60;
 
         Scanner inputUser = new Scanner(System.in);
 
@@ -26,81 +26,50 @@ class Carrera {
             System.out.println(COLOR_GREEN + "Opciones: 1 | 2 | 3 | 4" + COLOR_RESET);
             System.out.println("Elige un agujero al que tirar: ");
 
-            int seleccionJugador = seleccionarAgujero(inputUser);
+            int seleccionJugador;
+            do {
+                seleccionJugador = inputUser.nextInt() - 1;
+            } while (seleccionJugador < 0 || seleccionJugador > 3);
 
-            boolean jugadorAciertaEnElAgujero = aciertaEnElAgujero(seleccionJugador,
-                    PROBABILIDAD_DE_ACIERTAR_EN_EL_AGUJERO);
+            boolean jugadorAciertaEnElAgujero = Math
+                    .random() <= PROBABILIDAD_DE_ACIERTAR_EN_EL_AGUJERO[seleccionJugador];
 
-            int avanceDelCamello = moverCamello(jugadorAciertaEnElAgujero, seleccionJugador,
-                    AVANCE_DEL_CAMELLO_POR_ACERTAR_EN_EL_AGUJERO);
+            int avanceDelCamello = jugadorAciertaEnElAgujero
+                    ? AVANCE_DEL_CAMELLO_POR_ACERTAR_EN_EL_AGUJERO[seleccionJugador]
+                    : 0;
 
-            boolean camelloTropezo = tropiezaCamello(PROBABILIDAD_DE_QUE_EL_CAMELLO_TROPIECE);
+            boolean camelloTropezo = Math.random() <= PROBABILIDAD_DE_QUE_EL_CAMELLO_TROPIECE;
             if (camelloTropezo) {
                 avanceDelCamello = 0;
             }
 
             posicionDelCamello += avanceDelCamello;
-            fallosRealizados = gestionDeFallos(jugadorAciertaEnElAgujero, fallosRealizados);
+            fallosRealizados = jugadorAciertaEnElAgujero ? 0 : fallosRealizados++;
 
             if (fallosRealizados >= 3) {
                 fallosRealizados = 0;
                 posicionDelCamello = 0;
             }
 
-            mostrarEstadoDelJuego(avanceDelCamello, posicionDelCamello, camelloTropezo);
+            if (camelloTropezo) {
+                System.out.println("El camello se tropezó.");
+            }
+
+            System.out.println((avanceDelCamello == 0 ? COLOR_RED : COLOR_ORANGE) + "Has avanzado: " + avanceDelCamello
+                    + COLOR_RESET);
+
+            int recorridoRestante = RECORRIDO_TOTAL - posicionDelCamello;
+            if (recorridoRestante <= 0) {
+                recorridoRestante = 0;
+            }
+            System.out.println(
+                    (recorridoRestante == 0 ? COLOR_GREEN : COLOR_RED) + "Te quedan: " + recorridoRestante
+                            + COLOR_RESET);
 
             turno += camelloTropezo ? 2 : 1;
 
         } while (posicionDelCamello < RECORRIDO_TOTAL && turno <= 50);
 
         inputUser.close();
-    }
-
-    public static int seleccionarAgujero(Scanner inputUser) {
-        int seleccionJugador;
-        do {
-            seleccionJugador = inputUser.nextInt() - 1;
-        } while (seleccionJugador < 0 || seleccionJugador > 3);
-
-        return seleccionJugador;
-    }
-
-    public static boolean aciertaEnElAgujero(int seleccionJugador, double[] probabilidades) {
-        return Math.random() <= probabilidades[seleccionJugador];
-    }
-
-    public static int moverCamello(boolean acierta, int seleccionJugador, int[] avances) {
-        if (acierta) {
-            return avances[seleccionJugador];
-        }
-
-        return 0;
-    }
-
-    public static boolean tropiezaCamello(double probabilidad) {
-        return Math.random() <= probabilidad;
-    }
-
-    public static int gestionDeFallos(boolean acierta, int fallosRealizados) {
-        if (acierta) {
-            return 0;
-        }
-        return fallosRealizados + 1;
-    }
-
-    public static void mostrarEstadoDelJuego(int avanceDelCamello, int posicionDelCamello, boolean camelloTropezo) {
-        if (camelloTropezo) {
-            System.out.println("El camello se tropezó.");
-        }
-
-        System.out.println((avanceDelCamello == 0 ? COLOR_RED : COLOR_ORANGE) + "Has avanzado: " + avanceDelCamello
-                + COLOR_RESET);
-
-        int recorridoRestante = RECORRIDO_TOTAL - posicionDelCamello;
-        if (recorridoRestante <= 0) {
-            recorridoRestante = 0;
-        }
-        System.out.println(
-                (recorridoRestante == 0 ? COLOR_GREEN : COLOR_RED) + "Te quedan: " + recorridoRestante + COLOR_RESET);
     }
 }
