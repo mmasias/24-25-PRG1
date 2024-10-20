dc6cf0962fea0685baafa4e469dfea927e8bffaf
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-10-20 18:46:43 +0200

    Aplicado de forma correcta el array

--- entregas/lianoJavier/src/Carrera de Camellos/Carrera.java
+++ entregas/lianoJavier/src/Carrera de Camellos/Carrera.java
@@ -11,11 +11,6 @@ class Carrera {
 
         Scanner inputUser = new Scanner(System.in);
 
-        final int AGUJERO_1 = 0;
-        final int AGUJERO_2 = 1;
-        final int AGUJERO_3 = 2;
-        final int AGUJERO_4 = 3;
-
         final double[] PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO = { 0.6, 0.4, 0.3, 1 };
         final int[] AVANCE_POR_ACERTAR_EN_EL_AGUJERO = { 1, 2, 4, 6 };
 
@@ -35,30 +30,9 @@ class Carrera {
                 seleccionJugador = inputUser.nextInt() - 1;
             } while (seleccionJugador < 0 || seleccionJugador > 3);
 
-            int avance = 0;
-
-            switch (seleccionJugador) {
-                case AGUJERO_1:
-                    if (Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[AGUJERO_1]) {
-                        avance = AVANCE_POR_ACERTAR_EN_EL_AGUJERO[AGUJERO_1];
-                    }
-                    break;
-                case AGUJERO_2:
-                    if (Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[AGUJERO_2]) {
-                        avance = AVANCE_POR_ACERTAR_EN_EL_AGUJERO[AGUJERO_2];
-                    }
-                    break;
-                case AGUJERO_3:
-                    if (Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[AGUJERO_3]) {
-                        avance = AVANCE_POR_ACERTAR_EN_EL_AGUJERO[AGUJERO_3];
-                    }
-                    break;
-                case AGUJERO_4:
-                    if (Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[AGUJERO_4]) {
-                        avance = AVANCE_POR_ACERTAR_EN_EL_AGUJERO[AGUJERO_4];
-                    }
-                    break;
-            }
+            int avance = Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[seleccionJugador]
+                    ? AVANCE_POR_ACERTAR_EN_EL_AGUJERO[seleccionJugador]
+                    : 0;
 
             espacioRecorrido += avance;
 
@@ -66,7 +40,8 @@ class Carrera {
             recorridoRestante = (recorridoTotal - espacioRecorrido) >= 0 ? (recorridoTotal - espacioRecorrido) : 0;
 
             System.out.println((avance == 0 ? COLOR_RED : COLOR_ORANGE) + "Has avanzado: " + avance + COLOR_RESET);
-            System.out.println((recorridoRestante == 0 ? COLOR_GREEN : COLOR_RED) + "Te quedan: " + recorridoRestante + COLOR_RESET);
+            System.out.println((recorridoRestante == 0 ? COLOR_GREEN : COLOR_RED) + "Te quedan: " + recorridoRestante
+                    + COLOR_RESET);
 
             turno++;
 
