e2fda22f6b258c2b2ddf75203b95effea60d09c3
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-10-20 19:08:06 +0200

    Mejorada la legibilidad del código

--- entregas/lianoJavier/src/Carrera de Camellos/Carrera.java
+++ entregas/lianoJavier/src/Carrera de Camellos/Carrera.java
@@ -11,30 +11,46 @@ class Carrera {
 
         Scanner inputUser = new Scanner(System.in);
 
-        final double[] PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO = { 0.6, 0.4, 0.3, 1 };
+        final double[] PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO = { 0.6, 0.4, 0.3, 0.1 };
         final int[] AVANCE_POR_ACERTAR_EN_EL_AGUJERO = { 1, 2, 4, 6 };
 
         int espacioRecorrido = 0;
         int recorridoTotal = 60;
         int turno = 1;
-
+        
         do {
             System.out.println("");
             System.out.println(COLOR_BLUE + "TURNO - [" + turno + "]" + COLOR_RESET);
             System.out.println(COLOR_GREEN + "Opciones: 1 | 2 | 3 | 4" + COLOR_RESET);
-            System.out.print("Elige un agujero al que tirar: ");
-
+            System.out.println("Elige un agujero al que tirar: ");
+            
             int seleccionJugador;
-
+            
             do {
                 seleccionJugador = inputUser.nextInt() - 1;
             } while (seleccionJugador < 0 || seleccionJugador > 3);
+            
+            boolean aciertaEnElAgujero = Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[seleccionJugador];
+            
+            int avance = 0;
+            if (aciertaEnElAgujero) {
+                avance = AVANCE_POR_ACERTAR_EN_EL_AGUJERO[seleccionJugador];
+                espacioRecorrido += avance;
+            }
+            
+            int fallosRealizados = 0;
+            fallosRealizados += !aciertaEnElAgujero ? 1 : 0;
 
-            int avance = Math.random() <= PROBABILIDAD_ACIERTAR_EN_EL_AGUJERO[seleccionJugador]
-                    ? AVANCE_POR_ACERTAR_EN_EL_AGUJERO[seleccionJugador]
-                    : 0;
+            if (aciertaEnElAgujero) {
+                fallosRealizados = 0;
+            }
 
-            espacioRecorrido += avance;
+            boolean demasiadosFallosSeguidos = fallosRealizados >= 3;
+
+            if (demasiadosFallosSeguidos) {
+                fallosRealizados = 0;
+                espacioRecorrido = 0;
+            }
 
             int recorridoRestante;
             recorridoRestante = (recorridoTotal - espacioRecorrido) >= 0 ? (recorridoTotal - espacioRecorrido) : 0;
