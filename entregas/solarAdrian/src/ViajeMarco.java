84ee35aa56731498e884729816ab0a57b9519e56
adriansolar <adrian.solar@alumnos.uneatlantico.es>
2024-10-22 13:23:42 +0200

    Un día en la vida de Marco. Pendiente: -La madre -Los días

---
+++ entregas/solarAdrian/src/ViajeMarco.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class ViajeMarco{
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        final double VELOCIDAD_MAXIMA = 15;
+        final double VELOCIDAD_MINIMA = 10;
+        final double TIEMPO_MAXIMO = 10;
+        final double TIEMPO_MINIMO = 8;
+        final double PROBABILIDAD_MONO_ESCAPA = 0.15;
+        final double PROBABILIDAD_MONO_SE_CANSA = 0.25;
+
+        final double PROBABILIDAD_LLUVIA_FUERTE = 0.1;
+        final double PROBABILIDAD_LLUVIA_NORMAL = 0.4;
+
+        double velocidadMarco, tiempoMarco, avanceMarco;
+        double probabilidadLluvia;
+        double probabilidadSeCansa, probabilidadSeEscapa;
+
+        probabilidadLluvia= Math.random();
+        probabilidadSeCansa= Math.random();
+        probabilidadSeEscapa= Math.random();
+
+        velocidadMarco = (Math.random()*VELOCIDAD_MAXIMA-VELOCIDAD_MINIMA+1)+VELOCIDAD_MINIMA;
+
+        if(probabilidadLluvia<=PROBABILIDAD_LLUVIA_FUERTE){
+            System.out.println("Lluvia fuerte");
+            velocidadMarco = velocidadMarco *0.25;
+        }else   if (probabilidadLluvia<=PROBABILIDAD_LLUVIA_NORMAL){
+            System.out.println("Lluvia fina");
+            velocidadMarco = velocidadMarco*0.75;
+        } else{
+            System.out.println("Buen tiempo");
+        }
+        if (probabilidadSeCansa<=PROBABILIDAD_MONO_SE_CANSA) {
+            System.out.println("¡El mono se cansa!");
+            velocidadMarco = velocidadMarco * 0.9;
+        } 
+
+        tiempoMarco = (Math.random()*TIEMPO_MAXIMO-TIEMPO_MINIMO+1)+TIEMPO_MINIMO;
+
+        if (probabilidadSeEscapa<=PROBABILIDAD_MONO_ESCAPA){
+            System.out.println("¡El mono se escapa!");
+            tiempoMarco = tiempoMarco-2;
+        }
+
+        
+
+        avanceMarco = velocidadMarco*tiempoMarco;
+
+        System.out.println("Avanza " + avanceMarco);
+
+        entrada.close();
+    }
+}
