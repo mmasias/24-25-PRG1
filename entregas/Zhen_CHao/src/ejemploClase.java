c3a93a4692f8c6e4aca751765f83d6f72fef8d15
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-10-22 13:38:39 +0200

    -Velocidad Marco -Probabilidad Lluvia -probabilidad mono - avance de Marco

--- entregas/Zhen_CHao/src/ejemploClase.java
+++ entregas/Zhen_CHao/src/ejemploClase.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ejemploClase {
+public class EjemploClase {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
        int minimo,maximo;
