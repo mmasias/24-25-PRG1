c1c9f88b2b37bec33cd765b263971087a46b3154
ApKy <alejandro.delapena@alumnos.uneatlantico.es>
2024-10-22 13:31:37 +0200

    Realizado hasta ahora: -Un día del viaje de Marco -Implementación de lluvia y mono Por realizar: -Un día del recorrido de la madre -Secuencia de días

---
+++ entregas/deLaPeñaAlejandro/02. Java/src/ViajeMarco.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class ViajeMarco {
+public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+        final double VELOCIDAD_MAXIMA = 15;
+        final double VELOCIDAD_MINIMA = 10;
+        final double TIEMPO_MAXIMO = 10;
+        final double TIEMPO_MINIMO = 8;
+        final double PROBABILIDAD_LLUVIA_FUERTE = 0.1;
+        final double PROBABILIDAD_LLUVIA_NORMAL = 0.4;
+        final double PROBABILIDAD_MONO_ESCAPA = 0.15;
+        final double PROBABILIDAD_MONO_CANSANCIO = 0.25;
+
+        double velocidadMarco, tiempoMarco, avanceMarco;
+        double probabilidadLluvia;
+        double probabilidadCansancio, probabilidadEscapa;
+        
+        velocidadMarco = Math.random()*(VELOCIDAD_MAXIMA-VELOCIDAD_MINIMA)+VELOCIDAD_MINIMA;
+
+        probabilidadLluvia = Math.random();
+        if(probabilidadLluvia<=PROBABILIDAD_LLUVIA_FUERTE){
+            System.out.println("Lluvia fuerte");
+            velocidadMarco = velocidadMarco * 0.25;
+        }else if(probabilidadLluvia<=PROBABILIDAD_LLUVIA_NORMAL){
+            System.out.println("Lluvia suave");
+            velocidadMarco = velocidadMarco * 0.75;
+        }else{
+            System.out.println("Buen tiempo");
+        }
+
+        probabilidadCansancio = Math.random();
+        if (probabilidadCansancio<PROBABILIDAD_MONO_CANSANCIO) {
+            System.out.println("El mono se cansa!");
+            velocidadMarco = velocidadMarco * 0.9;
+        }
+
+        tiempoMarco = Math.random()*(TIEMPO_MAXIMO-TIEMPO_MINIMO)+TIEMPO_MINIMO;
+
+        probabilidadEscapa = Math.random();
+        if(probabilidadEscapa<= PROBABILIDAD_MONO_ESCAPA){
+            System.out.println("El mono se escapó!");
+            tiempoMarco = tiempoMarco -2;
+        }
+
+        avanceMarco = velocidadMarco*tiempoMarco;
+
+        System.out.println("Avanza:" + avanceMarco);
+        
+    entrada.close();
+}
+
+    
+}
