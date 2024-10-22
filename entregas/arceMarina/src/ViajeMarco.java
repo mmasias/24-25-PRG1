580e6af1fb16f06c5234db73f51d83a6a1e87c5c
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2024-10-22 13:22:36 +0200

    Avance viaje marco clase

---
+++ entregas/arceMarina/src/ViajeMarco.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class ViajeMarco {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+            final double VELOCIDAD_MAXIMA = 15;
+            final double VELOCIDAD_MINIMA = 10;
+            final double TIEMPO_MAXIMO = 10;
+            final double TIEMPO_MINIMO = 8;
+            final double PROBABILIDAD_LLUVIA_FUERTE = 0.1;
+            final double PROBABILIDAD_LLUVIA_NORMAL = 0.4;
+            final double PROBABILIDAD_MONO_ESCAPA = 0.15;
+            final double PROBABILIDAD_MONO_CANSADO = 0.25; 
+
+
+            double velocidadMarco, tiempoMarco, avanceMarco;
+            double probabilidadLluvia;
+            double probabilidadSeCansa, probabilidadSeEscapa;
+
+            probabilidadLluvia = Math.random();
+
+            velocidadMarco = Math.random()*(VELOCIDAD_MAXIMA-VELOCIDAD_MINIMA+1)+VELOCIDAD_MINIMA;
+
+            if (probabilidadLluvia<=PROBABILIDAD_LLUVIA_FUERTE){
+                System.out.println("Luvia fuerte");
+                velocidadMarco = velocidadMarco * 0.25;
+            }else if (probabilidadLluvia<=PROBABILIDAD_LLUVIA_NORMAL) {
+                System.out.println("Lluvia fina");
+                velocidadMarco = velocidadMarco * 0.75;
+            }else {
+                System.out.println("Buen tiempo");
+            }
+
+            probabilidadSeCansa = Math.random();
+            if (probabilidadSeEscapa<PROBABILIDAD_MONO_ESCAPA) {
+                System.out.println("El mono se escapa!");
+                velocidadMarco = velocidadMarco * 0.9;
+            } 
+
+            tiempoMarco = Math.random()*(TIEMPO_MAXIMO-TIEMPO_MINIMO+1)+TIEMPO_MINIMO;
+
+            probabilidadSeEscapa = Math.random();
+            if (probabilidadSeCansa<PROBABILIDAD_MONO_ESCAPA){
+                System.out.println("El mono se escapa!");
+                tiempoMarco = velocidadMarco * 0.9;
+            }
+
+            avanceMarco = velocidadMarco*tiempoMarco;
+
+            System.out.println("Avanza: " + avanceMarco);
+
+
+        entrada.close();
+    
+    }
+}
+
