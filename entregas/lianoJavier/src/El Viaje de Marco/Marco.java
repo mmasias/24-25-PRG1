578a5e882daa9cd319e7eb69b8db21d0a3cee7f8
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-10-22 13:25:50 +0200

    Mejorado el codigo en clase

--- entregas/lianoJavier/src/El Viaje de Marco/Marco.java
+++ entregas/lianoJavier/src/El Viaje de Marco/Marco.java
@@ -1,101 +1,90 @@
+
 class Marco {
-        public static void main(String[] args) {
 
-                final int VELOCIDAD_PROMEDIA_DIARIO_MINIMO = 10;
-                final int VELOCIDAD_PROMEDIA_DIARIO_MAXIMO = 15;
+    public static void main(String[] args) {
 
-                final int TIEMPO_PROMEDIO_DIARIO_MINIMO = 8;
-                final int TIEMPO_PROMEDIO_DIARIO_MAXIMO = 10;
+        final double VELOCIDAD_PROMEDIA_DIARIO_MINIMO = 10;
+        final double VELOCIDAD_PROMEDIA_DIARIO_MAXIMO = 15;
 
-                final double PROBABILIDAD_LLUVIA_FUERTE = 0.1;
-                final double PROBABILIDAD_LLUVIA_NORMAL = 0.3;
+        final double TIEMPO_PROMEDIO_DIARIO_MINIMO = 8;
+        final double TIEMPO_PROMEDIO_DIARIO_MAXIMO = 10;
 
-                final double REDUCCION_VELOCIDAD_LLUVIA_FUERTE = 0.25;
-                final double REDUCCION_VELOCIDAD_LLUVIA_NORMAL = 0.25;
+        final double PROBABILIDAD_LLUVIA_FUERTE = 0.1;
+        final double PROBABILIDAD_LLUVIA_NORMAL = 0.4;
 
-                final double PROBABILIDAD_MONO_EN_BRAZOS = 0.25;
-                final double PROBABILIDAD_MONO_ESCAPA = 0.15;
+        final double REDUCCION_VELOCIDAD_LLUVIA_FUERTE = 0.25;
+        final double REDUCCION_VELOCIDAD_LLUVIA_NORMAL = 0.75;
 
-                final double REDUCCION_VELOCIDAD_MONO_EN_BRAZOS = 0.1;
-                final int REDUCCION_TIEMPO_HORAS_MONO_ASCAPA = 2;
+        final double PROBABILIDAD_MONO_EN_BRAZOS = 0.25;
+        final double PROBABILIDAD_MONO_ESCAPA = 0.15;
 
-                final int VELOCIDAD_MADRE = 80;
+        final double REDUCCION_VELOCIDAD_MONO_EN_BRAZOS = 0.9;
+        final double REDUCCION_TIEMPO_HORAS_MONO_ESCAPA = 2;
 
-                final int DECIMALES_A_REDODNDEAR = 2;
+        final double VELOCIDAD_MADRE = 80;
 
-                final String TEXTO_LLUEVE_FUERTE = "¡Ha llovido muchísimo!";
-                final String TEXTO_LLUEVE_NORMAL = "Ha llovido un poco hoy. :s";
-                final String TEXTO_DIA_CLARO = "¡Hoy ha hecho buenisimo!";
+        final String TEXTO_LLUEVE_FUERTE = "¡Ha llovido muchísimo!";
+        final String TEXTO_LLUEVE_NORMAL = "Ha llovido un poco hoy. :s";
+        final String TEXTO_DIA_CLARO = "¡Hoy ha hecho buenisimo!";
 
-                double distancia = 466;
-                int dia = 1;
+        double distancia = 466;
+        int dia = 1;
 
-                System.out.println("DIARIO DEL VIAJE DE MARCO");
-                System.out.println("=========================");
+        System.out.println("------------------------");
+        System.out.println("DIARIO DEL VIAJE DE MARCO");
+        System.out.println("=========================");
 
-                do {
+        do {
+            double velocidadBase = Math.random() * (VELOCIDAD_PROMEDIA_DIARIO_MAXIMO - VELOCIDAD_PROMEDIA_DIARIO_MINIMO + 1) + VELOCIDAD_PROMEDIA_DIARIO_MINIMO;
 
-                        boolean LloviendoFuerte = Math.random() >= PROBABILIDAD_LLUVIA_FUERTE;
-                        boolean LloviendoNormal = Math.random() >= PROBABILIDAD_LLUVIA_NORMAL;
-                        boolean MonoEnBrazos = Math.random() >= PROBABILIDAD_MONO_EN_BRAZOS;
-                        boolean MonoEscapa = Math.random() >= PROBABILIDAD_MONO_ESCAPA;
+            double probabilidadDeLaLluvia = Math.random();
+            boolean LluveFuerte = probabilidadDeLaLluvia >= PROBABILIDAD_LLUVIA_FUERTE;
+            boolean LluveNormal = probabilidadDeLaLluvia >= PROBABILIDAD_LLUVIA_NORMAL;
 
-                        double velocidadMarco = ((Math.random()
-                                        * (VELOCIDAD_PROMEDIA_DIARIO_MAXIMO - VELOCIDAD_PROMEDIA_DIARIO_MINIMO + 1))
-                                        + VELOCIDAD_PROMEDIA_DIARIO_MINIMO)
-                                        * (LloviendoFuerte ? REDUCCION_VELOCIDAD_LLUVIA_FUERTE : 1)
-                                        - (LloviendoNormal ? REDUCCION_VELOCIDAD_LLUVIA_NORMAL : 0)
-                                        - (MonoEnBrazos ? REDUCCION_VELOCIDAD_MONO_EN_BRAZOS : 0);
+            boolean MonoEnBrazos = Math.random() >= PROBABILIDAD_MONO_EN_BRAZOS;
 
-                        double tiempoCaminado = ((Math.random()
-                                        * (TIEMPO_PROMEDIO_DIARIO_MAXIMO - TIEMPO_PROMEDIO_DIARIO_MINIMO + 1))
-                                        + TIEMPO_PROMEDIO_DIARIO_MINIMO)
-                                        - (MonoEscapa ? REDUCCION_TIEMPO_HORAS_MONO_ASCAPA : 0);
+            double velocidadMarco
+                    = velocidadBase
+                    * (LluveFuerte ? REDUCCION_VELOCIDAD_LLUVIA_FUERTE : 1)
+                    * (LluveNormal ? REDUCCION_VELOCIDAD_LLUVIA_NORMAL : 1)
+                    * (MonoEnBrazos ? REDUCCION_VELOCIDAD_MONO_EN_BRAZOS : 1);
 
-                        double distanciaRecorridaMarco = velocidadMarco * tiempoCaminado;
-                        int distanciaRecorridaMadre = VELOCIDAD_MADRE;
+            double tiempoCaminadoBase = Math.random() * (TIEMPO_PROMEDIO_DIARIO_MAXIMO - TIEMPO_PROMEDIO_DIARIO_MINIMO + 1) + TIEMPO_PROMEDIO_DIARIO_MINIMO;
 
-                        distancia = distancia
-                                        - distanciaRecorridaMarco
-                                        - distanciaRecorridaMadre;
+            boolean MonoEscapa = Math.random() >= PROBABILIDAD_MONO_ESCAPA;
 
-                        System.out.println("DÍA " + dia);
+            double tiempoCaminado
+                    = tiempoCaminadoBase
+                    - (MonoEscapa ? REDUCCION_TIEMPO_HORAS_MONO_ESCAPA : 0);
 
-                        if (LloviendoFuerte) {
-                                System.out.println(TEXTO_LLUEVE_FUERTE);
-                        } else if (LloviendoNormal) {
-                                System.out.println(TEXTO_LLUEVE_NORMAL);
-                        } else {
-                                System.out.println(TEXTO_DIA_CLARO);
-                        }
+            double distanciaRecorridaMarco = velocidadMarco * tiempoCaminado;
+            double distanciaRecorridaMadre = VELOCIDAD_MADRE;
 
-                        System.out.println("Avancé "
-                                        + Math.round(tiempoCaminado * Math.pow(10, DECIMALES_A_REDODNDEAR))
-                                                        / Math.pow(10, DECIMALES_A_REDODNDEAR)
-                                        + " horas a "
-                                        + Math.round(velocidadMarco * Math.pow(10, DECIMALES_A_REDODNDEAR))
-                                                        / Math.pow(10, DECIMALES_A_REDODNDEAR)
-                                        + " km/h recorriendo "
-                                        + Math.round(distanciaRecorridaMarco * Math.pow(10, DECIMALES_A_REDODNDEAR))
-                                                        / Math.pow(10, DECIMALES_A_REDODNDEAR)
-                                        + "km.");
+            distancia = distancia
+                    - distanciaRecorridaMarco
+                    - distanciaRecorridaMadre;
 
-                        if (distancia >= 0) {
-                                System.out
-                                                .println("Ahora mi madre esta a "
-                                                                + Math.round(distancia
-                                                                                * Math.pow(10, DECIMALES_A_REDODNDEAR))
-                                                                                / Math.pow(10, DECIMALES_A_REDODNDEAR)
-                                                                + "km.");
-                        } else {
-                                System.out.println("¡ESTOY CON MI MADRE!");
-                        }
+            System.out.println("DÍA " + dia);
 
-                        System.out.println("------------------------");
+            System.out.println(
+                    LluveFuerte ? TEXTO_LLUEVE_FUERTE
+                            : LluveNormal ? TEXTO_LLUEVE_NORMAL
+                                    : TEXTO_DIA_CLARO
+            );
 
-                        dia++;
+            System.out.println("Avancé " + ((double) (int) (tiempoCaminado * 100)) / 100 + " horas "
+                    + "a " + (((double) (int) velocidadMarco * 100) / 100) + " km/h "
+                    + "recorriendo " + ((double) (int) distanciaRecorridaMarco * 100 / 100) + "km.");
 
-                } while (distancia >= 0);
+            if (distancia >= 0) {
+                System.out.println("Ahora mi madre esta a " + (((double) (int) (distancia * 100)) / 100) + "km.");
+            } else {
+                System.out.println("¡ESTOY CON MI MADRE!");
+            }
 
-        }
+            dia++;
+
+        } while (distancia >= 0);
+
+    }
 }
