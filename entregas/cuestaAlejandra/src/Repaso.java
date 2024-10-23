1ba478f92d71a9787f402a1a68b36605023665ec
Alex Rodríguez <alejandra.cuesta@alumnos.uneatlantico.es>
2024-10-23 13:02:06 +0200

    Carrera de Camellos

--- entregas/cuestaAlejandra/src/Repaso.java
+++ entregas/cuestaAlejandra/src/Repaso.java
@@ -4,20 +4,24 @@ class Repaso{
     public static void main(String[] args){
     Scanner entrada = new Scanner(System.in);
 
-    int posicion = 0;
+    int  posicionPrimero = 0 ;
+    int  posicionSegundo = 0 ;
 
     final String CAMELLO = ",--,^";
     final String PISTA = "...";
 
     final int FINAL =20;
+    final int MAXIMO = 4;
+    final int MINIMO = 1;
 
     boolean enCarrera = true;
     int turno = 0;
+    int avance;
 
     while (enCarrera) {
         turno++;
 
-        int avance = (int)(Math.random() * MAXIMO - MINIMO + 1)+ MINIMO;
+        avance = (int)(Math.random() * MAXIMO - MINIMO + 1)+ MINIMO;
         posicionPrimero = posicionPrimero + avance;
 
         avance = (int)(Math.random() * MAXIMO - MINIMO + 1)+ MINIMO;
