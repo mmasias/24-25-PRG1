8320aa8643bad502dc4ef6912beae65030ddc442
manuel@U.-2.6 <manuel@masiasweb.com>
2024-10-23 13:58:44 +0200

    Avance de clase. Inicio de solución de la Batalla

--- entregas/masiasManuel/src/CarreraCamellos.java
+++ entregas/masiasManuel/src/CarreraCamellos.java
@@ -32,7 +32,7 @@ class CarreraCamellos {
             System.out.println("Turno:" + turno);
             System.out.println(PISTA.repeat(posicionPrimero) + CAMELLO);
             System.out.println(PISTA.repeat(posicionSegundo) + CAMELLO);
-            enCarrera = posicionPrimero < FINAL;
+            enCarrera = (posicionPrimero > FINAL || posicionSegundo > FINAL);
             entrada.nextLine();
         }
 
