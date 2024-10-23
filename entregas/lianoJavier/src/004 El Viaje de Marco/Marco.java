bd7d7d27eba6e55f777a170e6310ba7ea4a7097c
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-10-23 22:05:43 +0200

    Proyecto Marco terminado

--- entregas/lianoJavier/src/004 El Viaje de Marco/Marco.java
+++ entregas/lianoJavier/src/004 El Viaje de Marco/Marco.java
@@ -50,10 +50,6 @@ class Marco {
                 boolean seEncuentran = distancia >= 0;
 
                 do {
-                        double velocidadBaseDeMarco = Math.random()
-                                        * (VELOCIDAD_MEDIA__MAXIMA_DIARIO_DE_MARCO
-                                                        - VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MARCO + 1)
-                                        + VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MARCO;
 
                         double probabilidadDeLaLluviaAMarco = Math.random();
                         boolean LluveFuerteMarco = probabilidadDeLaLluviaAMarco >= PROBABILIDAD_LLUVIA_FUERTE_A_MARCO,
@@ -61,14 +57,21 @@ class Marco {
 
                         boolean MonoEnBrazos = Math.random() >= PROBABILIDAD_MONO_SE_CANSA;
 
-                        double velocidadMarco = velocidadBaseDeMarco
-                                        * (LluveFuerteMarco ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_FUERTE_A_MARCO : 1)
-                                        * (LluveNormalMarco ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_NORMAL_A_MARCO : 1)
-                                        * (MonoEnBrazos ? REDUCCION_DE_VELOCIDAD_MONO_SE_CANSA : 1);
+                        double velocidadMarco = Math.random()
+                                        * (VELOCIDAD_MEDIA__MAXIMA_DIARIO_DE_MARCO
+                                                        - VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MARCO)
+                                        + VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MARCO
+                                                        * (LluveFuerteMarco
+                                                                        ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_FUERTE_A_MARCO
+                                                                        : 1)
+                                                        * (LluveNormalMarco
+                                                                        ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_NORMAL_A_MARCO
+                                                                        : 1)
+                                                        * (MonoEnBrazos ? REDUCCION_DE_VELOCIDAD_MONO_SE_CANSA : 1);
 
                         double tiempoCaminadoBaseDeMarco = Math.random()
                                         * (TIEMPO_PROMEDIO_DIARIO_MAXIMO_DE_MARCO
-                                                        - TIEMPO_PROMEDIO_DIARIO_MINIMO_DE_MARCO + 1)
+                                                        - TIEMPO_PROMEDIO_DIARIO_MINIMO_DE_MARCO)
                                         + TIEMPO_PROMEDIO_DIARIO_MINIMO_DE_MARCO;
 
                         boolean MonoEscapa = Math.random() >= PROBABILIDAD_MONO_ESCAPA;
@@ -78,38 +81,55 @@ class Marco {
 
                         double distanciaRecorridaMarco = velocidadMarco * tiempoCaminado;
 
-                        double velocidadBaseDeMadre = Math.random()
-                                        * (VELOCIDAD_MEDIA__MAXIMA_DIARIO_DE_MADRE
-                                                        - VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MADRE + 1)
-                                        + VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MADRE;
-
                         double probabilidadDeLaLluviaAMadre = Math.random();
                         boolean LluveFuerteMadre = probabilidadDeLaLluviaAMadre >= PROBABILIDAD_LLUVIA_FUERTE_A_MADRE,
                                         LluveNormalMadre = probabilidadDeLaLluviaAMadre >= PROBABILIDAD_LLUVIA_NORMAL_A_MADRE;
 
-                        double velocidadMadre = velocidadBaseDeMadre
-                                        * (LluveFuerteMadre ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_FUERTE_A_MADRE : 1)
-                                        * (LluveNormalMadre ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_NORMAL_A_MADRE : 1);
+                        double velocidadMadre = Math.random()
+                                        * (VELOCIDAD_MEDIA__MAXIMA_DIARIO_DE_MADRE
+                                                        - VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MADRE)
+                                        + VELOCIDAD_MEDIA_MINIMA_DIARIA_DE_MADRE
+                                                        * (LluveFuerteMadre
+                                                                        ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_FUERTE_A_MADRE
+                                                                        : 1)
+                                                        * (LluveNormalMadre
+                                                                        ? REDUCCION_DE_VELOCIDAD_POR_LLUVIA_NORMAL_A_MADRE
+                                                                        : 1);
 
                         double tiempoCaminadoBaseDeMadre = Math.random()
                                         * (TIEMPO_PROMEDIO_DIARIO_MAXIMO_DE_MADRE
-                                                        - TIEMPO_PROMEDIO_DIARIO_MINIMO_DE_MADRE + 1)
+                                                        - TIEMPO_PROMEDIO_DIARIO_MINIMO_DE_MADRE)
                                         + TIEMPO_PROMEDIO_DIARIO_MINIMO_DE_MADRE;
 
                         double distanciaRecorridaMadre = velocidadMadre * tiempoCaminadoBaseDeMadre;
 
                         distancia -= distanciaRecorridaMarco + distanciaRecorridaMadre;
 
+                        if (distancia < 50) {
+                                final double PROBABILIDAD_ENCONTRAR_ALGUIEN_VIO_MADRE = 0.5;
+
+                                boolean marcoEncontroAAlguienQueVioASuMadre = Math
+                                                .random() >= PROBABILIDAD_ENCONTRAR_ALGUIEN_VIO_MADRE;
+
+                                if (marcoEncontroAAlguienQueVioASuMadre) {
+                                        distancia -= 25;
+                                }
+                        }
+
                         System.out.println("DÍA " + dia);
 
                         System.out.println(
-                                        LluveFuerteMarco ? TEXTO_LLUEVE_FUERTE_MARCO
-                                                        : LluveNormalMarco ? TEXTO_LLUEVE_NORMAL_MARCO
+                                        LluveFuerteMarco
+                                                        ? TEXTO_LLUEVE_FUERTE_MARCO
+                                                        : LluveNormalMarco
+                                                                        ? TEXTO_LLUEVE_NORMAL_MARCO
                                                                         : TEXTO_DIA_CLARO_MARCO);
 
                         System.out.println(
-                                        LluveFuerteMadre ? TEXTO_LLUEVE_FUERTE_MADRE
-                                                        : LluveNormalMadre ? TEXTO_LLUEVE_NORMAL_MADRE
+                                        LluveFuerteMadre
+                                                        ? TEXTO_LLUEVE_FUERTE_MADRE
+                                                        : LluveNormalMadre
+                                                                        ? TEXTO_LLUEVE_NORMAL_MADRE
                                                                         : TEXTO_DIA_CLARO_MADRE);
 
                         System.out.println("Avancé " + ((double) (int) (tiempoCaminado * 100)) / 100 + " horas "
