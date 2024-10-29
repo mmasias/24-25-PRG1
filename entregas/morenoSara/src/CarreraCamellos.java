ce4c5bc054b5ea4d9976d4ad41058747694929eb
moreno.sara <sara.moreno@alumnos.uneatlantico.es>
2024-10-29 16:11:26 +0100

    29/10-CentroComercial

--- entregas/morenoSara/src/CarreraCamellos.java
+++ entregas/morenoSara/src/CarreraCamellos.java
@@ -21,6 +21,14 @@ public class CarreraCamellos {
         while (enCarrera) {
             turno++;
 
+            int opcionUsuario = entrada.nextInt();
+            double probabilidad = Math.random();
+
+            if (opcionUsuario==1 && probabilidad<=0.6) {avance=1;} else
+            if (opcionUsuario==2 && probabilidad<=0.4) {avance=2;} else
+            if (opcionUsuario==3 && probabilidad<=0.3) {avance=4;} else
+            if (opcionUsuario==4 && probabilidad<=0.1) {avance=6;} 
+
             avance = (int) (Math.random() * MAXIMO - MINIMO + 1) + MINIMO;
             posicionPrimero = posicionPrimero + avance;
 
@@ -29,9 +37,9 @@ public class CarreraCamellos {
 
             System.out.println("===".repeat(FINAL + 1));
             System.out.println("Turno:" + turno);
-            System.out.println(PISTA.repeat(posicionPrimero) + CAMELLO);
-            System.out.println(PISTA.repeat(posicionSegundo) + CAMELLO);
-            enCarrera = posicionPrimero < FINAL;
+            System.out.println(PISTA.repeat (posicionPrimero) + CAMELLO);
+            System.out.println(PISTA.repeat (posicionSegundo) + CAMELLO);
+            enCarrera = (posicionPrimero >  FINAL || posicionSegundo > FINAL);
             entrada.nextLine();
         }
 
