cabd6d0ad6e816328cafdab1ae5b3e689e4b7601
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-01 15:59:41 +0100

    Reseteado proyecto Batalla

--- entregas/lianoJavier/src/005 Batalla/Batalla.java
+++ entregas/lianoJavier/src/005 Batalla/Batalla.java
@@ -1,19 +1,5 @@
-
 class Batalla {
-
-    public static void main(String[] args) {
-        int poderGuerrero = 20, poderVampiro = 10;
-        int ataqueGuerrero = 2, ataqueVampiro = 4;
-        double exitoGuerrero = 0.5, exitoVampito = 0.5;
-
-        boolean viven = poderGuerrero > 0 && poderVampiro > 0;
-        int turno = 0;
-
-        while (viven) {
-            
+    public static void main(string[] args) {
         
-            viven = poderGuerrero > 0 && poderVampiro > 0;
-            turno++;
-        }
     }
 }
