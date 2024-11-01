d103b0b4f775f17d7a3344ca88e077a037569566
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-01 16:31:23 +0100

    Versión simple Batalla

--- entregas/lianoJavier/src/005 Batalla/Batalla.java
+++ entregas/lianoJavier/src/005 Batalla/Batalla.java
@@ -1,5 +1,36 @@
 class Batalla {
-    public static void main(string[] args) {
-        
+    public static void main(String[] args) {
+        int poderGuerrero = 20, poderVampiro = 40;
+        final int ataqueGuerrero = 2, ataqueVampiro = 4;
+        final double exitoGuerrero = 0.5, exitoVampiro = 0.5;
+
+        boolean ganaGuerrero;
+        boolean ganaVampiro;
+
+        do {
+
+            boolean aciertaGuerrero = Math.random() >= exitoGuerrero;
+            int dañoVampiro = aciertaGuerrero ? ataqueGuerrero : 0;
+            poderVampiro -= dañoVampiro;
+
+            boolean aciertaVampiro = Math.random() >= exitoVampiro;
+            int dañoGuerrero = aciertaVampiro ? ataqueVampiro : 0;
+            poderGuerrero -= dañoGuerrero;
+
+            ganaGuerrero = poderGuerrero <= 0;
+            ganaVampiro = poderVampiro <= 0;
+
+            System.out.println("---------------------------------");
+            System.out.println("Vida del Vampiro: " + poderVampiro);
+            System.out.println("Vida del Guerrero: " + poderGuerrero);
+        } while (!(ganaGuerrero || ganaVampiro));
+        System.out.println("---------------------------------");
+
+        String ganador = "guerrero";
+        if (ganaVampiro) {
+            ganador = "vampiro";
+        }
+
+        System.out.println("Gana: " + ganador);
     }
 }
