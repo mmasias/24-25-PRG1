c182a0610510ba735c3648cbb6c825eacca9014a
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-01 18:21:14 +0100

    Avance en la extensión del reto

--- entregas/lianoJavier/src/005 Batalla/Batalla.java
+++ entregas/lianoJavier/src/005 Batalla/Batalla.java
@@ -1,24 +1,52 @@
 class Batalla {
     public static void main(String[] args) {
-        int poderGuerrero = 20, poderVampiro = 40;
-        final int ataqueGuerrero = 2, ataqueVampiro = 4;
-        final double exitoGuerrero = 0.5, exitoVampiro = 0.5;
+        int poderGuerrero = 150, poderVampiro = 60;
+
+        final int dañoGuerreroArma1 = 7,
+                dañoGuerreroArma2 = 15,
+                dañoGuerreroArma3 = 30;
+
+        final int dañoVampiroAtaque1 = 5,
+                dañoVampiroAtaque2 = 10,
+                dañoVampiroAtaque3 = 20;
+
+        final double exitoGuerreroArma1 = 0.5,
+                exitoGuerreroArma2 = 0.25,
+                exitoGuerreroArma3 = 0.12;
+
+        final double exitoVampiroAtaque1 = 0.9,
+                exitoVampiroAtaque2 = 0.6,
+                exitoVampiroAtaque3 = 0.4;
 
         boolean ganaGuerrero;
         boolean ganaVampiro;
 
         do {
+            int probabilidadAtaqueGuerrero = (int) (Math.random());
+            int dañoVampiro = 0;
 
-            boolean aciertaGuerrero = Math.random() >= exitoGuerrero;
-            int dañoVampiro = aciertaGuerrero ? ataqueGuerrero : 0;
+            if (probabilidadAtaqueGuerrero <= exitoGuerreroArma1) {
+                dañoVampiro = dañoGuerreroArma1;
+            } else if (probabilidadAtaqueGuerrero <= exitoGuerreroArma2) {
+                dañoVampiro = dañoGuerreroArma2;
+            } else if (probabilidadAtaqueGuerrero <= exitoGuerreroArma3) {
+                dañoVampiro = dañoGuerreroArma3;
+            }
             poderVampiro -= dañoVampiro;
 
-            boolean aciertaVampiro = Math.random() >= exitoVampiro;
-            int dañoGuerrero = aciertaVampiro ? ataqueVampiro : 0;
+            int dañoGuerrero = 0;
+            int probabilidadAtaqueVampiro = (int) (Math.random());
+            if (probabilidadAtaqueVampiro <= exitoVampiroAtaque1) {
+                dañoGuerrero = dañoGuerreroArma1;
+            } else if (probabilidadAtaqueVampiro <= exitoVampiroAtaque2) {
+                dañoGuerrero = dañoGuerreroArma2;
+            } else if (probabilidadAtaqueVampiro <= exitoVampiroAtaque3) {
+                dañoGuerrero = dañoGuerreroArma3;
+            }
             poderGuerrero -= dañoGuerrero;
 
-            ganaGuerrero = poderGuerrero <= 0;
-            ganaVampiro = poderVampiro <= 0;
+            ganaVampiro = poderGuerrero <= 0;
+            ganaGuerrero = poderVampiro <= 0;
 
             System.out.println("---------------------------------");
             System.out.println("Vida del Vampiro: " + poderVampiro);
