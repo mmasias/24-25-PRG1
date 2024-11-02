9794b9d9dfdb995e0e38d81035bf405279f8e1c9
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-02 20:59:32 +0100

    Reto mercadona fase base y extendido terminado.

--- entregas/lianoJavier/src/006 Mercadona/mercadona.java
+++ entregas/lianoJavier/src/006 Mercadona/mercadona.java
@@ -12,38 +12,85 @@ class mercadona {
 		int caja2 = 0;
 		int caja3 = 0;
 		int caja4 = 0;
+		int minutosColaVacia = 0;
+		int numeroPersonasAtendidas = 0;
+		int numeroItemsVendidos = 0;
 
-		for (tiempo = 0; tiempo > HORAS_ABIERTO * MINUTOS_POR_HORA; tiempo++) {
-			boolean colaVacia = cola == 0;
+		for (tiempo = 0; tiempo < HORAS_ABIERTO * MINUTOS_POR_HORA; tiempo++) {
+			boolean colaVacia;
 			boolean llegaAlguien = Math.random() <= PROBABILIDAD_DE_LLEGADA;
 			if (llegaAlguien) {
 				cola++;
 			}
 
 			boolean caja1Vacia = caja1 == 0;
+			colaVacia = cola <= 0;
 			if (caja1Vacia && !colaVacia) {
 				cola--;
-				caja1 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1) - MINIMOS_ITEMS);
+				numeroPersonasAtendidas++;
+				caja1 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1)
+						+ MINIMOS_ITEMS);
+			} else if (!caja1Vacia) {
+				caja1--;
+				numeroItemsVendidos++;
 			}
 
 			boolean caja2Vacia = caja2 == 0;
+			colaVacia = cola <= 0;
 			if (caja2Vacia && !colaVacia) {
 				cola--;
-				caja2 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1) - MINIMOS_ITEMS);
+				numeroPersonasAtendidas++;
+				caja2 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1)
+						+ MINIMOS_ITEMS);
+			} else if (!caja2Vacia) {
+				caja2--;
+				numeroItemsVendidos++;
 			}
 
 			boolean caja3Vacia = caja3 == 0;
+			colaVacia = cola <= 0;
 			if (caja3Vacia && !colaVacia) {
 				cola--;
-				caja3 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1) - MINIMOS_ITEMS);
+				numeroPersonasAtendidas++;
+				caja3 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1)
+						+ MINIMOS_ITEMS);
+			} else if (!caja3Vacia) {
+				caja3--;
+				numeroItemsVendidos++;
 			}
 
 			boolean caja4Vacia = caja4 == 0;
+			colaVacia = cola <= 0;
 			if (caja4Vacia && !colaVacia) {
 				cola--;
-				caja4 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1) - MINIMOS_ITEMS);
+				numeroPersonasAtendidas++;
+				caja4 = (int) (Math.random() * (MAXIMOS_ITEMS - MINIMOS_ITEMS + 1)
+						+ MINIMOS_ITEMS);
+			} else if (!caja4Vacia) {
+				caja4--;
+				numeroItemsVendidos++;
 			}
+			if (colaVacia && !llegaAlguien) {
+				minutosColaVacia++;
+			}
+			String caja = "[ ] ",
+					persona = "_o_ ";
+
+			System.out.println("Tiempo: " + (tiempo / MINUTOS_POR_HORA + HORA_DE_APERTURA < 10 ? (tiempo / MINUTOS_POR_HORA + HORA_DE_APERTURA + "0") : (tiempo / MINUTOS_POR_HORA + HORA_DE_APERTURA) ) + (tiempo % MINUTOS_POR_HORA < 10 ? (tiempo % MINUTOS_POR_HORA + "0") : (tiempo % MINUTOS_POR_HORA))); 
+			System.out.println("Cola: " + persona.repeat(cola));
+			System.out.println("Caja 1: " + caja.repeat(caja1));
+			System.out.println("Caja 2: " + caja.repeat(caja2));
+			System.out.println("Caja 3: " + caja.repeat(caja3));
+			System.out.println("Caja 4: " + caja.repeat(caja4));
 		}
+			System.out.println("");
+			System.out.println("RESUMEN");
+			System.out.println("===============================================");
+			System.out.println("Minutos con la cola vacia: " + minutosColaVacia);
+			System.out.println("Número de personas en cola al final del día: " + cola);
+			System.out.println("Número de personas atendidas hoy: " + numeroPersonasAtendidas);
+			System.out.println("Número de items vendidos hoy: " + numeroItemsVendidos);
+			System.out.println("===============================================");
 
 	}
 }
