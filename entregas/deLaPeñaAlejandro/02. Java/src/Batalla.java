072c43a8ad17c82a536446f87eb289ac26327ce7
ApKy <alejandro.delapena@alumnos.uneatlantico.es>
2024-11-05 11:18:12 +0100

    Practicas(Repaso)

--- entregas/deLaPeñaAlejandro/02. Java/src/Batalla.java
+++ entregas/deLaPeñaAlejandro/02. Java/src/Batalla.java
@@ -3,23 +3,28 @@ import java.util.Scanner;
 class Batalla {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
+
         int poderGuerrero = 20, poderVampiro = 10;
         int ataqueGuerrero = 2, ataqueVampiro = 4;
         double exitoGuerrero = 0.5, exitoVampiro = 0.5;
 
-        boolean viven = poderGuerrero > 0 && poderVampiro > 0;
         int turno = 0;
+        boolean viven = (poderGuerrero * poderVampiro) > 0;
 
-       while(viven){
-        turno++;
+        while(viven){
+            turno++;
 
-        
+            poderVampiro = poderVampiro - (Math.random()<exitoGuerrero?ataqueGuerrero:0);
+            poderGuerrero = poderGuerrero - (Math.random()<exitoVampiro?ataqueVampiro:0);
 
+            System.out.println("Turno [" + turno + "]: HP Guerrero [" + poderGuerrero + "] - HP Vampiro [" + poderVampiro + "]");
+
+            viven = (poderGuerrero * poderVampiro) > 0;
+            entrada.nextLine();
         }
+
+        System.out.println("Ha ganado el " + (poderGuerrero>poderVampiro?"Guerrero":"Vampiro"));
 
         entrada.close();
     }
-    
-
 }
