05b15dac5bd607265c9598edf82451e56c7e2241
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-16 12:46:18 +0100

    add eleccionOrdenador()

--- entregas/lianoJavier/src/007 Examen/010 Adivinación/adivinacion.java
+++ entregas/lianoJavier/src/007 Examen/010 Adivinación/adivinacion.java
@@ -1,5 +1,12 @@
 class adivinacion {
   public static void main(String[] args) {
+    final int NUMERO_MAXIMO = 100, NUMERO_MINIMO = 1;
 
+    int numeroOrdenador = eleccionOrdenador(NUMERO_MAXIMO, NUMERO_MINIMO);
+
+  }
+
+  private static int eleccionOrdenador(int numeroMaximo, int numeroMinimo) {
+    return (int) ((Math.random() * numeroMaximo - numeroMinimo + 1) + numeroMinimo);
   }
 }
