ce9664050069cdc8b7564ec88129da8975eaf2cb
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-16 12:52:02 +0100

    add eleccionUsuario

--- entregas/lianoJavier/src/007 Examen/010 Adivinación/adivinacion.java
+++ entregas/lianoJavier/src/007 Examen/010 Adivinación/adivinacion.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 class adivinacion {
   public static void main(String[] args) {
     final int NUMERO_MAXIMO = 100, NUMERO_MINIMO = 1;
 
     int numeroOrdenador = eleccionOrdenador(NUMERO_MAXIMO, NUMERO_MINIMO);
+    int numeroUsuario = eleccionUsuario(NUMERO_MAXIMO, NUMERO_MINIMO);
 
+  }
+
+  private static int eleccionUsuario(int numeroMaximo, int numeroMinimo) {
+    int numero;
+    boolean numeroCorrecto;
+    do {
+
+      Scanner input = new Scanner(System.in);
+      numero = input.nextInt();
+      input.close();
+
+      numeroCorrecto = numero >= numeroMinimo && numero <= numeroMaximo;
+    } while (numeroCorrecto);
+    return numero;
   }
 
   private static int eleccionOrdenador(int numeroMaximo, int numeroMinimo) {
