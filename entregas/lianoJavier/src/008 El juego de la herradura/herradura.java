8ac2432d0ce3cffeee328dd32c5aa3dd535c89ab
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-17 17:39:40 +0100

    Extraida la función a una varibale aparte

--- entregas/lianoJavier/src/008 El juego de la herradura/herradura.java
+++ entregas/lianoJavier/src/008 El juego de la herradura/herradura.java
@@ -1,22 +1,25 @@
 public class herradura {
   public static void main(String[] args) {
+    String resultado = tirarHerradura();
+    System.out.println(resultado);
+  }
+
+  private static String tirarHerradura() {
     final double PROBABILIDAD_PRIMER_TRAMO = .1, PROBABILIDAD_SEGUNDO_TRAMO = .12, PROBABILIDAD_TERCER_TRAMO = .78;
 
-    {
-      double precision = Math.random();
-      boolean primerTramo = precision <= PROBABILIDAD_PRIMER_TRAMO;
-      boolean segundoTramo = precision >= PROBABILIDAD_PRIMER_TRAMO && precision <= PROBABILIDAD_SEGUNDO_TRAMO;
-      boolean tercerTramo = precision >= PROBABILIDAD_SEGUNDO_TRAMO && precision <= PROBABILIDAD_TERCER_TRAMO;
+    double precision = Math.random();
+    boolean primerTramo = precision <= PROBABILIDAD_PRIMER_TRAMO;
+    boolean segundoTramo = precision >= PROBABILIDAD_PRIMER_TRAMO && precision <= PROBABILIDAD_SEGUNDO_TRAMO;
+    boolean tercerTramo = precision >= PROBABILIDAD_SEGUNDO_TRAMO && precision <= PROBABILIDAD_TERCER_TRAMO;
 
-      String resultado = "fuera de pista";
-      if (primerTramo) {
-        resultado = "muy cerca";
-      } else if (segundoTramo) {
-        resultado = "cerca";
-      } else if (tercerTramo) {
-        resultado = "lejos";
-      }
-      System.out.println(resultado);
+    String resultado = "fuera de pista";
+    if (primerTramo) {
+      resultado = "muy cerca";
+    } else if (segundoTramo) {
+      resultado = "cerca";
+    } else if (tercerTramo) {
+      resultado = "lejos";
     }
+    return resultado;
   }
 }
