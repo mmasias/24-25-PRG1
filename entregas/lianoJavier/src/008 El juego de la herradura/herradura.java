7e5cdc1547e7e6b8c410299242084e03330a392e
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-17 17:37:44 +0100

    Añadida funcionalidad para determinar donde cae una herradura

--- entregas/lianoJavier/src/008 El juego de la herradura/herradura.java
+++ entregas/lianoJavier/src/008 El juego de la herradura/herradura.java
@@ -1,5 +1,22 @@
 public class herradura {
   public static void main(String[] args) {
-    
+    final double PROBABILIDAD_PRIMER_TRAMO = .1, PROBABILIDAD_SEGUNDO_TRAMO = .12, PROBABILIDAD_TERCER_TRAMO = .78;
+
+    {
+      double precision = Math.random();
+      boolean primerTramo = precision <= PROBABILIDAD_PRIMER_TRAMO;
+      boolean segundoTramo = precision >= PROBABILIDAD_PRIMER_TRAMO && precision <= PROBABILIDAD_SEGUNDO_TRAMO;
+      boolean tercerTramo = precision >= PROBABILIDAD_SEGUNDO_TRAMO && precision <= PROBABILIDAD_TERCER_TRAMO;
+
+      String resultado = "fuera de pista";
+      if (primerTramo) {
+        resultado = "muy cerca";
+      } else if (segundoTramo) {
+        resultado = "cerca";
+      } else if (tercerTramo) {
+        resultado = "lejos";
+      }
+      System.out.println(resultado);
+    }
   }
 }
