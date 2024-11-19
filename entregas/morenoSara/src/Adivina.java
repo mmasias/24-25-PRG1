4582ae66d10f7f3ac5ed345b2015cd8a2b1bc7a3
sara.moreno <sara.moreno@alumnos.uneatlantico.es>
2024-11-19 14:01:56 +0100

    Adivina 2 parte con trampas

--- entregas/morenoSara/src/Adivina.java
+++ entregas/morenoSara/src/Adivina.java
@@ -5,11 +5,17 @@ public class Adivina{
         int turno = 0;
         final int MAXIMO_TURNOS = 10;
         int opcionOrdenador = pensarNumero() ;
+        boolean calienteAnterior = false;
+
         while (estaJugando) {
             turno ++;
             int opcionUsuario = pedirNumero();
             boolean adivino = opcionUsuario == opcionOrdenador;
             darPista(opcionOrdenador,opcionUsuario);
+            if (calienteAnterior == true && esCaliente (opcionOrdenador,opcionUsuario)){
+                opcionOrdenador = hacerTrampa(opcionOrdenador);
+            }
+            calienteAnterior = esCaliente (opcionOrdenador,opcionUsuario); 
             estaJugando = !adivino && turno <= MAXIMO_TURNOS ;
         }
         String estadoFinal = (turno < 10 ? "" : "No ") + "gano!";
@@ -29,5 +35,17 @@ public class Adivina{
     static void darPista(int opcionOrdenador, int opcionUsuario){
         int diferencia = Math.abs(opcionOrdenador-opcionUsuario);
         System.out.println(diferencia <= 5 ? "Caliente": diferencia <= 10? "Tibio":"Frio");
+    }
+    static boolean esCaliente (int opcionOrdenador, int opcionUsuario){
+        int diferencia = Math.abs(opcionOrdenador - opcionUsuario);
+        return diferencia <= 5;
+    }
+    static int hacerTrampa (int opcionOrdenador){
+        int signo = Math.random()< 0.5 ? -1 : 1;
+        int numero = (int) (Math.random() * 5 + 1 );
+        numero = signo * numero;
+        opcionOrdenador = opcionOrdenador + numero;
+        System.out.println("Achis!");
+        return (opcionOrdenador);
     }
 }
