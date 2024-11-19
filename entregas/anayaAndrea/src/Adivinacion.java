462d373341f9ff5df39fc355e61e8a4cf72694a8
unknown <usuariouc@CZC8108NSZ.aulas.alumnos.unican.es>
2024-11-19 13:23:37 +0100

    creacion de entrada de usuario en la consola

--- entregas/anayaAndrea/src/Adivinacion.java
+++ entregas/anayaAndrea/src/Adivinacion.java
@@ -1,6 +1,18 @@
-Class Adivinacion {
+class Adivinacion {
     public static void main(String [] args) {
 
+        boolean estaJugndo = true;
+        int numeroOrdenador = pensarNumero();
+        int turno = 0;
+        while (estaJugando) {
+            turno++;
+            int numeroUsuario=pedirNumero();
+            boolean adivino = pedirNumero() == numeroOrdenador;
+            darPista(numeroOrdenador,numeroUsuario);
+            estaJugando = turno < 10 || !adivino;
         
+        }
+        String estadoFinal = (turno < 10? "": "no")+ "gano!";
+        System.out.println(estadoFinal);
     }
 }
