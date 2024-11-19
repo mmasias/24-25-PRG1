b2b851085d4adc4bd0ea1709bef5b6e8b7252f64
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-11-19 12:32:21 +0100

    Añadida static tiraDado()

--- entregas/lianoJavier/src/101 Adivinacion/adivinacion.java
+++ entregas/lianoJavier/src/101 Adivinacion/adivinacion.java
@@ -1,5 +1,13 @@
+
+
 class adivinacion {
     public static void main(String[] args) {
-        
+        final int NUMERO_MAXIMO = 100;
+        final int NUMERO_MINIMO = 1;
+        int resultado = tiraDado(NUMERO_MINIMO, NUMERO_MAXIMO);
+}
+
+    static int tiraDado(int numeroMinimo, int numeroMaximo) {
+        return (int) (Math.random() * (numeroMaximo - numeroMinimo) + numeroMinimo);
     }
 }
