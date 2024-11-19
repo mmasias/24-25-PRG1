fa041c21d0ba4e97a091dd522255e37e85bb3320
marcos@macbook <marcos.gonzalez@alumnos.uneatlantico.es>
2024-11-19 13:57:05 +0100

    Adivinacion final

--- entregas/gonzalezMarcos/src/Adivinacion.java
+++ entregas/gonzalezMarcos/src/Adivinacion.java
@@ -7,54 +7,58 @@ class Adivinacion {
         int numeroOrdenador = pensarNumero();
         int turno = 0;
         boolean calienteAnterior = false;
-        while (estaJugando){
+        while (estaJugando) {
             turno++;
             int numeroUsuario = pedirNumero();
             comprobarNumero(numeroOrdenador, numeroUsuario);
             boolean adivino = numeroUsuario == numeroOrdenador;
             darPista(numeroOrdenador, numeroUsuario);
-            if (calienteAnterior == esCaliente(numeroOrdenador, numeroUsuario))
+            if (calienteAnterior == true && esCaliente(numeroOrdenador, numeroUsuario))
                 numeroOrdenador = hacerTrampa(numeroOrdenador);
-            
-            estaJugando = !adivino && turno < NUMERO_TURNOS ;
+            calienteAnterior = esCaliente(numeroOrdenador, numeroUsuario);
+            estaJugando = !adivino && turno < NUMERO_TURNOS;
         }
         String estadoFinal = (turno < NUMERO_TURNOS ? "" : "no") + " ganó, el número era: ";
-        System.out.println("---- "+ estadoFinal + numeroOrdenador +" ----");
+        System.out.println("---- " + estadoFinal + numeroOrdenador + " ----");
     }
-    static int pensarNumero(){
+
+    static int pensarNumero() {
         final int MAXIMO = 1;
         final int MINIMO = 100;
-        return (int)(Math.random()*(MAXIMO-MINIMO)+MINIMO);
+        return (int) (Math.random() * (MAXIMO - MINIMO) + MINIMO);
     }
-    static int pedirNumero(){
+
+    static int pedirNumero() {
         System.out.println("---- Dime un numero entre el 1 y el 100 ----");
         Scanner entradaUsuario = new Scanner(System.in);
         return entradaUsuario.nextInt();
     }
-    static void darPista(int numeroOrdenador, int numeroUsuario){
-        int diferencia = Math.abs(numeroOrdenador-numeroUsuario);
-        System.out.println(diferencia<=5?"-> Caliente <-":diferencia<=10?"-> Tibio <-":"-> Frío <-");
+
+    static void darPista(int numeroOrdenador, int numeroUsuario) {
+        int diferencia = Math.abs(numeroOrdenador - numeroUsuario);
+        System.out.println(diferencia <= 5 ? "-> Caliente <-" : diferencia <= 10 ? "-> Tibio <-" : "-> Frío <-");
     }
-    static void comprobarNumero(int numeroOrdenador, int numeroUsuario){
+
+    static void comprobarNumero(int numeroOrdenador, int numeroUsuario) {
         if (numeroUsuario < numeroOrdenador) {
             System.out.println("El número es mayor");
         } else if (numeroUsuario > numeroOrdenador) {
             System.out.println("El número es menor");
         }
     }
-    static boolean esCaliente(int numeroOrdenador, int numeroUsuario){
-        int diferencia = Math.abs(numeroOrdenador-numeroUsuario);
-        return diferencia<5;
+
+    static boolean esCaliente(int numeroOrdenador, int numeroUsuario) {
+        int diferencia = Math.abs(numeroOrdenador - numeroUsuario);
+        return diferencia < 5;
     }
-    static int hacerTrampa(int numeroOrdenador){
-        int signo = Math.random()<0.5?-1:1;
-        int numero = (int)(Math.random()*5+1);
+
+    static int hacerTrampa(int numeroOrdenador) {
+        int signo = Math.random() < 0.5 ? -1 : 1;
+        int numero = (int) (Math.random() * 5 + 1);
         numero = numero * signo;
         numeroOrdenador = numeroOrdenador + numero;
+        System.out.println("Atchis");
         return numeroOrdenador;
     }
-
-
-
 
 }
