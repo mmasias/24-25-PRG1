3d0975cf06425f643490d0528feebacf7104b4c7
Bernie Zablah <bernardo.zablah@alumnos.uneatlantico.es>
2024-11-19 13:57:04 +0100

    codigo Adivinacion funcional terminado

--- entregas/zablahBernardo/src/19-11/AdivinaElNumero.java
+++ entregas/zablahBernardo/src/19-11/AdivinaElNumero.java
@@ -1,43 +1,34 @@
 import java.util.Scanner;
 public class AdivinaElNumero {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        
         int NUMERO_A_ADIVINAR = pensarNumero();
         int oportunidadesRestantes = 10;
-        final int NUMERO_CALIENTE = 5;
-        final int NUMERO_TIBIO = 10;
-        boolean adivinoElNumero = false;
-
-
+        boolean estaJugando = false;
         System.out.println("Adivina el Numero!");
-        while (oportunidadesRestantes > 0 && !adivinoElNumero){
+        
+        while (!estaJugando && oportunidadesRestantes > 0){
             System.out.println("te quedan " + oportunidadesRestantes + " oportunidades");
             int entradaUsuario = pedirNumero();
-            System.out.println(NUMERO_A_ADIVINAR);
+
             if (entradaUsuario == NUMERO_A_ADIVINAR){
                 System.out.println("Has Adivinado el numero!");
-                adivinoElNumero = true;
-            } else if (NUMERO_A_ADIVINAR - entradaUsuario <= NUMERO_CALIENTE){
-                System.out.println("Caliente");
-                oportunidadesRestantes = oportunidadesRestantes - 1;
-            } else if (NUMERO_A_ADIVINAR - entradaUsuario <= NUMERO_TIBIO){
-                System.out.println("Tibio");
-                oportunidadesRestantes = oportunidadesRestantes - 1;
+                estaJugando = true;
             } else {
-                System.out.println("Frio");
-                oportunidadesRestantes = oportunidadesRestantes - 1;
+                darPista(entradaUsuario, NUMERO_A_ADIVINAR);
+                oportunidadesRestantes -= 1;
             }
         }
     }
     
-    public static int pensarNumero(){
+    private static int pensarNumero(){
         final int LIMITE_MAXIMO = 100;
         final int LIMITE_MINIMO = 1;
         final int NUMERO_A_ADIVINAR = (int)(Math.random()*LIMITE_MAXIMO-LIMITE_MINIMO + 1)+LIMITE_MINIMO;
         return (NUMERO_A_ADIVINAR);
     }
 
-    public static int pedirNumero(){
+    private static int pedirNumero(){
         int entradaUsuario;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserta un numero entre 1 y 100");
@@ -45,5 +36,9 @@ public class AdivinaElNumero {
         return(entradaUsuario);
     }
 
+    private static void darPista(int entradaUsuario,int NUMERO_A_ADIVINAR){
+        int diferencia = Math.abs(NUMERO_A_ADIVINAR - entradaUsuario);
+        System.out.println(diferencia <= 5 ? "Caliente" : diferencia <= 10 ? "Tibio" : "Frio");
+        }
+        
 }
-
