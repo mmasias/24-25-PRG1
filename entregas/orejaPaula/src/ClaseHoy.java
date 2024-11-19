42efca3f974043799d6784ccd875b2795ffb78e1
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2024-11-19 13:21:57 +0100

    adivinación

---
+++ entregas/orejaPaula/src/ClaseHoy.java
@@ -0,0 +1,12 @@
+ class ClaseHoy {
+    public static void main(String[] args) {
+        saluda();
+        quejate();
+        despidete();
+}
+  
+private static void saluda() {
+private static void  quejate() { 
+private static void despidete() {
+    
+}
