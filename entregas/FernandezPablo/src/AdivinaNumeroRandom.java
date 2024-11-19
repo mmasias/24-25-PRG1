b786af3d9f9e0a2b60ad373e16fe289e29bf2f69
pablofernandez2-cmd <pablo.fernandez2@uneatlantico.alumnos.es>
2024-11-19 13:42:34 +0100

    AdivinaNumeroRandom v.2

--- entregas/FernandezPablo/src/AdivinaNumeroRandom.java
+++ entregas/FernandezPablo/src/AdivinaNumeroRandom.java
@@ -1,29 +1,38 @@
 import java.util.Scanner;
 
 class Ejemplo{
-    publi static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main (String[] args) {
+        final int NUMERO_TURNOS = 10;
+        boolean estaJugando = true;
+        int NumeroOrdenador = pensarNumero();
+        int turnos = 0;
+        while (estaJugando) {
+            turnos++;
+            int numeroUsuario = pensarNumero();
+            boolean adivino = numeroUsuario == NumeroOrdenador;
+            darPista(numeroUsuario, NumeroOrdenador);
+            estaJugando = !adivino && turnos < NUMERO_TURNOS;
+        }
+        String estadoFinal = (turnos < NUMERO_TURNOS ? "" : "no") + "gano";
+        System.out.println(estadoFinal);
 
-        int minimo, maximo;
-        System.out.println("Nivel de dificultad");
-        System.out.println("Numero minimo");
-        minimo = scanner.nextInt();
-        System.out.println("Numero maximo");
-        maximo = scanner.nextInt();
-
-        int aleatorio = (int) (Math.random() * (maximo-minimo)+minimo);
-
-        int numeroDelUsuario;
-
-        System.out.println("Dime un numero entre "+minimo+" y "+maximo+" (inclusive)");
-        numeroDelUsuario = scanner.nextInt();
-
-        String veredicto;
-
-        veredicto = numeroDelUsuario==aleatorio ? "Adivino" : "No adivino";
-
-        System.out.println(veredicto);
-
-        scanner.close();
     }
+static int pensarNumero(){
+final int MAXIMO = 100;
+final int MINIMO = 1;
+int NumeroOrdenador = (int)(Math.random()*(MAXIMO-MINIMO)+MINIMO);
+return NumeroOrdenador;
+}
+
+static int pedirNumero(){
+System.out.println("Dime un numero entre 1 y 100");
+Scanner entradaUsuario = new Scanner(System.in);
+return entradaUsuario.nextInt();
+}
+
+static void darPista(int numeroUsuario,int NumeroOrdenador){
+int diferencia = Math.abs(NumeroOrdenador - numeroUsuario);
+System.out.println(diferencia <= 5 ? "Caliente" : diferencia <= 10 ? "Tibio" : "Frio");
+}
+
 }
