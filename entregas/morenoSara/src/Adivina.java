472c44becdd5e7454588d57c60a0cbf18ff88b28
Sara Moreno Andrade <sara.moreno@unealum.local>
2024-11-19 13:38:12 +0100

    Adivina 1 parte sin trampas

---
+++ entregas/morenoSara/src/Adivina.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Adivina{
+    public static void main (String[] args){
+        boolean estaJugando = true;
+        int turno = 0;
+        final int MAXIMO_TURNOS = 10;
+        int opcionOrdenador = pensarNumero() ;
+        while (estaJugando) {
+            turno ++;
+            int opcionUsuario = pedirNumero();
+            boolean adivino = opcionUsuario == opcionOrdenador;
+            darPista(opcionOrdenador,opcionUsuario);
+            estaJugando = !adivino && turno <= MAXIMO_TURNOS ;
+        }
+        String estadoFinal = (turno < 10 ? "" : "No ") + "gano!";
+        System.out.println(estadoFinal);
+    }
+    static int pensarNumero (){
+        final int NUMERO_MAXIMO = 100;
+        final int NUMERO_MINIMO = 1;
+        int numeroOrdenador = (int) (Math.random() * NUMERO_MAXIMO - NUMERO_MINIMO+1) + NUMERO_MINIMO ;
+        return (numeroOrdenador);
+    }
+    static int pedirNumero (){
+        System.out.println("Dime un numero entre el 1 y el 100.");
+        Scanner entradaUsuario = new Scanner(System.in);
+        return entradaUsuario.nextInt(); 
+    }
+    static void darPista(int opcionOrdenador, int opcionUsuario){
+        int diferencia = Math.abs(opcionOrdenador-opcionUsuario);
+        System.out.println(diferencia <= 5 ? "Caliente": diferencia <= 10? "Tibio":"Frio");
+    }
+}
