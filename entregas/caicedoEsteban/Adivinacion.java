50171fc6e82581a7a8ee71b3f5141763c11fed25
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2024-11-19 11:54:34 +0000

    Se establece lo especificado por el problema

---
+++ entregas/caicedoEsteban/Adivinacion.java
@@ -0,0 +1,10 @@
+class Adivinacion{
+    public static void main(String[] args) {
+        
+        final int NUMERO_DE_TURNOS = 10;
+        final int NUMERO_MAXIMO = 100;
+        final int NUMERO_MINIMO = 1;
+
+
+    }
+}
