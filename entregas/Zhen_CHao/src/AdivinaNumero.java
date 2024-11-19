7b6e48f1b0910547cae62a544e3a5ee617b3c122
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-19 13:39:35 +0100

    validacion para la advinacion

--- entregas/Zhen_CHao/src/AdivinaNumero.java
+++ entregas/Zhen_CHao/src/AdivinaNumero.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.naming.event.NamingExceptionEvent;
 public class AdivinaNumero {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -8,8 +9,29 @@ public class AdivinaNumero {
             final int NUMERO_TURNOS = 10;
             boolean estaJugando = true ;
 
-            int numeroMaquina = (int) Math.random()*((MAXIMO-MINIMO+1)+MINIMO);
+            int numeroMaquina = (int) (Math.random()*(MAXIMO-MINIMO+1)+MINIMO);
             int turno = 0;
+            
+            do { 
+              turno ++;
+              System.out.println(" Dime un numero");
+              int numeroUsuario = entrada.nextInt();
+              if (numeroMaquina == numeroUsuario){
+                System.out.println("adivino");
+              }else if (numeroMaquina >numeroUsuario) {
+                System.out.println("es mayor");
+              } else {
+                System.out.println("es menor");
+              }
+
+
+
+              if (turno > NUMERO_TURNOS) {
+                  final boolean estaJugando = false;
+              }  
+
+            } while (estaJugando);
+        
 
 
 
@@ -18,7 +40,8 @@ public class AdivinaNumero {
 
 
 
-        entrada.close();
+
+       
 
         
     }
