794dace5a03fff1bca560037b21513e3455ce46f
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-19 13:01:56 +0100

    Declaración de variables

---
+++ entregas/Zhen_CHao/src/AdivinaNumero.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class AdivinaNumero {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+            final int MINIMO = 1;
+            final int MAXIMO = 100;
+            final int NUMERO_TURNOS = 10;
+            boolean estaJugando = true ;
+
+            int numeroMaquina = (int) Math.random()*((MAXIMO-MINIMO+1)+MINIMO);
+            int turno = 0;
+
+
+
+
+
+
+
+
+        entrada.close();
+
+        
+    }
+
+}
