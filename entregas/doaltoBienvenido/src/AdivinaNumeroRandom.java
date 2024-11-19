6d92f96abfa5036597e5d71b2ee3416186c67d30
Bienvenido Doalto Cavada <bienvenido.doalto@alumnos.uneatlantico.es>
2024-11-19 13:55:14 +0100

    AdivinaNumero.java

--- entregas/doaltoBienvenido/src/AdivinaNumeroRandom.java
+++ entregas/doaltoBienvenido/src/AdivinaNumeroRandom.java
@@ -4,29 +4,40 @@ import java.util.Random;
 
 class AdivinaNumeroRandom{
     public static void main(String[] args) {
-       
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+              
+        final int NUMERO_TURNOS = 10;
+        boolean estanJugando = true;
+        int turno = 0;
+        int numeroOrdenador = pensarNumeroMaquina();
 
-        int minimo;
-        int maximo;
-        int numeroUsuario;
-        
-        System.out.println("Dime el numero minimo: ");
-        minimo = scanner.nextInt();
-        
-        System.out.println("Dime un numero maximo: ");
-        maximo = scanner.nextInt();
-
-        int numeroRandom = random.nextInt(maximo - minimo) + minimo;
-
-        System.out.println("Dime un numero entre " + minimo + " y " + maximo + " :");
-        numeroUsuario = scanner.nextInt();
-
-        String resultado = numeroUsuario==numeroRandom ? "Acertaste" : "Fallaste";
-        System.out.println(resultado);
-
-        System.out.println("El numero random era: " +numeroRandom);
-  
+        do{
+            turno++;
+            int numeroUsuario = pedirNumeroUsuario();
+            verificarNumero(numeroOrdenador, numeroUsuario);
+            boolean adivino = numeroUsuario == numeroOrdenador;
+            estanJugando = !adivino && turno <  NUMERO_TURNOS;
+        }while(estanJugando);
+        String estadoFinal = (turno < NUMERO_TURNOS ? "" : "no") + "gano!";
+        System.out.println(estadoFinal);
     }
+
+    static int pedirNumeroUsuario()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dime un numero entre el 1 y el 100");
+        return scanner.nextInt();
+        
+    }
+
+    static int pensarNumeroMaquina()
+    {
+        Random random = new Random();
+        int numeroRandom = random.nextInt(100 - 1) + 1;
+        return numeroRandom;   
+    }
+
+    static void verificarNumero(int numeroOrdenador, int numeroUsuario) {
+        int diferencia = Math.abs(numeroOrdenador-numeroUsuario);
+        System.out.println(diferencia <= 5 ? "Caliente" : diferencia <= 10 ? "Tibio" : "Frio");
+    }  
 }
