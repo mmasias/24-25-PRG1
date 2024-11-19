9a41c2852de1d209dad5b56260e0d9840fc8cf45
Guillermo Martin Chavez <guillermo.chavez@alumnos.uneatlantico.es>
2024-11-20 00:24:16 +0100

    gran adivinanza
    commit con funciones como deteccion de plural o singular
    
    veo que me falta conocimiento de sintaxis, no he absorbido bien como funcionan las funciones en java, aparentemente mi codigo falla causado porque las funciones no pueden detectar variables fuera de si mismas.

--- entregas/chavezGuillermo/scr/granadivinanza.java
+++ entregas/chavezGuillermo/scr/granadivinanza.java
@@ -7,20 +7,29 @@ class granadivinanza{
         int intento=0;
         boolean juega=true;
         int numeroGenerado=rng();
-        int numeroUsuario=0;
+        int numeroUsuario;
         int pistasIntento1=3;
+        int pistasIntento2=6;
         int rangoDePrueba=10;
         
         while(juega) {
+            System.out.println("Adivina el numero! Tienes ["+(intentos-intento)+"] intentos restantes");
             intento=intento+1;
             numeroUsuario=escaneoUsuario();
 
             if (intento==pistasIntento1){
                 System.out.println(pista1());
             }
-            
+            if (intento==pistasIntento2){
+                System.out.println(pista2());
+            }
+
             if(numeroUsuario==numeroGenerado){
-                System.out.println("HAS ADIVINADO");
+                String singular="] Solo intento! Wao!";
+                if (intentos<1){
+                    singular="] intentos!";
+                }
+                System.out.println("HAS ADIVINADO \n \nTe has tardado ["+intento+singular);
                 juega=false;            
             }
             if(intento>=intentos){
@@ -28,7 +37,7 @@ class granadivinanza{
             }
             
         }
-        System.out.println("yo cuando");
+        System.out.println("= = = Juego Terminado = = =");
     }
     public static int rng(){
         int minimo=1;
@@ -45,8 +54,19 @@ class granadivinanza{
 
     public static String pista1(){
         int resultadoPrueba=(numeroGenerado-rangoDePrueba);
-        if (resultadoPrueba>0){
-            return"Es un numero muy pequeno";
+        if (resultadoPrueba<0){
+            return"Pista:Es un numero muy pequeno";
+        }else{
+            int rangoMenor=(int)Math.random()*rangoDePrueba;
+            int numeroMenor=numeroGenerado-rangoMenor;
+            return("Pista: el numero es mas grande que"+numeroMenor);
+        }
+    }
+
+    public static String pista2(){
+        int resultadoPrueba=(numeroGenerado+rangoDePrueba);
+        if (resultadoPrueba>100){
+            return"Pista:Es un numero muy grande";
         }else{
             int rangoMenor=(int)Math.random()*rangoDePrueba;
             int numeroMenor=numeroGenerado-rangoMenor;
