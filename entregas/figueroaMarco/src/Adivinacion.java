7ffb3264ab1ecc53eafde6dc2cd286552fb37ee4
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2024-11-19 12:57:26 +0100

    Declaracion inicial y creando metodos.

---
+++ entregas/figueroaMarco/src/Adivinacion.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Adivinacion {
+    public static void main(String[] args) {
+        
+        final int OPORTUNIDADES_ADIVINAR = 10;
+        final int PISTA_CALIENTE = 5;
+        final int PISTA_TIBIA = 10;
+        final int PISTA_FRIA = 10;
+
+        Scanner input = new Scanner(System.in);
+
+
+        
+
+    }
+
+    private static int generarNumero() {
+        
+    }
+
+    private static int pedirNumero() {
+
+    }
+
+    private static void darPista() {
+
+    }
+
+
+    
+}
