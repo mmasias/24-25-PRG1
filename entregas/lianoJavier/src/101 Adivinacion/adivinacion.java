b35b463e19c3afdc9e55fb57318a172c21e48189
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-11-19 13:00:55 +0100

    Cambiado un resultado aleatorio del jugador por preguntarle directamente

--- entregas/lianoJavier/src/101 Adivinacion/adivinacion.java
+++ entregas/lianoJavier/src/101 Adivinacion/adivinacion.java
@@ -1,10 +1,29 @@
+
+import java.util.Scanner;
+
 class adivinacion {
     public static void main(String[] args) {
         final int NUMERO_MAXIMO = 100;
         final int NUMERO_MINIMO = 1;
-        int resultado = tiraDado(NUMERO_MINIMO, NUMERO_MAXIMO);
+        int resultado = preguntarUsuario(NUMERO_MINIMO, NUMERO_MAXIMO);
         int numeroAAdivinar = tiraDado(NUMERO_MINIMO, NUMERO_MAXIMO);
         imprimirResumen(resultado, numeroAAdivinar);
+
+    }
+
+    static int preguntarUsuario(int numeroMinimo, int numeroMaximo) {
+        Scanner input = new Scanner(System.in);
+        boolean esCorrecto;
+        int respuesta;
+        System.out.print("Escoja un número entre 1 y 100: ");
+        do {
+            respuesta = input.nextInt();
+            esCorrecto = numeroMaximo >= respuesta && numeroMinimo <= respuesta;
+            System.out.println();
+            if (!esCorrecto) System.out.println("Numero incorrecto.");
+        } while (!esCorrecto);
+        input.close();
+        return respuesta;
     }
 
     static int tiraDado(int numeroMinimo, int numeroMaximo) {
