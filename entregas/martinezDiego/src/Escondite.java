6ed278057db4ca3a0dedffb3821f64c63d5cafa3
31Diego <diegomartinezt31@gmail.com>
2024-11-26 17:04:30 +0100

    1

---
+++ entregas/martinezDiego/src/Escondite.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Escondite {
+
+    public static void main(String[] args) {
+
+        final int NUMERO_MAXIMO_INTENTOS;
+        
+        final int ARBOL = 1;
+        final int BANCO = 2;
+        final int ARBUSTO = 3;
+        final int COLUMPIO = 4;
+        final int CASETA = 5;
+        final int TOBOGAN = 6;
+
+        int niño1, niño2, niño3;
+        int posicionNiño1, posicionNiño2, posicionNiño3;
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+
+    
+}
---
+++ entregas/martinezDiego/src/MercadonaMetodos.java
@@ -0,0 +1,44 @@
+
+
+
+public class MercadonaMetodos {
+    
+    final static int MINUTOS_ABIERTO = 720;
+    final static double PROBABILIDAD_CLIENTE = 0.6;
+    final static int MINIMO_PRODUCTOS = 5;
+    final static int MAXIMO_PRODUCTOS = 15;
+    
+    static int caja1 = 0;
+    static int caja2 = 0;
+    static int caja3 = 0;
+    static int caja4 = 0;
+
+    static boolean caja1Abierta = false;
+    static boolean caja2Abierta = false;
+    static boolean caja3Abierta = false;
+    static boolean caja4Abierta = false;
+
+    static int productosCaja1 = 0;
+    static int productosCaja2 = 0;
+    static int productosCaja3 = 0;
+    static int productosCaja4 = 0;
+    static int fila = 0;
+    static int minuto = 0;
+    
+
+    
+    public static void main(String[] args) {
+        
+    }
+
+    private static calcularProductos{
+
+        
+
+
+
+
+    }
+
+    
+}
