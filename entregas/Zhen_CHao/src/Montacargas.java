c2cdfc12bbd768885e36078624afd196925f25ec
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 14:27:54 +0100

    Cambio de varibles y proceso de subida del montacargas

--- entregas/Zhen_CHao/src/Montacargas.java
+++ entregas/Zhen_CHao/src/Montacargas.java
@@ -1,14 +1,34 @@
 import java.util.Scanner;
 public class Montacargas {
-   public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
-        final int PLANTA_ACTUAL = 0; 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+         int plantaActual = 0; 
         boolean continuar = true;
         
         while (continuar) {
-            System.out.println("El montacargas esta en la planta "+ PLANTA_ACTUAL);
-            System.out.println("A que planta deseas ir (de la 1 a la 10)");
-            final int PLANTA_DESTINO = entrada.nextInt();
+            System.out.println("El montacargas esta en la planta "+ plantaActual);
+            System.out.println("A que planta deseas ir (de la 0 a la 10, O INGRESA -1 PASA SALIR)");
+            final int plantaDestino = entrada.nextInt();
+            if (plantaDestino == -1){
+                continuar = false;
+                System.out.println("Simulacion finalizada");
+            }
+
+            if (plantaDestino>plantaActual) {
+
+                System.out.println("El montacargas sube de la planta " + plantaActual + " a la planta " + plantaDestino + ".");
+                } else if (plantaDestino < plantaActual) {
+                    System.out.println("El montacargas baja de la planta " + plantaActual + " a la planta " + plantaDestino + ".");
+                } else {
+                    System.out.println("El montacargas ya está en la planta " + plantaActual + ".");
+                }
+
+               
+            }
+
+
+            
+
 
             
 
