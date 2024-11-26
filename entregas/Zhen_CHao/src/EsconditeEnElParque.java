c363814e07700a33f7436467b5ab262e414b5d6c
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 13:55:24 +0100

    logica sobre la probabilidad de encontrar niño

--- entregas/Zhen_CHao/src/EsconditeEnElParque.java
+++ entregas/Zhen_CHao/src/EsconditeEnElParque.java
@@ -11,21 +11,51 @@ public class EsconditeEnElParque {
         final int COLUMPIO = 4;
         final int CASETA = 5;
         final int TOBOGAN = 6;
+        final double PROBABILIDAD_OCULTARSE = 0.1;
+        boolean encontradoEnEstaBusqueda = false;
+        boolean intentosRestantes = 12 ; 
         
-        
-        final int NIÑO_1 = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;;
-        int NIÑO_2 = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;;
-        int NIÑO_3 = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;; 
+        final int PROBABILIDAD_NIÑO_1 = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;;
+        int PROBABILIDAD_NIÑO_2 = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;;
+        int PROBABILIDAD_NIÑO_3 = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;; 
+        boolean encontradoNiño1 =false,encontradoNiño2 =false,encontradoNiño3 =false;
 
+        while (intentosRestantes > 0 && !(encontradoNiño1 && encontradoNiño2 && encontradoNiño3)){
         System.out.println("Donde quiere buscar 1-Árbol 2-Banco 3-Arbusto 4-Columpio 5-Caseta 6-Tobogan");
         int respuestaUsuario = entrada.nextInt();
 
-        if (respuestaUsuario == ARBOL && NIÑO_1 == ARBOL ) {
-            System.out.println("");
-            
+            if (PROBABILIDAD_NIÑO_1 == respuestaUsuario && !encontradoNiño1){
+                if (Math.random() < PROBABILIDAD_OCULTARSE) {
+                    System.out.println("¡El niño en el lugar " + respuestaUsuario + " se ha movido!");
+                    } else {
+                        encontradoNiño1 = true;
+                        System.out.println("¡Has encontrado al primer niño en el lugar " + respuestaUsuario + "!");
+                        encontradoEnEstaBusqueda = true;
+                }
+
+            }
+            if (PROBABILIDAD_NIÑO_2 == respuestaUsuario && !encontradoNiño1){
+                if (Math.random() < PROBABILIDAD_OCULTARSE) {
+                    System.out.println("¡El niño en el lugar " + respuestaUsuario + " se ha movido!");
+                    } else {
+                        encontradoNiño2 = true;
+                        System.out.println("¡Has encontrado al primer niño en el lugar " + respuestaUsuario + "!");
+                        encontradoEnEstaBusqueda = true;
+                }
+
+            }
+            if (PROBABILIDAD_NIÑO_3 == respuestaUsuario && !encontradoNiño1){
+                if (Math.random() < PROBABILIDAD_OCULTARSE) {
+                    System.out.println("¡El niño en el lugar " + respuestaUsuario + " se ha movido!");
+                    } else {
+                        encontradoNiño3 = true;
+                        System.out.println("¡Has encontrado al primer niño en el lugar " + respuestaUsuario + "!");
+                        encontradoEnEstaBusqueda = true;
+                }
+
+            }
+        
         }
-        
-        
         
         
         entrada.close();
