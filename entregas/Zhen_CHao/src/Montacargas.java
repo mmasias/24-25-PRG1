ab76207bbb4b37415bb60073560785dac3cb5d6e
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 14:17:02 +0100

    SE DEFINEN ALGUNAS VARIABLES A USAR Y SE PLANTEA UN WHILE PARA EL PROCEDIMIENTO

--- entregas/Zhen_CHao/src/Montacargas.java
+++ entregas/Zhen_CHao/src/Montacargas.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 public class Montacargas {
    public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
-    
+        final int PLANTA_ACTUAL = 0; 
+        boolean continuar = true;
+        
+        while (continuar) {
+            System.out.println("El montacargas esta en la planta "+ PLANTA_ACTUAL);
+            System.out.println("A que planta deseas ir (de la 1 a la 10)");
+            final int PLANTA_DESTINO = entrada.nextInt();
+
+            
+
+            
+        }
    } 
 }
