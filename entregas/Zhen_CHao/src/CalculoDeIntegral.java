76b70dc133f624c137efe20dcbf4455076a628e8
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 12:53:18 +0100

    Definir el scanner para hacer pregunta al ususario

--- entregas/Zhen_CHao/src/CalculoDeIntegral.java
+++ entregas/Zhen_CHao/src/CalculoDeIntegral.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
 public class CalculoDeIntegral {
-    
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+      System.out.println(" Exceriba un numeor del 1 al 10");
+      int inputUsuario = scanner.nextInt();
+      System.out.println(Math.sin(5)(sin(inputUsuario/1.59)))
+
+     scanner.close();
+    }
 }
