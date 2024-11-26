5ee6c80565082d72ba05450d21711049fe60e01f
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 13:08:40 +0100

    primer commit Escondite ene le parque

---
+++ entregas/Zhen_CHao/src/EsconditeEnElParque.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class EsconditeEnElParque {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        entrada.close();
+    }
+}
