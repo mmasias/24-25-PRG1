1d66a8ae5bcab3faeb747614beb01eaa60b71059
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 13:08:21 +0100

    me rindo en el de calculo integral

--- entregas/Zhen_CHao/src/CalculoDeIntegral.java
+++ entregas/Zhen_CHao/src/CalculoDeIntegral.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 public class CalculoDeIntegral {
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
-      System.out.println(" Exceriba un numeor del 1 al 10");
-      int inputUsuario = scanner.nextInt();
-      System.out.println(Math.sin(5)(sin(inputUsuario/1.59)))
+      System.out.println(" Escriba numero numero limite inferior");
+      double inputLimiteInferior = scanner.nextDouble();
+      System.out.println(" Escriba numero numero limite superior");
+      double inputLimiteSuperior = scanner.nextDouble();
+      final double RESULTADO = Math.sin(5) * (Math.sin(inputUsuario / 1.59) + 0.2 * Math.sin(5 * inputUsuario / 1.59));
+      System.out.println(RESULTADO);
 
      scanner.close();
     }
