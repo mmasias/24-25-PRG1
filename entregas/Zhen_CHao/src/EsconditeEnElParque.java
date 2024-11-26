e112ec41fe08ed43ae10a9bdfa3a02f45442b284
chao.zhen <chao.zhen@alumnos.uneatlantico.esn>
2024-11-26 13:16:34 +0100

    Se definen las variables para ProbabilidadEscondite

--- entregas/Zhen_CHao/src/EsconditeEnElParque.java
+++ entregas/Zhen_CHao/src/EsconditeEnElParque.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class EsconditeEnElParque {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        final int MAXIMO = 6;
+        final int MINIMO = 1;
+        int ProbabilidadEscondite = (int)(Math.random()*MAXIMO-MINIMO+1)+MINIMO;
+        
+        System.out.println("Donde quiere esconder al niño 1-Árbol 2-Banco 3-Arbusto 4-Columpio 5-Caseta 6-Tobogan");
+        
+        
+        
         
         entrada.close();
     }
