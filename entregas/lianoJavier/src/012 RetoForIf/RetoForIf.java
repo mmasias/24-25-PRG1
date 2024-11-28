e15a6929ae6faa46d427cc56082ac5585cf8be4f
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-11-28 13:34:08 +0100

    RetoForIF | ejemplo 5

--- entregas/lianoJavier/src/012 RetoForIf/RetoForIf.java
+++ entregas/lianoJavier/src/012 RetoForIf/RetoForIf.java
@@ -8,7 +8,7 @@ class RetoForIf {
 
                 if (
                 // El código desde aqui *********************************
-                    i>=8 && i<=14
+                    i==11 && j==11
                 // El código hasta aquí *********************************
                 ) {
                     System.out.print("(*)");
