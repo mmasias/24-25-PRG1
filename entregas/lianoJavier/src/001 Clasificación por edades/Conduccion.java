8be1b56f851d1ed52bcccb0984518d6cb9ec1971
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-12-02 14:04:10 +0100

    new method pedirEdad()
    nuevo metodo para pedir la edad al usuario

--- entregas/lianoJavier/src/001 Clasificación por edades/Conduccion.java
+++ entregas/lianoJavier/src/001 Clasificación por edades/Conduccion.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
 
 class Conduccion {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
 
-        boolean tieneCarnetDeConducir;
-        int edad;
+    boolean tieneCarnetDeConducir;
+    int edad;
 
-        System.out.println("Ingrese su edad:");
-        edad = entrada.nextInt();
+    edad = pedirEdad();
 
-        System.out.println("¿Tiene licencia de conducir? (true/false):");
-        tieneCarnetDeConducir = entrada.nextBoolean();
+    System.out.println("¿Tiene licencia de conducir? (true/false):");
+    tieneCarnetDeConducir = entrada.nextBoolean();
 
-        boolean menorDeEdad = edad < 18;
-        boolean mayorSinLicencia = edad >= 18 && !tieneCarnetDeConducir;
-        boolean conductorNovel = edad <= 19 && tieneCarnetDeConducir;
-        boolean conductorExperimentado = edad <= 27 && tieneCarnetDeConducir;
-        boolean conductorJubilado = edad >= 65 && tieneCarnetDeConducir;
+    boolean menorDeEdad = edad < 18;
+    boolean mayorSinLicencia = edad >= 18 && !tieneCarnetDeConducir;
+    boolean conductorNovel = edad <= 19 && tieneCarnetDeConducir;
+    boolean conductorExperimentado = edad <= 27 && tieneCarnetDeConducir;
+    boolean conductorJubilado = edad >= 65 && tieneCarnetDeConducir;
 
-        String resultado = menorDeEdad ? "Eres menor"
-                : mayorSinLicencia ? "No tiene licencia"
-                        : conductorNovel ? "Conductor Novel"
-                                : conductorExperimentado ? "Conductor Experimentado"
-                                        : "Conductor Senior";
+    String resultado = menorDeEdad ? "Eres menor"
+    : mayorSinLicencia ? "No tiene licencia"
+    : conductorNovel ? "Conductor Novel"
+    : conductorExperimentado ? "Conductor Experimentado"
+    : "Conductor Senior";
 
-        System.out.println(
-                "Clasificación: " + resultado + ". " +
-                        ((!tieneCarnetDeConducir || conductorJubilado)
-                                ? "No puede conducir"
-                                : "Puede conducir"));
+    System.out.println(
+      "Clasificación: " + resultado + ". " +
+    ((!tieneCarnetDeConducir || conductorJubilado)
+      ? "No puede conducir"
+      : "Puede conducir"));
 
-    }
+  }
+  static int pedirEdad() {
+    System.out.println("Ingrese su edad:");
+    respuesta = new Scanner(System.in).nextInt();
+    // if(evaluarRespuesta(respuesta) == "error") respuesta = pedirEdad();
+    return respuesta;
+  }
 }
