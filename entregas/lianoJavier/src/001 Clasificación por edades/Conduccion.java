e8b125bac27975b38811a4de3fd1756b95ea231d
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-12-02 22:28:54 +0100

    arreglado bug con Scanner
    lo he sacado a un metodo externo y cerrado en el main y ha funcionado

--- entregas/lianoJavier/src/001 Clasificación por edades/Conduccion.java
+++ entregas/lianoJavier/src/001 Clasificación por edades/Conduccion.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 class Conduccion {
+  static Scanner entrada = new Scanner(System.in);
+
   public static void main(String[] args) {
 
-    int edad;
-    edad = pedirEdad();
-
+    int edad = pedirEdad();
     boolean tieneCarnet = pedirCarnet();
+
     String mensaje;
     if (tieneCarnet) {
       mensaje = "Eres " + evaluar(edad) + ".";
@@ -15,33 +16,44 @@ class Conduccion {
     }
     System.out.println(mensaje);
 
+    entrada.close();
   }
-  static int pedirEdad() {
-    int edad;
 
+  static int pedirEdad() {
     System.out.println("Ingrese su edad:");
 
-    edad = new Scanner(System.in).nextInt();
+    int edad = entrada.nextInt();
 
-    if (evaluar(edad) == "error") edad = pedirEdad();
+    if (evaluar(edad) == "error")
+      edad = pedirEdad();
 
     return edad;
   }
+
   static boolean pedirCarnet() {
     System.out.println("¿Tiene licencia de conducir? (true/false):");
-    return new Scanner(System.in).nextBoolean();
+
+    return entrada.nextBoolean();
   }
+
   static String evaluar(
-    int edad
-  ) {
+      int edad) {
     final String ERROR = "error";
     String mensaje;
-    if (edad <= 0) mensaje = ERROR;
-    if (edad < 18) mensaje = "menor de edad"; else
-    if (edad <= 19) mensaje = "conductor novel"; else
-    if (edad <= 27) mensaje = "conductor experimentado"; else
-    if (edad <= 64) mensaje = "conducor senior"; else 
-    if (edad < 100) mensaje = "conductor juvilado"; else mensaje = ERROR;
+    if (edad <= 0)
+      mensaje = ERROR;
+    if (edad < 18)
+      mensaje = "menor de edad";
+    else if (edad <= 19)
+      mensaje = "conductor novel";
+    else if (edad <= 27)
+      mensaje = "conductor experimentado";
+    else if (edad <= 64)
+      mensaje = "conductor senior";
+    else if (edad < 100)
+      mensaje = "conductor juvilado";
+    else
+      mensaje = ERROR;
     return mensaje;
   }
 }
