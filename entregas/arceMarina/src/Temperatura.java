1cad071acbca0c3cdf3c1e68b701515db0e3fd62
Marina <marina.arce@alumnos.uneatlantico.es>
2024-12-03 15:23:31 +0100

    clase matrices 03/12

---
+++ entregas/arceMarina/src/Temperatura.java
@@ -0,0 +1,12 @@
+class Temperatura {
+    public static void main(String[]args) {
+
+        double[] temperaturasS = new double[7];
+
+        for(int dia=0;dia<7;dia++);
+            temperatura[dia] = pedirTemperatura;
+
+
+
+    }
+}
