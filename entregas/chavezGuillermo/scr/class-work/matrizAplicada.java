b07fc1511bb9220db79b9f93967fa302b351fad4
Guillermo Martin Chavez <guillermo.chavez@alumnos.uneatlantico.es>
2024-12-04 04:57:15 -0800

    Add files via upload

---
+++ entregas/chavezGuillermo/scr/class-work/matrizAplicada.java
@@ -0,0 +1,16 @@
+class matrizAplicada{
+    public static void main(String[] args) {
+    int[] t=new int[]{1,2,3,4}
+    int[] tt=new int[]{1,2,3,4}
+
+        if(t==tt){
+
+        }
+
+        t=tt
+    }
+
+    int[][] t=new int[10][100]
+    //se hace la priumera memoria de t
+    // esta hace referwncia y se hace el espacio para guardar 10 espacios de 100 variables integros
+    //11 referencias para 
