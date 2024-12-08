0018aa372296f8c9c2dbc6a31ad7eb39544665f0
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-12-08 13:56:27 +0100

    retoque estetico

--- entregas/lianoJavier/src/011 Carrera de Camellos/Carrera.java
+++ entregas/lianoJavier/src/011 Carrera de Camellos/Carrera.java
@@ -7,20 +7,24 @@ class Carrera {
     casillaActual += calcularAvance();
     evaluarResulrado(casillaActual);
   }
+
   static int preguntarJugador() {
     Scanner entrada = new Scanner(System.in);
     int respuesta = entrada.nextInt();
     entrada.close();
     return respuesta;
   }
+
   static int calcularAvance(int agujero) {
     final int[] AVANCE = {1,2,4,6};
     if (isAcierto()) {
-       return AVANCE[agujero]
+      return AVANCE[agujero]
     }
   }
+  
   static boolean isAcierto(int agujero) {
-  double[] PROBABILIDAD_ACIERTO = {0.6, 0.4, 0.3, 0.1};
+    double[] PROBABILIDAD_ACIERTO = {0.6, 0.4, 0.3, 0.1};
     return math.random() <= PROBABILIDAD_ACIERTO[agujero];
   }
+
 }
