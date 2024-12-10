9b633ab09eefe6dec565b290d6ed57c6fd8c12d1
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-12-10 14:06:12 +0100

    :warning: matrices
    errores
    solo para subir al repo
    punto de guardado

--- entregas/lianoJavier/src/013 Matrices/Matrices.java
+++ entregas/lianoJavier/src/013 Matrices/Matrices.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Matrices {
   public static void main(String[] args) {
 
@@ -13,22 +14,44 @@ class Matrices {
       {1,2,2,1,0,2,2,1,0,0},
       {1,0,1,0,2,1,2,2,2,1}
     };
-    imprimir(mapa);
+
+    int[] posicion = constructorPosicion();
+
+    imprimir(mapa, posicion);
+  }
+
+  static int[] constructorPosicion() {
+    System.out.print("Dame el valor de X: ");
+    int posicionX = pedirPosicion();
+    System.out.print("Dame el valor de Y: ");
+    int posicionY = pedirPosicion();
+    int[] posicion = {posicionX, posicionY};
+    return posicion;
+  }
+
+  static int pedirPosicion() {
+    return new Scanner(System.in).nextInt();
   }
 
   static String parse(int pixel) {
     final String[] SKINS = {
       "[ ]",
       "...",
-      "[-]"
+      "[-]",
+      "_O_"
     };
     return SKINS[pixel];
   }
 
-  static void imprimir(int[][] mapa) {
-    for (int[] columna : mapa) {
-      for (int pixel : columna) {
-        System.out.print(parse(pixel));
+  static void imprimir(int[][] mapa, int[] coordenadaPersonaje) {
+    int PERSONAJE = 3;
+    boolean isPersonaje;
+    String mensaje;
+    for (int columna = 0; columna <= mapa.length; columna++) {
+      for (int fila = 0; fila <= mapa[columna].length; fila++) {
+        isPersonaje = fila == coordenadaPersonaje[1] && columna == coordenadaPersonaje[0];
+        mensaje = isPersonaje ? parse(coordenadaPersonaje[PERSONAJE]) : parse(mapa[fila][columna]);
+        System.out.print(mensaje);
       }
       System.out.println();
     }
