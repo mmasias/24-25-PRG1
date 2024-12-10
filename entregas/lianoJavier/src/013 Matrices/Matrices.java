8ca18b3fefa718b809c6577350489265f32062e3
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-12-10 16:31:05 +0100

    Matrices - a0.2
    hemos hecho el codigo base.
    - [x] hacer una matríz con sentido.
    - [ ] hacer más de un parse con su index.
    - [ ] configurar el movimiento

--- entregas/lianoJavier/src/013 Matrices/Matrices.java
+++ entregas/lianoJavier/src/013 Matrices/Matrices.java
@@ -4,57 +4,52 @@ class Matrices {
   public static void main(String[] args) {
 
     int[][] mapa = {
-        { 1, 1, 0, 0, 0, 0, 0, 2, 2, 2 },
-        { 1, 1, 0, 0, 0, 0, 0, 2, 2, 2 },
-        { 1, 2, 0, 2, 2, 2, 2, 1, 1, 1 },
-        { 1, 2, 0, 2, 0, 2, 0, 2, 1, 0 },
-        { 2, 2, 1, 0, 0, 2, 2, 0, 1, 2 },
-        { 2, 0, 0, 0, 1, 1, 2, 0, 2, 2 },
-        { 2, 2, 0, 2, 0, 0, 1, 2, 2, 2 },
-        { 0, 2, 2, 1, 1, 0, 1, 2, 2, 1 },
-        { 0, 2, 0, 1, 1, 1, 0, 1, 1, 2 },
-        { 1, 2, 2, 1, 0, 2, 2, 1, 0, 0 },
-        { 1, 0, 1, 0, 2, 1, 2, 2, 2, 1 }
+        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 1, 1, 1, 0, 1, 1 },
+        { 0, 1, 0, 0, 1, 0, 1, 0, 1, 0 },
+        { 0, 1, 1, 1, 1, 0, 1, 0, 1, 0 },
+        { 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
+        { 0, 0, 0, 1, 1, 1, 1, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 },
+        { 0, 1, 1, 1, 0, 1, 1, 1, 1, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     };
 
-    int[] posicion = constructorPosicion();
+    int[] posicion = { 1, 0 };
+
+    int[] posicion = mover();
 
     imprimir(mapa, posicion);
-  }
-
-  static int[] constructorPosicion() {
-    System.out.print("Dame el valor de X: ");
-    int posicionX = pedirPosicion();
-    System.out.print("Dame el valor de Y: ");
-    int posicionY = pedirPosicion();
-    int[] posicion = { posicionX, posicionY };
-    return posicion;
-  }
-
-  static int pedirPosicion() {
-    return new Scanner(System.in).nextInt();
   }
 
   static String parse(int pixel) {
     final String[] SKINS = {
         "[ ]",
-        "...",
+        "///",
         "[-]",
         "_O_"
     };
     return SKINS[pixel];
+    // TODO: Añadir parsers distintos.
   }
 
   static void imprimir(int[][] mapa, int[] coordenadaPersonaje) {
     int PERSONAJE = 3;
     boolean isPersonaje;
-    String mensaje;
-    for (int columna = 0; columna <= mapa.length; columna++) {
-      for (int fila = 0; fila <= mapa[columna].length; fila++) {
+    String skin;
+
+    for (int fila = 0; fila < mapa.length; fila++) {
+      for (int columna = 0; columna < mapa[columna].length; columna++) {
+        skin = parse(mapa[fila][columna]);
+
         isPersonaje = fila == coordenadaPersonaje[1] && columna == coordenadaPersonaje[0];
-        mensaje = isPersonaje ? parse(coordenadaPersonaje[PERSONAJE]) : parse(mapa[fila][columna]);
-        System.out.print(mensaje);
+        skin = isPersonaje ? parse(PERSONAJE) : skin;
+
+        System.out.print(skin);
       }
+
       System.out.println();
     }
   }
