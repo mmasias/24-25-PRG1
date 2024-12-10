f73435e9b1bd3264cfa4def48f6795858e310bbf
carlosvaldesbarquin <carlos.valdes@alumnos.uneatlantico.es>
2024-12-10 13:42:12 +0100

    Ejercicio Matriz 10-12

---
+++ entregas/valdesCarlos/src/EjercicioMatriz.java
@@ -0,0 +1,49 @@
+class EjercicioMatriz {
+    public static void main(String[] args) {
+        int[][] matriz = {
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 },
+            { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+            { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 },
+            { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0, 0 },
+            { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 },
+            { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            { 9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0 },
+            { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5 },
+            { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }
+        };
+
+        imprime(matriz);
+
+    }
+
+    static void imprime(int[][] matriz) {
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.print(parse(matriz[fila][columna]));
+            }
+            System.out.println();
+        }
+    }
+
+    static String parse(int valor) {
+        final String[] TILES = {
+            "   ", 
+            "[@]", 
+            " . ", 
+            "~ ~", 
+            "` ´", 
+            ":::", 
+            ", '", 
+            "/^/", 
+            "^/^", 
+            ":/:"  
+        };
+
+        if (valor >= 0 && valor < TILES.length) {
+            return TILES[valor];
+        } else {
+            return "???"; 
+        }
+    }
+}
