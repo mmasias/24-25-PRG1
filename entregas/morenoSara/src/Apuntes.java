2adc7dfbd8e3a04de46c8f6acee70bfa3c375869
sara.moreno@alumnos.uneatlantico.es <sara.moreno@alumnos.uneatlantico.es>
2025-01-13 20:45:56 +0100

    Teoria 13/1

---
+++ entregas/morenoSara/src/Apuntes.java
@@ -0,0 +1,70 @@
+//TIPOS DE VARIABLES:
+//    INT = Números
+//    LONG = entero de mayor tamaño
+//    FLOAT = números decimales
+//    BOUBLE = números decimales
+//    BOOLEAN = TRUE / FALSE
+//    CHAR = Caracterres individuales o letras
+//OPERADORES:
+//    -OPERADORES ARITMÉTICOS:
+//        + : suma
+//        - : resta
+//        * : multiplicación
+//        / : división
+//        % : porcentaje
+//    -OPERADORES RELACIONAL:
+//        == : igual
+//        != : no igual
+//        > : mayor
+//        >= : mayor e igual
+//        < : menor
+//        <= : meno o igual
+//    -OPERADORES LÓGICOS:
+//        && : AND -> y
+//        || : OR -> o
+//        !  : NOT -> no
+//CONDICIONALES:
+//    - IF Y ELSE:
+//        if imprescindible y evalua la CONDICIÓN
+//        else que hacer si no se cumple, puede o no aparecer
+//    - ELSE IF:
+//        Encadena instrucciones (varias condicciones)
+//        Pueden ser largas
+//        Usar sangrias
+//    - SWITCH
+//        Varias condicciones
+//        If anidados
+//            Estructura:
+//            int dia = 3;
+//            String nombreDia;
+//                switch (dia)
+//                   case 1:
+//                    nombreDia = "Lunes";
+//                    break;
+//                    case 2:
+//                    nombreDia = "Martes";
+//                    break;
+//                    case 3:
+//                    nombreDia = "Miércoles";
+//                    break;
+//                    default:
+//                    nombreDia = "Día inválido;
+//                
+//                
+//CICLOS:
+//    -FOR:
+//        Se conoce de antemano el número de iteraciones
+//        Estructura:
+//            for (int i=0; > < >= <= ==; i++);
+//            for (declarar variable; condición ;implemento)
+//    -WHILE:
+//        Revisa la condición, si es verdadera ejecuta el código y revisa la condicción
+//        Se repite hasta que sea falsa
+//        Estructura:
+//          While(expresión lógica){
+//              instrucción que se repetirá
+//          }
+//        
+//    -DO WHILE:
+//
+//
