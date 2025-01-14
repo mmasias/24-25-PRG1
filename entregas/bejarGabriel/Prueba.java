dff50b3d06ed6b2ec385a7ffd6383cde25895586
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-14 22:18:25 +0100

    Declare many variables

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,11 +1,11 @@
 public class Prueba {
     public static void main(String[] args) {
-        String firstName = "Gabriel ";
-        String lastName = "Béjar";
-        String fullName = firstName + lastName;
-        System.out.println(fullName);
-        int x = 5;
-        int y = 7;
-        System.out.println(x + y);
+        /*int x = 5;
+        int y = 6;
+        int z = 7;
+        System.out.println(x + y + z);
+        ambos significan lo mismo*/
+        int x = 5, y = 6, z = 7;
+        System.out.println(x + y + z);
     }
 }
