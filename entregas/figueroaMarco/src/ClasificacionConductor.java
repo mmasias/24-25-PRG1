e0afdd3bd4e7509c66dce4710eb3bae74abcc33d
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-01-14 17:39:18 +0100

    Iteracion de clasificado de conductor.
    Posterior a la respuesta de la consulta en el repo, decidi hacer otra iteracion siguiendo una linea similar a la ya comenzada pero utilizando el poder de las referencias de arrays como parametros de 1 funcion principal.

--- entregas/figueroaMarco/src/ClasificacionConductor.java
+++ entregas/figueroaMarco/src/ClasificacionConductor.java
@@ -3,21 +3,33 @@ import java.util.Scanner;
 public class ClasificacionConductor {
 
     public static void main(String[] args) {
-        
-        userInput();
-        hasLicense();
-        ageClasification();
+        int[] inputs;
+        inputs = userInput();
+        outputPrinting(hasLicense(inputs[1]),ageClasification(inputs[0]));  
 
     }
     
-    public static int userInput() {
+    public static int[] userInput() {
         System.out.println("Ingrese la edad del conductor: ");
         Scanner input = new Scanner(System.in);
         int age = input.nextInt();
         System.out.println("Ingrese si tiene licencia (1 para Si, 2 para No): ");
         int licenseUserInput = input.nextInt();
         input.close();
+        int[] inputs = {age, licenseUserInput};
 
-        return age, licenseUserInput
+        return inputs;
     };
 
+    public static boolean hasLicense(int licenseInput) {
+        boolean hasLicense;
+        hasLicense = licenseInput == 1 ? true : false;
+        return hasLicense;
+    };
+
+    public static string ageClasification(int ageInput) {
+        
+    };
+
+    public static void outputPrinting(boolean hasLicense, string ageClasification) {
+      };
