1952519492231b97b936f31fd0a04126d0c9eb66
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-01-15 00:01:06 +0100

    remake halloween start

--- entregas/chavezGuillermo/scr/home-work/halloweenremake.java
+++ entregas/chavezGuillermo/scr/home-work/halloweenremake.java
@@ -1,7 +1,9 @@
 class halloweenremake{
-    public static void main(String[] args){
-    
-        int bagOne=0,bagTwo=0,bagThree=0;
-        System.out.println(bagTwo);
+    public static void main(String[]args){
+            int bagOne=0,bagTwo=0,bagThree=0;
+            Boolean fullOne=false,fullTwo=false,fullThree=false;
+            int building[][][];
+
+
     }
 } 
