82959f4589fe06de64c85314ad507a339283a05b
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-14 22:19:42 +0100

    one value multiple variables

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,11 +1,7 @@
 public class Prueba {
     public static void main(String[] args) {
-        /*int x = 5;
-        int y = 6;
-        int z = 7;
-        System.out.println(x + y + z);
-        ambos significan lo mismo*/
-        int x = 5, y = 6, z = 7;
+        int x, y, z;
+        x = y = z = 50;
         System.out.println(x + y + z);
     }
 }
