98b4ac8178e00673c914e68ad2d42fa817fbf71e
sara.moreno@alumnos.uneatlantico.es <sara.moreno@alumnos.uneatlantico.es>
2025-01-14 21:52:20 +0100

    1ªopción: Configuración,2ª opción:Test y 3ª opción: Salir

--- entregas/morenoSara/src/Examen.java
+++ entregas/morenoSara/src/Examen.java
@@ -9,55 +9,161 @@ public class Examen {
         final int SALIR = 3;
 
         int eleccionUsuario;
+        int primerFactor = 0;
+        int nivelDificultad = 0;
+
+        boolean examinandose = true;
 
         System.out.println("1.Configuración");
         System.out.println("2.Ejecucción del Test");
         System.out.println("3.Salir del sistema");
+
         eleccionUsuario = scanner.nextInt();
 
-        if (eleccionUsuario == CONFIGURACION) {
-            configuración();
-        }else if (eleccionUsuario == TEST) {
-            System.out.println("2");
-
-        }else{
-            System.out.println("3");
+        int resultado;
+        while (examinandose) {
+            if (eleccionUsuario == CONFIGURACION) {
+                primerFactor = configuraciónPrimerFactor();
+                nivelDificultad = configuraciónNivelDificultad();
+   
+            }else if (eleccionUsuario == TEST) {
+                if (primerFactor == 0 && nivelDificultad == 0) {
+                    System.out.println("Configura primero el examen.");
+                    examinandose = false;
+                }else{
+                    preguntasTest(nivelDificultad, primerFactor);
+                }
+            }else{
+                examinandose = false;
+            }
         }
-        
+    
         scanner.close();
     }
-    static void configuración (){
+    static int configuraciónPrimerFactor (){
+
         Scanner scanner = new Scanner(System.in);
-
-        final int FACIL = 1;
-        final int NORMAL = 2;
-        final int DIFICIL = 3;
-
         
         int primerFactor;
-        int nivelDificultad;
-        int segundoFactor;
 
         System.out.println("Elige un número entre el 2 y el 10 para realizar la evaluación:");
         primerFactor = scanner.nextInt();
 
-        System.out.println("Elige el nivel de dificultad: 1 Fácil 2 Normal 3 Avanzado");
-        nivelDificultad = scanner.nextInt();
+        return (primerFactor);
 
+    }
+    static int configuraciónNivelDificultad (){
+        Scanner scanner = new Scanner(System.in);
+
+        int nivelDificultadElige;
+
+        System.out.println("Elige el nivel de dificultad: 1 Fácil 2 Normal 3 Avanzado");
+        nivelDificultadElige = scanner.nextInt();
+
+        return (nivelDificultadElige);
+    }
+        
+    static void preguntasTest ( int  nivelDificultad,int primerFactor){
+        Scanner scanner = new Scanner(System.in);
+
+        final int FACIL = 1;
+        final int NORMAL = 2;
+        final int DIFÍCIL = 3;
+
+        int segundoFactor;
+        int resultadoTest;
+
+        int puntuacionUsuario = 0;
+        
+        boolean preguntando = true;
+        
         if (nivelDificultad == FACIL) {
             int maximo = 6;
             int minimo = 1;
-            segundoFactor = (int) (Math.random() * maximo - minimo + 1) + minimo;
+            int numeroPregunta = 0;
+
+            while (preguntando) {
+                numeroPregunta ++;
+                int solucionPregunta;
+                int respuestaUsurario;
+
+                segundoFactor = (int) (Math.random() * maximo - minimo + 1) + minimo;
+                System.out.println("Cuanto es:"+ primerFactor + " x "+ segundoFactor);
+
+                respuestaUsurario = scanner.nextInt();
+
+                solucionPregunta = primerFactor*segundoFactor;
+
+                if (numeroPregunta == 5) {
+                    preguntando = false;
+                }
+                if (respuestaUsurario == solucionPregunta) {
+                    puntuacionUsuario = puntuacionUsuario + 2;
+                }else{
+                    puntuacionUsuario = puntuacionUsuario - 1;
+                }
+            }
+    
         }else if (nivelDificultad == NORMAL) {
             int maximo = 10;
             int minimo = 1;
-            segundoFactor = (int) (Math.random() * maximo - minimo + 1) + minimo;
-        }else{
+            int numeroPregunta = 0;
+
+            while (preguntando) {
+                numeroPregunta ++;
+                int solucionPregunta;
+                int respuestaUsurario;
+
+                segundoFactor = (int) (Math.random() * maximo - minimo + 1) + minimo;
+                System.out.println("Cuanto es:"+ primerFactor + " x "+ segundoFactor);
+
+                respuestaUsurario = scanner.nextInt();
+
+                solucionPregunta = primerFactor*segundoFactor;
+
+                if (numeroPregunta == 5) {
+                    preguntando = false;
+                }
+                if (respuestaUsurario == solucionPregunta) {
+                    puntuacionUsuario = puntuacionUsuario + 2;
+                }else{
+                    puntuacionUsuario = puntuacionUsuario - 1;
+                }
+            }
+            
+        }else {
             int maximo =10;
             int minimo = 4;
-            segundoFactor = (int) (Math.random() * maximo - minimo + 1) + minimo;
-        }
-        scanner.close();
+            int numeroPregunta = 0;
 
+            while (preguntando) {
+                numeroPregunta ++;
+                int solucionPregunta;
+                int respuestaUsurario;
+
+                segundoFactor = (int) (Math.random() * maximo - minimo + 1) + minimo;
+                System.out.println("Cuanto es:"+ primerFactor + " x "+ segundoFactor);
+
+                respuestaUsurario = scanner.nextInt();
+
+                solucionPregunta = primerFactor*segundoFactor;
+
+                if (numeroPregunta == 5) {
+                    preguntando = false;
+                }
+                if (respuestaUsurario == solucionPregunta) {
+                    puntuacionUsuario = puntuacionUsuario + 2;
+                }else{
+                    puntuacionUsuario = puntuacionUsuario - 1;
+                }
+            }
+        }
+        System.out.println("Puntuación:" + puntuacionUsuario);
+        if (puntuacionUsuario > 5) {
+            System.out.println("Aprobado");
+        }else{
+            System.out.println("Suspenso");
+        }
+        
     }
 }
