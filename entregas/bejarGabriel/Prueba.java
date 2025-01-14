faf394cff2266d2b3c2a96da7566ac5b9cb853e3
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-14 22:34:12 +0100

    Ejemplo Info de Estudiante

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,7 +1,16 @@
 public class Prueba {
     public static void main(String[] args) {
-        int x, y, z;
-        x = y = z = 50;
-        System.out.println(x + y + z);
+        //students data
+        String studentName = "Gabriel Béjar";
+        int studentID = 7;
+        int studentAge = 20;
+        float studentFee = 72.5f;
+        char studentGrade = 'A';
+        //print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
     }
 }
