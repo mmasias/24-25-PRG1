dce44491f1d5f2a98fea0786ee30178bad42cc4c
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-14 11:52:21 +0100

    pequeñas correcciones

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,5 +1,7 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("I'm learning Java!");
+        System.out.println("This is great :)");
     }
 }
