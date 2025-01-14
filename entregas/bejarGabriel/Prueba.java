c078c56d4f38b1ba422bfeaf85128f4ddabb9c65
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-14 22:05:06 +0100

    Variables

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,7 +1,12 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("I'm learning Java!");
-        System.out.println("This is great :)");
+        String name = "Gabriel";
+        System.out.println(name);
+        final int myNum = 25;
+        System.out.println(myNum);
+        float myFloat = 9.84f;
+        System.out.println(myFloat);
+        char myChar = 'D';
+        System.out.println(myChar);
     }
 }
