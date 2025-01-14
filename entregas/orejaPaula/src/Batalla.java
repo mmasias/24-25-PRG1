c3008c739deedf749719a11abc64e96a9cdc2814
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-01-14 11:48:40 +0100

    definir variables iniciales para Guerrero y Vampiro

--- entregas/orejaPaula/src/Batalla.java
+++ entregas/orejaPaula/src/Batalla.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 class Batalla {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        int poderGuerrero = 20, poderVampiro = 10;
+        int ataqueGuerrero = 2, ataqueVampiro = 4;
+        double exitoGuerrero = 0.5, exitoVampiro = 0.5;
+
+        int turno = 0;
+        boolean viven = (poderGuerrero * poderVampiro) > 0;
+
+
+
+
+
+
+
+
+
+
+        
+
         entrada.close();
     }
 }
