4ecb455e8c4fd6b0de8f7487f2354b4a3530dd30
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 17:14:33 +0100

    if else ejemplo 2

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,12 +1,10 @@
 public class Prueba {
     public static void main(String[] args) {
-        int x = 20;
-        int y = 48;
-
-        if (x >= y){
-            System.out.println(x + " es mas grande que " + y);
+        int time = 11;
+        if (time >= 12){
+            System.out.println("Buenas tardes!");
         } else{
-            System.out.println(y + " es mas grande que " + x);
+            System.out.println("Buenos días!");
         }
         }
     }
