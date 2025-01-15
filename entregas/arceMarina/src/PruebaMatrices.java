02977bb5924b301dec34b83815d81a3adc53e18c
Marina <marina.arce@alumnos.uneatlantico.es>
2025-01-15 18:58:55 +0100

    Recorrer una matriz unidimensional

---
+++ entregas/arceMarina/src/PruebaMatrices.java
@@ -0,0 +1,10 @@
+class PruebaMatrices {
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento en la posición " + i + ": " + numeros[i]);
+        }
+    }
+}
