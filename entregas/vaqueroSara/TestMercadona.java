f7a20cb9c1677d07e818781183d492468e729631
Sara Vaquero Relea <sara.vaquero@alumnos.uneatlantico.es>
2025-01-15 17:33:03 +0100

    Mercadona con arrays

--- entregas/vaqueroSara/TestMercadona.java
+++ entregas/vaqueroSara/TestMercadona.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class TestMercadona {
 
     public static void main(String[] args) {
@@ -9,22 +7,18 @@ public class TestMercadona {
         final int MAX_PACKS = 15;
         final int CAJA_VACIA = 0;
         final int CLIENTE_EN_COLA = 1;
+        final int NUMERO_CAJAS = 4;
         final double PROBABILIDAD_LLEGADA = 0.6;
 
         int genteEsperando = 0;
 
-        int itemsCaja1 = 0;
-        int itemsCaja2 = 0;
-        int itemsCaja3 = 0;
-        int itemsCaja4 = 0;
 
+        int[] itemsCajas = new int[NUMERO_CAJAS];
        
         int minutosSinCola = 0;
         int personasAtendidas = 0;
         int itemsVendidos= 0;
 
-
-        Scanner scanner = new Scanner(System.in);
 
         for (int tiempoActual = 0; tiempoActual <= TIEMPO_ABIERTO; tiempoActual++) {
 
@@ -34,43 +28,33 @@ public class TestMercadona {
 
             if (genteEsperando > 0) {
 
-                if (itemsCaja1 == CAJA_VACIA && genteEsperando > 0) {
-                    genteEsperando--;
-                    personasAtendidas++;
-                    itemsCaja1 = (int) (Math.random() * (MAX_PACKS - MIN_PACKS) + MIN_PACKS);
-                }
-
-                if (itemsCaja2 == CAJA_VACIA && genteEsperando > 0) {
-                    genteEsperando--;
-                    personasAtendidas++;
-                    itemsCaja2 = (int) (Math.random() * (MAX_PACKS - MIN_PACKS) + MIN_PACKS);
-                }
-
-                if (itemsCaja3 == CAJA_VACIA && genteEsperando > 0) {
-                    genteEsperando--;
-                    personasAtendidas++;
-                    itemsCaja3 = (int) (Math.random() * (MAX_PACKS - MIN_PACKS) + MIN_PACKS);
-                }
-
-                if (itemsCaja4 == CAJA_VACIA && genteEsperando > 0) {
-                    genteEsperando--;
-                    personasAtendidas++;
-                    itemsCaja4 = (int) (Math.random() * (MAX_PACKS - MIN_PACKS) + MIN_PACKS);
+                for (int i = 0; i < itemsCajas.length; i++) {
+                    if (itemsCajas[i] == CAJA_VACIA && genteEsperando > 0) {
+                        genteEsperando--;
+                        personasAtendidas++;
+                        itemsCajas[i] = generarItemsCliente(MIN_PACKS, MAX_PACKS);
+                    }
                 }
             }
 
-            if (itemsCaja1 > 0) {itemsCaja1--; itemsVendidos++;}
-            if (itemsCaja2 > 0) {itemsCaja2--; itemsVendidos++;}
-            if (itemsCaja3 > 0) {itemsCaja3--; itemsVendidos++;}
-            if (itemsCaja4 > 0) {itemsCaja4--; itemsVendidos++;}
+            for (int i = 0; i < itemsCajas.length; i++) {
+                if (itemsCajas[i] > 0) {
+                    itemsCajas[i]--;
+                    itemsVendidos++;
+                }
+            }
+
+            for (int i = 0; i < itemsCajas.length; i++) {
+                System.out.println("Objetos pagándose en caja " + (i + 1) + ": " + itemsCajas[i]);
+            }
 
 
             System.out.println("Tiempo que ha pasado: " + tiempoActual + " minutos.");
             System.out.println("Gente en la cola: " + genteEsperando);
-            System.out.println("Objetos pagándose en caja 1: " + itemsCaja1);
-            System.out.println("Objetos pagándose en caja 2: " + itemsCaja2);
-            System.out.println("Objetos pagándose en caja 3: " + itemsCaja3);
-            System.out.println("Objetos pagándose en caja 4: " + itemsCaja4);
+            System.out.println("Objetos pagándose en caja 1: " + itemsCajas[0]);
+            System.out.println("Objetos pagándose en caja 2: " + itemsCajas[1]);
+            System.out.println("Objetos pagándose en caja 3: " + itemsCajas[2]);
+            System.out.println("Objetos pagándose en caja 4: " + itemsCajas[3]);
 
             
 
@@ -91,7 +75,11 @@ public class TestMercadona {
 
         return genteEsperando;
     }
-
+    
+    static int generarItemsCliente(int MIN_PACKS, int MAX_PACKS) {
+        return (int) (Math.random() * (MAX_PACKS - MIN_PACKS) + MIN_PACKS);
+    }
+    
     static void resultadosFinales(int minutosSinCola, int genteEsperando, int personasAtendidas, int itemsVendidos){
 
         System.out.println("-------------------");
