e7ab932161e34514bf71b4a9588dd5e170d96781
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 20:20:56 +0100

    Concatenar

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,14 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
-        /*int time = 20;
-        if (time < 18){
-            System.out.print("Good day!");
-            } else {
-            System.our.print("Good evening!");
-            }
-        ambos son lo mismo*/
-        int time = 20;
-        String result = (time<=18) ? "Good day!" : "Good evening!";
-        System.out.println(result);
+        String nombre = "Gabriel ";
+        String primerApellido = "Béjar ";
+        String segundoApellido = "Pizarro";
+        System.out.println("Mi nombre completo es " + nombre.concat(primerApellido).concat(segundoApellido));
         }
     }
