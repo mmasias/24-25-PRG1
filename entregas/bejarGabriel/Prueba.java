c568ed2bfb3a27ea1621933d3955988e9ef28da9
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 14:44:07 +0100

    Narrowing casting

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,8 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
-        int myInt = 9;
-        double myDouble = myInt; // Automatic casting: int to double
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual casting: double to int
+        System.out.println(myDouble);   // Outputs 9.78
         System.out.println(myInt);      // Outputs 9
-        System.out.println(myDouble);   // Outputs 9.0
     }
 }
