0abc7f042f2f06988cd386675a67f708a8c3e2e0
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 15:04:30 +0100

    operadores

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,15 +1,7 @@
 public class Prueba {
     public static void main(String[] args) {
-        // Set the maximum possible score in the game to 500
-        int maxScore = 500;
-
-        // The actual score of the user
-        int userScore = 423;
-
-        /* Calculate the percantage of the user's score in relation to the maximum available score.
-        Convert userScore to float to make sure that the division is accurate */
-        float percentage = (float) userScore / maxScore * 100.0f;
-
-        System.out.println("User's percentage is " + percentage);
+        int x = 10;
+        int y = 2;
+        System.out.println(x>y); //se imprime true
     }
 }
