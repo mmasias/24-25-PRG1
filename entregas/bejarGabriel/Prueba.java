91c6c705cf3a791a6941b599dafac983f5d11a58
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 14:48:27 +0100

    Type Casting Example

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,8 +1,15 @@
 public class Prueba {
     public static void main(String[] args) {
-        double myDouble = 9.78d;
-        int myInt = (int) myDouble; // Manual casting: double to int
-        System.out.println(myDouble);   // Outputs 9.78
-        System.out.println(myInt);      // Outputs 9
+        // Set the maximum possible score in the game to 500
+        int maxScore = 500;
+
+        // The actual score of the user
+        int userScore = 423;
+
+        /* Calculate the percantage of the user's score in relation to the maximum available score.
+        Convert userScore to float to make sure that the division is accurate */
+        float percentage = (float) userScore / maxScore * 100.0f;
+
+        System.out.println("User's percentage is " + percentage);
     }
 }
