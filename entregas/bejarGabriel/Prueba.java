35bc90c4fec82987084a2bcd22665564abb44f42
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 14:36:17 +0100

    Widening Casting

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,12 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
-        byte myByte = 127; //Stores whole numbers from -128 to 127
-        short myShort = 32767; //Stores whole numbers from -32,768 to 32,767
-        int myInt = 24; //Stores whole numbers from -2,147,483,648 to 2,147,483,647
-        long myLong = -47; //Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-        float myFloat = 7.84f; //Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
-        double myDouble = 3.14159; //More precise than float
-        boolean myBool = true; //Stores true or false values
-        char myChar = 'F'; //Stores a single character/letter or ASCII values
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+        System.out.println(myInt);      // Outputs 9
+        System.out.println(myDouble);   // Outputs 9.0
     }
 }
