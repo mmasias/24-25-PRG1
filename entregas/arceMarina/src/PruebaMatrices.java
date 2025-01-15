8289125330bee15bc5b6649fb353916b9b895f5f
Marina <marina.arce@alumnos.uneatlantico.es>
2025-01-15 19:00:35 +0100

    Recorrer una matriz bidimensional

--- entregas/arceMarina/src/PruebaMatrices.java
+++ entregas/arceMarina/src/PruebaMatrices.java
@@ -1,10 +1,17 @@
 class PruebaMatrices {
     public static void main(String[] args) {
 
-        int[] numeros = {10, 20, 30, 40, 50};
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
 
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println("Elemento en la posición " + i + ": " + numeros[i]);
+        for (int i = 0; i < matriz.length; i++) { 
+            for (int j = 0; j < matriz[i].length; j++) { 
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
