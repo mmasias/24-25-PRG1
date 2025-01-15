1b424b9613f8baad91dc0bb5de373be1c64694bb
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-15 17:17:46 +0100

    else if ejemplo

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,10 +1,12 @@
 public class Prueba {
     public static void main(String[] args) {
-        int time = 11;
-        if (time >= 12){
-            System.out.println("Buenas tardes!");
-        } else{
-            System.out.println("Buenos días!");
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good morning.");
+        } else if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
         }
         }
     }
