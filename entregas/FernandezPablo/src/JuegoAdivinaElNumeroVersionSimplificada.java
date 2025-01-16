c2d3d25b53205ae6ca89a358499144be4bbc3342
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-01-16 20:05:19 +0100

    Defino Nuevas Variables

--- entregas/FernandezPablo/src/JuegoAdivinaElNumeroVersionSimplificada.java
+++ entregas/FernandezPablo/src/JuegoAdivinaElNumeroVersionSimplificada.java
@@ -5,10 +5,25 @@ public class JuegoAdivinaElNumeroVersionSimplificada {
     public static void main (String args[]){
 
         final int NUMERO_INTENTOS = 5;
+        final int NUMERO_MAXIMO = 50;
+        final int NUMERO_MINIMO = 1;
+        boolean estaJugando = true;
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("Bienvenido al juego de Adivinar el Numero (Versión Simplificada)");
+
+        int NumeroOrdenador = (int)(Math.random()*(NUMERO_MAXIMO-NUMERO_MINIMO)+NUMERO_MINIMO);
+
+        while (estaJugando){
+            
+        }
+
+
+
+
+
+
 
     }
     
