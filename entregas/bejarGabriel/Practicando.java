c1fa706e8d06391eab5fa1247d7ea30684439ba9
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 20:56:52 +0100

    Ejemplo de promedio de edades

--- entregas/bejarGabriel/Practicando.java
+++ entregas/bejarGabriel/Practicando.java
@@ -1,8 +1,12 @@
 public class Practicando {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for (String i : cars) {
-            System.out.println(i);
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+        double avg, sum = 0;
+        int length = ages.length;
+        for (int age : ages){
+            sum += age;
         }
+        avg = sum/length;
+        System.out.println("The average age is: "+avg);
     }
 }
