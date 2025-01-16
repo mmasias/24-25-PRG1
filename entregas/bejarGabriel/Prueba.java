5fe0a006e16159111ea1443fa01704b85ae2ce20
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 12:38:55 +0100

    numeros pares hasta 10

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i += 10){
+        for (int i = 0; i <= 10; i += 2){ //en el tercer statement tambien se podria usar "i = i + 2"
             System.out.println(i);
         }
     }
