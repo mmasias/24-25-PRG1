f8c36b6f4a33e2b293485af1712b0caf13bbcb4a
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 12:31:32 +0100

    For Each Loop Ejemplo con Array

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,10 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
-        for (int i = 1; i <= 2; i++){
-            System.out.println("Outer: " + i);
-            for (int j = 1; j <= 3; j++){
-                System.out.println(" Inner: " + j);
-            }
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars){
+            System.out.println(i);
         }
     }
 }
