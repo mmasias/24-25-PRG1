33dd709c6f4dcb0f7d2288f495f0ea5db743b07d
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-01-16 19:01:23 +0100

    Defino las variables e inicio el scanner y el random

---
+++ entregas/FernandezPablo/src/JuegoLaberintoDePalabras.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class JuegoLaberintoDePalabras {
+    public static void main (String args[]){
+
+        final int NUMERO_INTENTOS = 6;
+        String[] palabras = {"gato", "mesa", "cielo", "nube", "piedra", "flor", "luz", "río"};
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        boolean jugarDeNuevo = true;
+
+
+        
+       
+    } 
+
+    
+}
