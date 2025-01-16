8a900a3e96ffea31513235292cf5fb6a4c333d5f
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 11:42:54 +0100

    While

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,24 +1,9 @@
 public class Prueba {
     public static void main(String[] args) {
-        int dia = 6;
-        switch (dia){
-            case 1:
-            System.out.println("Lunes");
-            break;
-            case 2:
-            System.out.println("Martes");
-            break;
-            case 3:
-            System.out.println("Miercoles");
-            break;
-            case 4:
-            System.out.println("Jueves");
-            break;
-            case 5:
-            System.out.println("Viernes");
-            break;
-            default:
-            System.out.println("Fin de semana!");
+        int i = 0;
+        while (i < 5){
+            System.out.println(i);
+            i++;
         }
         }
     }
