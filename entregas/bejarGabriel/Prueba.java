3c2ac95692f6675289417c8bd7849942f2a36f35
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 11:09:55 +0100

    Switch

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,6 +1,28 @@
 public class Prueba {
     public static void main(String[] args) {
-        int randomNumber = (int)(Math.random() * 101); //escoge un numero al azar entre 0 y 100
-        System.out.println(randomNumber);
+        int dia = 4;
+        switch (dia){
+            case 1:
+            System.out.println("Lunes");
+            break;
+            case 2:
+            System.out.println("Martes");
+            break;
+            case 3:
+            System.out.println("Miercoles");
+            break;
+            case 4:
+            System.out.println("Jueves");
+            break;
+            case 5:
+            System.out.println("Viernes");
+            break;
+            case 6:
+            System.out.println("Sabado");
+            break;
+            case 7:
+            System.out.println("Domingo");
+            break;
+        }
         }
     }
