f4193751c1e3babeadd3b422778500389c5ef650
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 12:42:43 +0100

    tabla de multiplicar para "number"

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,7 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i += 2){ //en el tercer statement tambien se podria usar "i = i + 2"
-            System.out.println(i);
+        int number = 2;
+        for (int i = 1; i <= 10; i++){
+        System.out.println(number + " x " + i + " = " + (number * i));
         }
     }
 }
