b903964ee689f5c7d79ee88cbba06818107718f3
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 12:18:18 +0100

    Nested Loop

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,7 +1,10 @@
 public class Prueba {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i = i + 2){
-            System.out.println(i);
+        for (int i = 1; i <= 2; i++){
+            System.out.println("Outer: " + i);
+            for (int j = 1; j <= 3; j++){
+                System.out.println(" Inner: " + j);
+            }
         }
     }
 }
