0f54d9d5baf06928744634cc9f45dd6480d24ffa
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-01-16 17:17:50 +0100

    desarollo mejor elaborado

--- entregas/chavezGuillermo/scr/home-work/halloweenremake.java
+++ entregas/chavezGuillermo/scr/home-work/halloweenremake.java
@@ -1,9 +1,27 @@
-class halloweenremake{
-    public static void main(String[]args){
-            int bagOne=0,bagTwo=0,bagThree=0;
-            Boolean fullOne=false,fullTwo=false,fullThree=false;
-            int building[][][];
+public class halloweenremake {
+    public static void main(String[] args) {
+        int kidBagOne = 0;
+        int kidBagTwo = 0;
+        int kidBagThree = 0;
 
+        final int MAX_CANDIES = 20;
+        boolean fullBagOne = false;
+        boolean fullBagTwo = false;
+        boolean fullBagThree = false;
+        final int FLOORS_PER_BUILDING = 5;
+        final int APARTMENTS_PER_FLOOR = 4;
 
+        final double OPEN_HOUSE_CHANCE = 0.7;
+        final double GIVES_CANDY_CHANCE = 0.8;
+        final int MAX_CANDY_PER_HOUSE = 3;
+        final int MIN_CANDY_PER_HOUSE = 1;
+
+        int actualFloor = 1;
+        int actualHouse = 1;
+
+        while (actualFloor <= FLOORS_PER_BUILDING) {
+            System.out.println("== == Floor " + actualFloor + " == ==");
+            actualFloor = actualFloor + 1;
+        }
     }
-} 
+}
