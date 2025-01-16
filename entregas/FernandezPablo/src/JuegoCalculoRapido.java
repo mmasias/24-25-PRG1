76661c0a70c7e799dc791e8f30b3196bb90375c1
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-01-16 21:08:21 +0100

    Creo el bloque de codigo while donde dentro tiene un bucle for que define un switch con varios casos de operaciones

--- entregas/FernandezPablo/src/JuegoCalculoRapido.java
+++ entregas/FernandezPablo/src/JuegoCalculoRapido.java
@@ -11,8 +11,51 @@ public class JuegoCalculoRapido {
 
         System.out.println("¡Bienvenido a Cálculo Rápido!");
 
+        while (jugarDeNuevo) {
+            int respuestasCorrectas = 0;
+            long inicioTiempo = System.currentTimeMillis();
 
-        
+            for (int i = 0; i < numeroDePreguntas; i++) {
+                int numero1 = random.nextInt(20) + 1;
+                int numero2 = random.nextInt(20) + 1;
+                int operacion = random.nextInt(3); 
+
+                int respuestaCorrecta = 0;
+                String simboloOperacion = "";
+
+                switch (operacion) {
+                    case 0: 
+                        respuestaCorrecta = numero1 + numero2;
+                        simboloOperacion = "+";
+                        break;
+                    case 1: 
+                        respuestaCorrecta = numero1 - numero2;
+                        simboloOperacion = "-";
+                        break;
+                    case 2: 
+                        respuestaCorrecta = numero1 * numero2;
+                        simboloOperacion = "*";
+                        break;
+                }
+
+                System.out.println("¿Cuánto es " + numero1 + " " + simboloOperacion + " " + numero2 + "?");
+                int respuestaUsuario = scanner.nextInt();
+
+                if (respuestaUsuario == respuestaCorrecta) {
+                    respuestasCorrectas++;
+                    System.out.println("Correcto.");
+                } else {
+                    System.out.println("Incorrecto. La respuesta correcta era " + respuestaCorrecta);
+                }
+
+
+                
+            }
+
+        }
+
+
+
 
     }
 
