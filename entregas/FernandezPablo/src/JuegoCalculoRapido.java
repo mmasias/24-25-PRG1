9cd3b9989e9d1c9339cc6ff035b7d90465f5c2b1
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-01-16 21:05:08 +0100

    Defino las primeras variables

---
+++ entregas/FernandezPablo/src/JuegoCalculoRapido.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class JuegoCalculoRapido {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean jugarDeNuevo = true;
+        int numeroDePreguntas = 5;
+
+        System.out.println("¡Bienvenido a Cálculo Rápido!");
+
+
+        
+
+    }
+
+
+    
+}
