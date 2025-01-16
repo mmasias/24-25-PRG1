55549721c09a319dc4abeab12f0843ab4a67b8c7
pablofernandez2-cmd <pablo.fernandez2@alumnos.uneatlantico.es>
2025-01-16 20:44:40 +0100

    Defino las primeras variables

---
+++ entregas/FernandezPablo/src/JuegoSumarHasta100.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class JuegoSumarHasta100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean jugarDeNuevo = true;
+
+        System.out.println("¡Bienvenido a Sumar Hasta 100!");
+
+
+
+
+        
+
+    }
+
+    
+}
