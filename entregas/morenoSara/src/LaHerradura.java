3494808872beca709610cd9656c73948739c5465
sara.moreno@alumnos.uneatlantico.es <sara.moreno@alumnos.uneatlantico.es>
2025-01-16 12:03:51 +0100

    intento de hacer el código con métodos

--- entregas/morenoSara/src/LaHerradura.java
+++ entregas/morenoSara/src/LaHerradura.java
@@ -5,23 +5,14 @@ public class LaHerradura {
 
         Scanner entrada = new Scanner(System.in);
 
-        final double PRIMER_TRAMO = 0.1;
-        final double SEGUNDO_TRAMO = 0.22;
-        final double TERCER_TRAMO = 1;
-
-        final int MAXIMO_DE_PUNTOS = 50;
-
-        final int PUNTOS_PARA_ACIERTO = 5;
-        final int PUNTOS_PARA_GANADOR = 3;
-        final int PUNTOS_PARA_EMPATE = 1;
-
         int puntosNaneh = 0;
-        int puntosArmand = 0;
+        int puntosArmand= 0;
+        String dondeCaeArmand;
+        String dondeCaeNaneh;
 
         int numeroTiro = 0;
 
-        String dondeCaeNaneh = "";
-        String dondeCaeArmand = "";
+        
 
         boolean jugando = true;
 
@@ -34,71 +25,122 @@ public class LaHerradura {
             tiroNaneh = Math.random();
             tiroArmand = Math.random();
 
+            
+
+            puntosArmand = repartirPuntosArmand(puntosArmand, numeroTiro);
+            puntosNaneh = repartirPuntosNaneh(puntosNaneh, puntosArmand, numeroTiro);
+            dondeCaeArmand = lugarDeCaidaArmand(puntosArmand);
+            dondeCaeNaneh = lugarDeCaidaNaneh(numeroTiro);
             numeroTiro ++;
-            if (tiroArmand == tiroNaneh) {
-
-                puntosArmand = puntosArmand + PUNTOS_PARA_EMPATE;
-                puntosNaneh = puntosNaneh + PUNTOS_PARA_EMPATE;
-
-                if (tiroArmand <= PRIMER_TRAMO) {
-                    dondeCaeArmand = "Primer tramo";
-                }else if (tiroArmand <= SEGUNDO_TRAMO) {
-                    dondeCaeArmand = "Segundo tramo";
-                }else{
-                    dondeCaeArmand = "Tercer tramo";
-                }
-
-                if (tiroNaneh <= PRIMER_TRAMO) {
-                    dondeCaeNaneh = "Primer tramo";
-                }else if (tiroNaneh <= SEGUNDO_TRAMO) {
-                    dondeCaeNaneh = "Segundo tramo";
-                }else{
-                    dondeCaeNaneh = "Tercer tramo";
-                }
-            }else{
-                if (tiroArmand <= PRIMER_TRAMO) {
-                    dondeCaeArmand = "Primer tramo";
-                    puntosArmand = puntosArmand + PUNTOS_PARA_ACIERTO;
-                }else if (tiroArmand <= SEGUNDO_TRAMO) {
-                    dondeCaeArmand = "Segundo tramo";
-                }else{
-                    dondeCaeArmand = "Tercer tramo";
-                }
-       
-                if (tiroNaneh <=  PRIMER_TRAMO  ) {
-                    dondeCaeNaneh = "Primer tramo";
-                    puntosNaneh = puntosNaneh + PUNTOS_PARA_ACIERTO;
-                }else if (tiroNaneh<= SEGUNDO_TRAMO) {
-                    dondeCaeNaneh = "Segundo tramo";
-                }else{
-                    dondeCaeNaneh = "Tercer tramo";
-                }
-
-                if (tiroArmand < tiroNaneh) {
-                    puntosArmand = puntosArmand + PUNTOS_PARA_GANADOR;
-                } else{
-                    puntosNaneh = puntosNaneh + PUNTOS_PARA_GANADOR;
-                }
-    
-            }
-
-            System.out.println("");
+            System.out.println(""); 
             System.out.println("---------------------");
             System.out.println("Tiro nª:"+ numeroTiro);
+            System.out.println("Armand ha caido en :"+ tiroArmand);
+            System.out.println("Naneh ha caido en :"+ tiroNaneh);
             System.out.println("Armand ha caido en :"+ dondeCaeArmand);
             System.out.println("Naneh ha caido en :"+ dondeCaeNaneh);
             System.out.println("Puntos:");
-            System.out.println("    -Naneh:"+ puntosArmand );
-            System.out.println("    -Armand:" + puntosNaneh);
+            System.out.println("    -Naneh:"+ puntosNaneh );
+            System.out.println("    -Armand:" + puntosArmand);
             System.out.println("---------------------");
             System.out.println("");
-
-            if (puntosArmand >= MAXIMO_DE_PUNTOS || puntosNaneh >= 50) {
-                jugando = false;
-            }
+            jugando = sigueJugando(puntosArmand, puntosNaneh, jugando);
+            
             entrada.nextLine();
-
+            
         }
         entrada.close();
+    }
+    static int repartirPuntosArmand ( int tiroArmand,int tiroNaneh){
+
+        final double PRIMER_TRAMO = 0.1;
+
+        final int PUNTOS_PARA_ACIERTO = 5;
+        final int PUNTOS_PARA_GANADOR = 3;
+        final int PUNTOS_PARA_EMPATE = 1;
+
+       
+        int puntosArmand = 0;
+
+        if (tiroArmand <= PRIMER_TRAMO) {
+            puntosArmand = puntosArmand + PUNTOS_PARA_ACIERTO;
+        }
+        
+        if (tiroArmand < tiroNaneh) {
+            puntosArmand = puntosArmand + PUNTOS_PARA_GANADOR;
+        }
+
+        if (tiroArmand == tiroNaneh) {
+            puntosArmand = puntosArmand - PUNTOS_PARA_EMPATE;
+        }
+
+        return puntosArmand;
+    }
+    static int repartirPuntosNaneh  (int puntosNaneh,int tiroArmand,int tiroNaneh){
+
+        final double PRIMER_TRAMO = 0.1;
+
+        final int PUNTOS_PARA_ACIERTO = 5;
+        final int PUNTOS_PARA_GANADOR = 3;
+        final int PUNTOS_PARA_EMPATE = 1;
+    
+       
+
+        if (tiroNaneh <= PRIMER_TRAMO) {
+            puntosNaneh = puntosNaneh + PUNTOS_PARA_ACIERTO;
+        }
+        
+        if (tiroNaneh < tiroArmand) {
+            puntosNaneh = puntosNaneh + PUNTOS_PARA_GANADOR;
+        }
+
+        if (tiroArmand == tiroNaneh) {
+            puntosNaneh = puntosNaneh - PUNTOS_PARA_EMPATE;
+        }
+
+        return puntosNaneh;
+    }
+    
+    static String lugarDeCaidaArmand (int tiroArmand){
+        final double PRIMER_TRAMO = 0.1;
+        final double SEGUNDO_TRAMO = 0.22;
+        final double TERCER_TRAMO = 1;
+
+        String dondeCaeArmand = "";
+
+        if (tiroArmand <= PRIMER_TRAMO) {
+            dondeCaeArmand = "Primer tramo";
+        }else if (tiroArmand <= SEGUNDO_TRAMO) {
+            dondeCaeArmand = "Segundo tramo";
+        }else{
+            dondeCaeArmand = "Tercer tramo";
+        }
+
+        return dondeCaeArmand;
+    }
+    static String lugarDeCaidaNaneh (int tiroNaneh){
+        final double PRIMER_TRAMO = 0.1;
+        String dondeCaeNaneh = "";
+        
+        final double SEGUNDO_TRAMO = 0.22;
+        final double TERCER_TRAMO = 1;
+
+        if (tiroNaneh <=  PRIMER_TRAMO  ) {
+            dondeCaeNaneh = "Primer tramo";
+        }else if (tiroNaneh <= SEGUNDO_TRAMO) {
+            dondeCaeNaneh = "Segundo tramo";
+        }else{
+            dondeCaeNaneh = "Tercer tramo";
+        }
+
+        return dondeCaeNaneh;
+    }
+
+    static boolean sigueJugando (int puntosArmand, int puntosNaneh,boolean jugando){
+        final int MAXIMO_DE_PUNTOS = 50;
+        if (puntosArmand >= MAXIMO_DE_PUNTOS || puntosNaneh >= MAXIMO_DE_PUNTOS) {
+            jugando = false;
+        }
+        return jugando;
     }
 }
