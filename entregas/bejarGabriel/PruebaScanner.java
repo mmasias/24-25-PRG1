f4295ef5735b3c5667834a7353758c1d599f0058
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 21:26:55 +0100

    ejemplo completo de array con scanner

--- entregas/bejarGabriel/PruebaScanner.java
+++ entregas/bejarGabriel/PruebaScanner.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class PruebaScanner {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
-        double numero;
+        try (Scanner entrada = new Scanner (System.in)) {
+            double numero;
 
-        System.out.println("Digite un número: ");
-        numero = entrada.nextDouble();
-        
-        System.out.println("Su número es: "+numero);
+            System.out.println("Digite un número: ");
+            numero = entrada.nextDouble();
+            
+            System.out.println("Su número es: "+numero);
+        }
     }
 }
