4e5f4f655628a4bd5e8a4c4f2ea36d03628b290d
marcos@macbook <marcos.gonzalez@alumnos.uneatlantico.es>
2025-01-17 00:31:32 +0100

    cambios en la prueba

--- entregas/gonzalezMarcos/src/PruebaExamen.java
+++ entregas/gonzalezMarcos/src/PruebaExamen.java
@@ -14,7 +14,6 @@ public class PruebaExamen {
         final int COMPRA_MINIMA_MARIA = 2;
         final int COMPRA_MINIMA_PEDRO = 1;
 
-
         int[] billetes = new int[3];
         int[] cantidadComprada = new int[3];
 
@@ -26,54 +25,42 @@ public class PruebaExamen {
         billetes[maria] = 8;
         billetes[pedro] = 5;
 
-        for(int estacionActual = 1; estacionActual < NUMERO_ESTACIONES; estacionActual++){
+        for (int estacionActual = 1; estacionActual <= NUMERO_ESTACIONES; estacionActual++) {
+            System.out.println("Estacion actual = " + estacionActual);
 
-            billetes[luis] = billetes[luis] - (int)(Math.random()*(GASTO_MAXIMO - GASTO_MINIMO + 1) + GASTO_MINIMO);
-            billetes[maria] = billetes[maria] - (int)(Math.random()*(GASTO_MAXIMO - GASTO_MINIMO + 1) + GASTO_MINIMO);
-            billetes[pedro] = billetes[pedro] - (int)(Math.random()*(GASTO_MAXIMO - GASTO_MINIMO + 1) + GASTO_MINIMO);
+            for (int i = 0; i < billetes.length; i++) {
 
+                int gasto = (int)(Math.random()*(GASTO_MAXIMO - GASTO_MINIMO + 1) + GASTO_MINIMO);
+                billetes[i] -= gasto;
 
+                String nombre = (i == 0) ? "Luis" : (i == 1) ? "María" : "Pedro";
+                System.out.println(nombre + " gastó un total de " + gasto + " billete/s");
 
-            if (billetes[luis] <= 0) {
-                int compra = (int)(Math.random()*(COMPRA_MAXIMA_LUIS - COMPRA_MINIMA_LUIS + 1) + COMPRA_MINIMA_LUIS);
-                billetes[luis] += compra;
-                cantidadComprada[luis] += compra;
-                System.out.println("Luis se ha quedado sin billetes, compra un nuevo billete");
-                if (Math.random() < PROBABILIDAD_LUIS)
-                    billetes[luis]++;
+                if (billetes[i] <= 0) {
+                    billetes[i] = 0;
+                    double probabilidadCompra = (i == luis) ? PROBABILIDAD_LUIS : (i == maria) ? PROBABILIDAD_MARIA : PROBABILIDAD_PEDRO;    
+                    int compraMaxima = (i == luis) ? COMPRA_MAXIMA_LUIS : (i == maria) ? COMPRA_MAXIMA_MARIA : COMPRA_MAXIMA_PEDRO;
+                    int compraMinima = (i == luis) ? COMPRA_MINIMA_LUIS : (i == maria) ? COMPRA_MINIMA_MARIA : COMPRA_MINIMA_PEDRO;
+                    
+                    if (Math.random() < probabilidadCompra) {
+                        int compra = (int)(Math.random()*(compraMaxima - compraMinima + 1) + compraMinima);
+                        billetes[i] += compra;
+                        cantidadComprada[i] += compra;
+                        System.out.println(nombre + " se quedó sin billetes y compró " + compra + " nuevos billetes.");                        
+                    } else {
+                        billetes[i]++;
+                        cantidadComprada[i]++;
+                    }
 
-            } else if (billetes[maria] <= 0) {
-                int compra = (int)(Math.random()*(COMPRA_MAXIMA_MARIA - COMPRA_MINIMA_MARIA + 1) + COMPRA_MINIMA_MARIA);
-                billetes[maria] += compra;
-                cantidadComprada[maria] += compra;
-                System.out.println("Maria se ha quedado sin billetes, compra un nuevo billete");
-                if (Math.random() < PROBABILIDAD_MARIA)
-                    billetes[maria]++;
-        
-            } else if (billetes[pedro] <= 0) {
-                int compra = (int)(Math.random()*(COMPRA_MAXIMA_PEDRO - COMPRA_MINIMA_PEDRO + 1) + COMPRA_MINIMA_PEDRO);
-                billetes[pedro] += compra;
-                cantidadComprada[pedro] += compra;
-                System.out.println("Pedro se ha quedado sin billetes, compra un nuevo billete");
-                if (Math.random() < PROBABILIDAD_PEDRO)
-                    billetes[pedro]++;
+                }
             }
 
-            
-            
-            System.out.println("Estacion actual = " + estacionActual);
             System.out.println("Billetes de Luis = " + billetes[luis] + " Billetes de Maria = " + billetes[maria] + " Billetes de Pedro = " + billetes[pedro]);
             System.out.println("-------------------------------------------------------------------------------------------------------------------------------");
-        
+
         }
 
         System.out.println("Billetes totales de Luis [" + billetes[luis] + "] --- Billetes totales de Maria [" + billetes[maria] + "] --- Billetes totales de Pesdro [" + billetes[pedro] + "]");
-        System.out.println("Pedro ha comprado un total de [" + cantidadComprada[pedro] + "] billetes --- Maria ha comprado un total de [" + cantidadComprada[maria] + "] billetes --- Pedro ha comprado un total de [" + cantidadComprada[pedro] + "] billetes");
+        System.out.println("Luis ha comprado un total de [" + cantidadComprada[luis] + "] billetes --- Maria ha comprado un total de [" + cantidadComprada[maria] + "] billetes --- Pedro ha comprado un total de [" + cantidadComprada[pedro] + "] billetes");
     }
-
-
-
-
-
-
 }
