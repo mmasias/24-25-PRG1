c25b8eee9742957d8eaf16a1804ba4d4bdf5d1e8
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 19:32:12 +0100

    for each

--- entregas/bejarGabriel/Practicando.java
+++ entregas/bejarGabriel/Practicando.java
@@ -1,8 +1,8 @@
 public class Practicando {
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for (int i = 0; i < cars.length; i++){
-            System.out.println(cars[i]);
+        for (String i : cars) {
+            System.out.println(i);
         }
     }
 }
