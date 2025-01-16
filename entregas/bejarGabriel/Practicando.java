df9dcd583a9fb323935d8703818ee49830ca4734
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 15:43:27 +0100

    continue

--- entregas/bejarGabriel/Practicando.java
+++ entregas/bejarGabriel/Practicando.java
@@ -2,7 +2,7 @@ public class Practicando {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++){
             if (i == 4){
-                break;
+                continue;
             }
             System.out.println(i);
         }
