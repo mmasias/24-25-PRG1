51ee1077ecf28f8705acb11400c9eb7071645894
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 15:54:05 +0100

    arrays

--- entregas/bejarGabriel/Practicando.java
+++ entregas/bejarGabriel/Practicando.java
@@ -1,10 +1,8 @@
 public class Practicando {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
-            if (i == 4){
-                continue;
-            }
-            System.out.println(i);
-        }
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+        int[] myNum = {10, 20, 30, 40};
+        System.out.println(myNum[3]);
     }
 }
