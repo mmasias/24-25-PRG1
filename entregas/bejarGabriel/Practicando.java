ee8da8dc5f83c6dce5971c6f206f1b3cfe6d674f
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 15:42:55 +0100

    Break

---
+++ entregas/bejarGabriel/Practicando.java
@@ -0,0 +1,10 @@
+public class Practicando {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            if (i == 4){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
--- entregas/bejarGabriel/PruebaScanner.java
+++ entregas/bejarGabriel/PruebaScanner.java
@@ -4,8 +4,10 @@ public class PruebaScanner {
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
         double numero;
+
         System.out.println("Digite un número: ");
         numero = entrada.nextDouble();
+        
         System.out.println("Su número es: "+numero);
     }
 }
