979040a13825ad27da97d0f82980e37c0fd3c0d1
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 19:30:00 +0100

    Loop through an array

--- entregas/bejarGabriel/Practicando.java
+++ entregas/bejarGabriel/Practicando.java
@@ -1,8 +1,8 @@
 public class Practicando {
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        System.out.println(cars[0]);
-        int[] myNum = {10, 20, 30, 40};
-        System.out.println(myNum[3]);
+        for (int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
     }
 }
