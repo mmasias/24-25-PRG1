c5c23a3ca1c432c61e72782dd347c5773c946069
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-16 12:07:58 +0100

    Ejemplo for 2

--- entregas/bejarGabriel/Prueba.java
+++ entregas/bejarGabriel/Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba {
     public static void main(String[] args) {
-        for(int i = 1; i < 5; i++){
+        for (int i = 0; i <= 10; i = i + 2){
             System.out.println(i);
         }
-        }
     }
+}
