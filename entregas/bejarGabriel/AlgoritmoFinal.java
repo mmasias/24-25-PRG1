67df6e1190697eec85a80545c2848043e955a1f4
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-17 11:46:54 +0100

    Declaramos Array y Variables

---
+++ entregas/bejarGabriel/AlgoritmoFinal.java
@@ -0,0 +1,8 @@
+public class AlgoritmoFinal {
+    public static void main(String[] args) {
+        int[] numCarnet = {2, 0, 2, 4, 0, 2, 0, 0, 4, 3};
+        int sumPares = 0;
+        int sumImpares = 0;
+
+    }
+}
