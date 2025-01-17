c21b7c1d3dfef062ee4107b2e5783ae020d9f978
AaronUreta <aaron.ureta@uneatlantico.es>
2025-01-17 10:13:52 +0100

    Separar los commits por archivos

--- entregas/uretaAaron/Ejercicios/Retoforif2.java
+++ entregas/uretaAaron/Ejercicios/Retoforif2.java
@@ -1,14 +1,11 @@
-class RetoForIf {
+public class Retoforif2 {
     public static void main(String[] args) {
-
         int dimension = 21;
-
         for (int j = 1; j <= dimension; j = j + 1) {
             for (int i = 1; i <= dimension; i = i + 1) {
-
                 if (
                 // El código desde aqui *********************************
-                    i==11 && j<22 || i<22 && j==11
+                    i==2 && j<22
                 // El código hasta aquí *********************************
                 ) {
                     System.out.print("(*)");
@@ -20,4 +17,3 @@ class RetoForIf {
         }
     }
 }
-
