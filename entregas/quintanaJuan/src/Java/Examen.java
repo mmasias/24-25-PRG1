c517cc28316780f4f60fd43279ccfaf31a6ed09e
juanquintanaing <juan.quintana@alumnos.uneatlantico.es>
2025-01-17 12:23:19 +0100

    suma impares + suma pares

--- entregas/quintanaJuan/src/Java/Examen.java
+++ entregas/quintanaJuan/src/Java/Examen.java
@@ -1,5 +1,8 @@
 public class Examen{
     public static void main(String[] args){
         int [] carnet = {2,0,2,4,0,2,1,2,3,4};
+
+        int sumaImpares = carnet[0] + carnet[2] + carnet[4] + carnet[6] + carnet[8];
+        int sumaPares = carnet[1] + carnet[3] + carnet[5] + carnet[7] + carnet[9];
     }
 }
