cd6b2d41416d8f4f671920d7cae5dbe5fabe87b0
Gabriel <gabriel.bejar@alumnos.uneatlantico.es>
2025-01-17 12:05:42 +0100

    declaracion de numeros que seran el resultado final

--- entregas/bejarGabriel/AlgoritmoFinal.java
+++ entregas/bejarGabriel/AlgoritmoFinal.java
@@ -14,5 +14,8 @@ public class AlgoritmoFinal {
         System.out.println("Suma de los digitos en posiciones pares: " + sumaPares);
         System.out.println("Suma de los digitos en posiciones impares: " + sumaImpares);
 
+        int numero1 = (sumaPares % 6) + 1;
+        int numero2 = (sumaImpares % 6) + 1;
+        int numero3 = ((sumaPares + sumaImpares) % 6) + 1;
     }
 }
