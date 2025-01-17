b6227e869d6c31f791e605abc80a0001c8d48501
juanquintanaing <juan.quintana@alumnos.uneatlantico.es>
2025-01-17 12:24:08 +0100

    calculo º pregunta

--- entregas/quintanaJuan/src/Java/Examen.java
+++ entregas/quintanaJuan/src/Java/Examen.java
@@ -1,8 +1,15 @@
-public class Examen{
-    public static void main(String[] args){
-        int [] carnet = {2,0,2,4,0,2,1,2,3,4};
+public class Examen {
+    public static void main(String[] args) {
+        int[] carnet = { 2, 0, 2, 4, 0, 2, 1, 2, 3, 4 };
 
         int sumaImpares = carnet[0] + carnet[2] + carnet[4] + carnet[6] + carnet[8];
         int sumaPares = carnet[1] + carnet[3] + carnet[5] + carnet[7] + carnet[9];
+        
+        System.out.println("Suma impares:"+sumaImpares);
+        System.out.println("Suma pares:" +sumaPares);
+
+        int pregunta1 = (sumaPares % 6) +1;
+        int pregunta2 = (sumaImpares % 6) +1;
+        int pregunta3 = ((sumaImpares + sumaPares) %6) +1;
     }
 }
