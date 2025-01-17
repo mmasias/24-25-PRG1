31e7296cd4d864b195106d464b4864c2e76cc5e9
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-01-17 02:35:37 +0100

    aplicada funcion que permite el loop de direcciones

--- entregas/chavezGuillermo/scr/home-work/Elavador.java
+++ entregas/chavezGuillermo/scr/home-work/Elavador.java
@@ -6,54 +6,65 @@ public class Elavador {
 
         int currentFloor = 0;
         FLOORS[currentFloor]=1;
-        int destiny;
-        int userDestiny;
+        int destiny=0;
+        int userDestiny=0;
         String direction="-";        
         
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select the floor: ");
-        userDestiny = scanner.nextInt();
-        destiny = userDestiny;
+        askfloor(userDestiny,destiny);
+
         
-        printing(currentFloor,destiny,FLOORS,direction);
+        printing(currentFloor,destiny,FLOORS,direction,userDestiny);
         
                             
     }
                     
-    static void printing(int currentFloor, int destiny, int[] FLOORS,String direction) {
-        while (currentFloor <= destiny) {
+    static void printing(int currentFloor, int destiny, int[] FLOORS,String direction,int userDestiny) {
+        while (currentFloor != destiny) {
             int elevatorPrint=(FLOORS.length-1);
-            while(elevatorPrint>=0){
-                direction=directionMaking(currentFloor,destiny);
-                System.out.println(elevatorPrint+"-> "+parse(FLOORS[elevatorPrint],destiny,direction));
-                elevatorPrint=elevatorPrint-1;
-            } 
-            currentFloor=currentFloor+1;
+                while(elevatorPrint>=0){
+                    direction=directionMaking(currentFloor,destiny);
+                    System.out.println(elevatorPrint+"-> "+parse(FLOORS[elevatorPrint],destiny,direction));
+                    elevatorPrint=elevatorPrint-1;} 
+            if(currentFloor<destiny){
+                currentFloor=currentFloor+1;
+            }
+            if(currentFloor>destiny){
+                currentFloor=currentFloor-1;
+            }
             Arrays.fill(FLOORS,0);
             FLOORS[currentFloor]=1;
             System.out.println();
         }
+        if(currentFloor==destiny){
+            int elevatorPrint=(FLOORS.length-1);
+                while(elevatorPrint>=0){
+                    direction=directionMaking(currentFloor,destiny);
+                    System.out.println(elevatorPrint+"-> "+parse(FLOORS[elevatorPrint],destiny,direction));
+                    elevatorPrint=elevatorPrint-1;}
+            askfloor(userDestiny, destiny);
+        }
     }
-    
     static String parse(int change, int destiny, String direction){
         return switch (change) {
             case 0 -> "|   |";
             case 1 -> "|"+direction+ destiny +direction+"|";
             default -> "???";
-        };
-    }
+        };}
 
     static String directionMaking(int currentFloor, int destiny) {
         if(currentFloor<destiny){
-            return "^";
-        }
+            return "^";}
         if(currentFloor>destiny){
-            return "v";
-        }
+            return "v";}
         //if(currentFloor==destiny){
         else{
-            return "-";
-        }
+            return "-";}}
+
+    static void askfloor(int userDestiny, int destiny) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select the floor: ");
+        userDestiny = scanner.nextInt();
+        destiny = userDestiny;
     }
 }
         
