8e16ebd76fc83c3ccd3ea3f4922dd6d4805e7e67
juanquintanaing <juan.quintana@alumnos.uneatlantico.es>
2025-01-17 12:25:48 +0100

    condicional para modificar calculo de pregunta 3 porque se repite + putprint ºpregunta

--- entregas/quintanaJuan/src/Java/Examen.java
+++ entregas/quintanaJuan/src/Java/Examen.java
@@ -4,12 +4,20 @@ public class Examen {
 
         int sumaImpares = carnet[0] + carnet[2] + carnet[4] + carnet[6] + carnet[8];
         int sumaPares = carnet[1] + carnet[3] + carnet[5] + carnet[7] + carnet[9];
-        
-        System.out.println("Suma impares:"+sumaImpares);
-        System.out.println("Suma pares:" +sumaPares);
 
-        int pregunta1 = (sumaPares % 6) +1;
-        int pregunta2 = (sumaImpares % 6) +1;
-        int pregunta3 = ((sumaImpares + sumaPares) %6) +1;
+        System.out.println("Suma impares:" + sumaImpares);
+        System.out.println("Suma pares:" + sumaPares);
+
+        int pregunta1 = (sumaPares % 6) + 1;
+        int pregunta2 = (sumaImpares % 6) + 1;
+        int pregunta3 = ((sumaImpares + sumaPares) % 6) + 1;
+
+        if( pregunta3 == pregunta2){
+            pregunta3= pregunta3 +1;
+        }
+    
+        System.out.println("Pregunta 1º:"+pregunta1);
+        System.out.println("Pregunta 2º:"+pregunta2);
+        System.out.println("Pregunta 3º:"+pregunta3);
     }
 }
