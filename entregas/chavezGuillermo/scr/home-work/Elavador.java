994b94d3b34bc24b7e3bded39b5ac0d64fae75bf
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-01-17 02:19:39 +0100

    implementado funcion de identificacion de direccion

--- entregas/chavezGuillermo/scr/home-work/Elavador.java
+++ entregas/chavezGuillermo/scr/home-work/Elavador.java
@@ -2,28 +2,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Elavador {
     public static void main(String[] args) {
-        final int FLOOR_TEMPLATE[] = {0,0,0,0,0,0,0,0,0,0}; 
         int FLOORS [] = {0,0,0,0,0,0,0,0,0,0}; 
 
         int currentFloor = 0;
         FLOORS[currentFloor]=1;
         int destiny;
-        int userDestiny;        
+        int userDestiny;
+        String direction="-";        
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select the floor: ");
         userDestiny = scanner.nextInt();
         destiny = userDestiny;
         
-        printing(currentFloor,destiny,FLOORS,FLOOR_TEMPLATE);
+        printing(currentFloor,destiny,FLOORS,direction);
         
                             
     }
                     
-    static void printing(int currentFloor, int destiny, int[] FLOORS, int[] FLOOR_TEMPLATE) {
+    static void printing(int currentFloor, int destiny, int[] FLOORS,String direction) {
         while (currentFloor <= destiny) {
             int elevatorPrint=(FLOORS.length-1);
             while(elevatorPrint>=0){
+                direction=directionMaking(currentFloor,destiny);
                 System.out.println(elevatorPrint+"-> "+parse(FLOORS[elevatorPrint],destiny,direction));
                 elevatorPrint=elevatorPrint-1;
             } 
@@ -34,12 +35,25 @@ public class Elavador {
         }
     }
     
-    static String parse(int change, int destiny String direction){
+    static String parse(int change, int destiny, String direction){
         return switch (change) {
             case 0 -> "|   |";
             case 1 -> "|"+direction+ destiny +direction+"|";
             default -> "???";
         };
+    }
+
+    static String directionMaking(int currentFloor, int destiny) {
+        if(currentFloor<destiny){
+            return "^";
+        }
+        if(currentFloor>destiny){
+            return "v";
+        }
+        //if(currentFloor==destiny){
+        else{
+            return "-";
+        }
     }
 }
         
